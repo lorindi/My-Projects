b90b6709408f71e70777107419b36d945e37b126
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2023-11-15 12:48:44 +0200

    Create site service

--- TheWizardOfWebsites/frontend/src/services/applicationService.jsx
+++ TheWizardOfWebsites/frontend/src/services/applicationService.jsx
@@ -1,1 +1,13 @@
+const baseUrl = 'http://localhost:3030/jsonstore'
 
+export const create = async (siteData) => {
+    const response = await fetch(`${baseUrl}/sites`, {
+        method: "POST",
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(siteData)
+    })
+    const result = await response.json()
+    return result
+}
