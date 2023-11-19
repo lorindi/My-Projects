df9d5900bc7769396d85fd731febed1d9ab87cde
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2023-11-19 10:39:50 +0200

    Adding animation for each card

--- TheWizardOfWebsites/frontend/src/components/Application/Item/Item.jsx
+++ TheWizardOfWebsites/frontend/src/components/Application/Item/Item.jsx
@@ -3,21 +3,25 @@ import styles from "./Item.module.css";
 
 export const Item = ({ _id, image, title, price, shortDescription }) => {
   return (
-    <Link to={`/sites/${_id}/details`} className={styles.contentCard}>
-      <img className={styles.cardImg} src={image} alt="" />
-      <div className={styles.cardInfo}>
-        <h4 className={styles.cardTitle}>{title}</h4>
-        <div className={styles.cardMoreInfo}>
-          <p className={styles.cardDescription}>{shortDescription}</p>
-          <p className={styles.cardPrice}>
-            {price}
-            <span>lv.</span>
-          </p>
-        </div>
-        {/* <Link to={`/sites/${_id}/details`} className={styles.cardButton}>
+    <div className={styles.containerCard}>
+      <Link to={`/sites/${_id}/details`} className={styles.contentCard}>
+        <img className={styles.cardImg} src={image} alt="" />
+
+        <div className={styles.cardInfo}>
+       
+            <h4 className={styles.cardTitle}>{title}</h4>
+
+            <p className={styles.cardDescription}>{shortDescription}</p>
+            <p className={styles.cardPrice}>
+              {price}
+              <span>lv.</span>
+            </p>
+          </div>
+          {/* <Link to={`/sites/${_id}/details`} className={styles.cardButton}>
             More
           </Link> */}
-      </div>
-    </Link>
+  
+      </Link>
+    </div>
   );
 };
