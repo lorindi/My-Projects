52292d3ef087faeb8cd84ea99003bb6c9d3f74aa
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2023-11-22 16:46:04 +0200

    Update Navigation

--- TheWizardOfWebsites/frontend/src/components/Header/Header.jsx
+++ TheWizardOfWebsites/frontend/src/components/Header/Header.jsx
@@ -1,47 +1,77 @@
+import { useContext } from "react";
+import { Contexts } from "../../contexts/Contexts";
+
 import styles from "./Header.module.css";
 import { NavLink } from "react-router-dom";
 
-
 export const Header = () => {
+  const { isAuthenticated, username } = useContext(Contexts);
   return (
     <header className={styles.header}>
       <nav className={`${styles.navigation} ${styles.dark}`}>
         <ul className={styles.navBar} role="list">
           <li className={styles.navBarEl}>
-            <NavLink className={styles.link} style={({ isActive }) => ({
-            color: isActive ? "lightgreen" : "lightblue",
-          })} to="/">
+            <NavLink
+              className={styles.link}
+              style={({ isActive }) => ({
+                color: isActive ? "lightgreen" : "lightblue",
+              })}
+              to="/"
+            >
               Home
             </NavLink>
           </li>
           {/* <li className={styles.navBarEl}>
-            <NavLink className={styles.link} style={({ isActive }) => ({
-            color: isActive ? "lightblue" : "lightgreen",
-          })} to="/about-us">
+            <NavLink
+              className={styles.link}
+              style={({ isActive }) => ({
+                color: isActive ? "lightblue" : "lightgreen",
+              })}
+              to="/about-us"
+            >
               About us
             </NavLink>
           </li> */}
-          <li className={styles.navBarEl}>
-            <NavLink className={styles.link} style={({ isActive }) => ({
-            color: isActive ? "lightgreen" : "lightblue",
-          })} to="/sites">
-              Sites
-            </NavLink>
-          </li>
-          <li className={styles.navBarEl}>
-            <NavLink className={styles.link} style={({ isActive }) => ({
-            color: isActive ? "lightgreen" : "lightblue",
-          })} to="/profile">
-              Profile
-            </NavLink>
-          </li>
-          <li className={styles.navBarEl}>
-            <NavLink className={styles.link} style={({ isActive }) => ({
-            color: isActive ? "lightgreen" : "lightblue",
-          })} to="/login">
-              Sign in
-            </NavLink>
-          </li>
+          {isAuthenticated && (
+            <li className={styles.navBarEl}>
+              <NavLink
+                className={styles.link}
+                style={({ isActive }) => ({
+                  color: isActive ? "lightgreen" : "lightblue",
+                })}
+                to="/sites"
+              >
+                Sites
+              </NavLink>
+            </li>
+          )}
+          {isAuthenticated && (
+            <li className={styles.navBarEl}>
+              <NavLink
+                className={styles.link}
+                style={({ isActive }) => ({
+                  color: isActive ? "lightgreen" : "lightblue",
+                })}
+                to="/profile"
+              >
+                {username}
+              </NavLink>
+            </li>
+          )}
+          {!isAuthenticated && (
+            <li className={styles.navBarEl}>
+              <NavLink
+                className={styles.link}
+                style={({ isActive }) => ({
+                  color: isActive ? "lightgreen" : "lightblue",
+                })}
+                to="/login"
+              >
+                Sign in
+              </NavLink>
+            </li>
+          )}
+
           {/* <li className={styles.navBarEl}>
             <NavLink className={styles.link} style={({ isActive }) => ({
             color: isActive ? "lightblue" : "lightgreen",
