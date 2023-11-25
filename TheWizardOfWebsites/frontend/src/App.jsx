0d2033dac1c3dd0979a5cae19cc4b03f4987c8cf
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2023-11-25 15:20:12 +0200

    Create auth provider

--- TheWizardOfWebsites/frontend/src/App.jsx
+++ TheWizardOfWebsites/frontend/src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import { Routes, Route, useNavigate } from "react-router-dom";
-
-import * as authService from "./services/authService";
-import { Contexts } from "./contexts/Contexts";
+import { AuthProvider } from "./contexts/Contexts";
 import Path from "./components/paths";
 
 import { NotFound } from "./components/NotFound/NotFound";
@@ -17,53 +14,11 @@ import { Profile } from "./components/Profile/Profile";
 import { Footer } from "./components/Footer/Footer";
 import { List } from "./components/Application/List/List";
 import { Create } from "./components/Application/Create/Create";
-// import { Edit } from "./components/Application/Edit/Edit";
 import { Details } from "./components/Application/Details/Details";
-// import { Comments } from "./components/Application/Comments/Comments";
-// import { useEffect, useState } from "react";
 
 function App() {
-  const navigate = useNavigate();
-  const [auth, setAuth] = useState(() => {
-    localStorage.removeItem('accessToken')
-    return {}
-  });
-
-  const onLoginSubmit = async (values) => {
-    const result = await authService.login(values.email, values.password);
-    setAuth(result);
-
-    localStorage.setItem("accessToken", result.accessToken);
-    navigate(Path.Home);
-  };
-
-  const registerSubmitHandler = async (values) => {
-    const result = await authService.register(values.email, values.password);
-
-    localStorage.setItem("accessToken", result.accessToken);
-
-    setAuth(result);
-    navigate(Path.Home);
-  };
-
-  const logoutHandler = () => {
-    setAuth({});
-    localStorage.removeItem
-    // navigate(Path.Home)
-  };
-
-  const values = {
-    registerSubmitHandler,
-    onLoginSubmit,
-    logoutHandler,
-    username: auth.username || auth.email,
-    email: auth.email,
-    // isAuthenticated: !!auth.accessToken,
-
-    isAuthenticated: !!auth.email,
-  };
   return (
-    <Contexts.Provider value={values}>
+    <AuthProvider>
       <div id="box">
         <Header />
         <main className="main">
@@ -82,7 +37,7 @@ function App() {
         </main>
         <Footer />
       </div>
-    </Contexts.Provider>
+    </AuthProvider>
   );
 }
 
--- TheWizardOfWebsites/frontend/src/contexts/Contexts.jsx
+++ TheWizardOfWebsites/frontend/src/contexts/Contexts.jsx
@@ -1,3 +1,52 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import * as authService from "../services/authService";
+import { useNavigate } from "react-router-dom";
+import Path from "../components/paths";
 
 export const Contexts = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [auth, setAuth] = useState(() => {
+    localStorage.removeItem("accessToken");
+    return {};
+  });
+
+  const onLoginSubmit = async (values) => {
+    const result = await authService.login(values.email, values.password);
+    setAuth(result);
+
+    localStorage.setItem("accessToken", result.accessToken);
+    navigate(Path.Home);
+  };
+
+  const registerSubmitHandler = async (values) => {
+    const result = await authService.register(values.email, values.password);
+
+    localStorage.setItem("accessToken", result.accessToken);
+
+    setAuth(result);
+    navigate(Path.Home);
+  };
+
+  const logoutHandler = () => {
+    setAuth({});
+    localStorage.removeItem;
+    // navigate(Path.Home)
+  };
+
+  const values = {
+    registerSubmitHandler,
+    onLoginSubmit,
+    logoutHandler,
+    username: auth.username || auth.email,
+    email: auth.email,
+    // isAuthenticated: !!auth.accessToken,
+
+    isAuthenticated: !!auth.email,
+  };
+  return (
+  <Contexts.Provider value={values}>
+    {children}
+  </Contexts.Provider>);
+};
