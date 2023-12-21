bc5f1ea622f83ac66ab500080fb6981a0edc0053
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2023-12-21 21:30:39 +0200

    Add files via upload

--- WebCrafters/softuni-practice-server-master/client.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    input: './client/js/app.js',
-    output: {
-        name: 'Client',
-        file: './dist/client.js',
-        format: 'es'
-    },
-    external: ['lit-html', 'page']
-};
--- WebCrafters/softuni-practice-server-master/client/js/dom.js
+++
@@ -1,8 +0,0 @@
-import { html, render } from 'https://unpkg.com/lit-html@1.3.0?module';
-import { until } from 'https://unpkg.com/lit-html@1.3.0/directives/until?module';
-
-export {
-    html,
-    render,
-    until
-};
