2453551e44bb3ab4e09dcf3e534932e2eab09b11
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-01-21 15:33:29 +0200

    Update App.jsx

--- MagicSushiLora/MagicSushiLoraReact/src/App.jsx
+++ MagicSushiLora/MagicSushiLoraReact/src/App.jsx
@@ -1,17 +1,29 @@
 import { Routes, Route } from "react-router-dom";
 import { Header } from "./components/header/Header";
 import { Footer } from "./components/footer/Footer";
+import { AuthProvider } from "./contexts/Contexts";
+import { Login } from "./components/login/Login";
+import { Register } from "./components/register/Register";
+import { Logout } from "./components/logout/Logout";
+import { Home } from "./components/home/Home";
+
 function App() {
   return (
-    <div>
-      <Header/>
-      <main>
-        <Routes>
-          <Route></Route>
-        </Routes>
-      </main>
-      <Footer/>
-    </div>
+    <AuthProvider>
+      <div>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/logout" element={<Logout />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </AuthProvider>
   );
 }
 
