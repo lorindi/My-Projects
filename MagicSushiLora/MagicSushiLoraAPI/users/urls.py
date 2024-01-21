3934702fa9f1b09178d76eb7dc73fa456b1f39a5
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-01-21 15:36:43 +0200

    Update user urls

--- MagicSushiLora/MagicSushiLoraAPI/users/urls.py
+++ MagicSushiLora/MagicSushiLoraAPI/users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import SushiUserList
+from .views import SushiUserList, UserRegistration, UserLogin, UserLogout
 
 urlpatterns = [
     path('', SushiUserList.as_view(), name='user-list'),
+    path('register/', UserRegistration.as_view(), name='user-registration'),
+    path('login/', UserLogin.as_view(), name='user-login'),
+    path('logout/', UserLogout.as_view(), name='user-logout'),
 ]
