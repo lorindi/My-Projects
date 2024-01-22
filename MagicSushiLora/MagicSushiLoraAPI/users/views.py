a0bbfb5a5d0ef43437fb6ef37537b11580b47b2d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-01-22 18:29:11 +0200

    Adding registration and login

--- MagicSushiLora/MagicSushiLoraAPI/users/views.py
+++ MagicSushiLora/MagicSushiLoraAPI/users/views.py
@@ -1,51 +1,67 @@
-import pdb
-
-from django.http import JsonResponse
-from rest_framework import generics, permissions
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+
+from .serializers import UserSerializer
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from django.core.exceptions import ObjectDoesNotExist
 from .models import SushiUser
-from .serializers import SushiUserSerializer
+import re
 
 
-class SushiUserList(generics.ListAPIView):
-    queryset = SushiUser.objects.all()
-    serializer_class = SushiUserSerializer
-
-
-class UserRegistration(APIView):
-    permission_classes = [permissions.AllowAny]
-
-    def post(self, request):
-        serializer = SushiUserSerializer(data=request.data)
+class RegisterUserAPIView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
-            Token.objects.create(user=user)
-            return Response(serializer.data, status=201)
-        print("User registration failed. Errors:", serializer.errors)
-
-        return Response(serializer.errors, status=400)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class UserLogin(APIView):
-    permission_classes = [permissions.AllowAny]
-
-    def post(self, request):
-        email = request.data.get('email')
+class LoginUserAPIView(APIView):
+    def post(self, request, *args, **kwargs):
         password = request.data.get('password')
-        user = authenticate(request, email=email, password=password)
+        email = request.data.get('email')
+        user = None
+
+        if email != '' and re.match(r'^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+(?:\.[a-zA-Z]+)*$', email) and password !='':
+            try:
+                user = SushiUser.objects.get(email=email)
+            except ObjectDoesNotExist:
+                pass
+
+        if not user:
+            user = authenticate(email=email, password=password)
+
         if user:
-            login(request, user)
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key, 'user_id': user.id})
-        return Response({'error': 'Invalid credentials'}, status=401)
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
-class UserLogout(APIView):
-    permission_classes = [permissions.IsAuthenticated]
 
-    def post(self, request):
-        logout(request)
-        return Response({'message': 'Logout successful'}, status=200)
+
+class UserLogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        try:
+            # Delete the user's token to logout
+            request.user.auth_token.delete()
+            return Response({'message': 'Successfully logged out.'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+# @api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+# def user_logout(request):
+#     if request.method == 'POST':
+#         try:
+#             # Delete the user's token to logout
+#             request.user.auth_token.delete()
+#             return Response({'message': 'Successfully logged out.'}, status=status.HTTP_200_OK)
+#         except Exception as e:
+#             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
