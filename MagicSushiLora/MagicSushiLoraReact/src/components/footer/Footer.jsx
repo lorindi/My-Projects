b3e4b1818d941dd21a8f1e1053c7cd43d953c3bc
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-01-27 22:03:06 +0200

    Add files via upload

--- MagicSushiLora/MagicSushiLoraReact/src/components/footer/Footer.jsx
+++ MagicSushiLora/MagicSushiLoraReact/src/components/footer/Footer.jsx
@@ -9,17 +9,73 @@ import {
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./Footer.module.css";
+import { Gmail } from "./Gmail/Gmail";
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
+      <section className={styles.sectionInformationAboutUs}>
+        <div className={styles.informationAboutUs}>
+          <h4 className={styles.informationAboutUsTitle}>Address</h4>
+        </div>
+        <div className={styles.informationAboutUs}>
+          <h4 className={styles.informationAboutUsTitle}>Work Time</h4>
+          <ul role="list" className={styles.workTimeList}>
+            <li className={styles.workTimeListElement}>
+              <h5 className={styles.dayOfTheWeek}>Mon</h5>
+              <p className={styles.workTime}>10:00 - 17:00</p>
+            </li>
+            <li className={styles.workTimeListElement}>
+              <h5 className={styles.dayOfTheWeek}>Tue..</h5>
+              <p className={styles.workTime}>10:00 - 17:00</p>
+            </li>
+            <li className={styles.workTimeListElement}>
+              <h5 className={styles.dayOfTheWeek}>Wed</h5>
+              <p className={styles.workTime}>10:00 - 17:00</p>
+            </li>
+            <li className={styles.workTimeListElement}>
+              <h5 className={styles.dayOfTheWeek}>Thu.</h5>
+              <p className={styles.workTime}>10:00 - 17:00</p>
+            </li>
+            <li className={styles.workTimeListElement}>
+              <h5 className={styles.dayOfTheWeek}>Fri...</h5>
+              <p className={styles.workTime}>10:00 - 17:00</p>
+            </li>
+            <li className={styles.workTimeListElement}>
+              <h5 className={styles.dayOfTheWeek}>Sat..</h5>
+              <p className={styles.workTime}>10:00 - 17:00</p>
+            </li>
+            <li className={styles.workTimeListElement}>
+              <h5 className={styles.dayOfTheWeek}>Sun.</h5>
+              <p className={styles.workTime}>10:00 - 17:00</p>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.informationAboutUs}>
+          <h4 className={styles.informationAboutUsTitle}>Email Us</h4>
+          <Gmail />
+        </div>
+        <div className={styles.informationAboutUs}>
+          <h4 className={styles.informationAboutUsTitle}>Our Gallery</h4>
+        </div>
+      </section>
       <section className={styles.contentFooterNavigation}>
         <h3 className={styles.footerNavigationTitle}>Magic Sushi Lora</h3>
         <div className={styles.contentFooterLinks}>
-          <Link className={styles.contentFooterLink} to="">About us</Link>
-          <Link className={styles.contentFooterLink} to="">Recipes</Link>
-          <Link className={styles.contentFooterLink} to="">Menu</Link>
-          <Link className={styles.contentFooterLink} to="">Contacts </Link>
-          <Link className={styles.contentFooterLink} to="">Profile</Link>
+          <Link className={styles.contentFooterLink} to="">
+            About us
+          </Link>
+          <Link className={styles.contentFooterLink} to="">
+            Recipes
+          </Link>
+          <Link className={styles.contentFooterLink} to="">
+            Menu
+          </Link>
+          <Link className={styles.contentFooterLink} to="">
+            Contacts
+          </Link>
+          <Link className={styles.contentFooterLink} to="">
+            Profile
+          </Link>
         </div>
         <div className={styles.contentFooterIcons}>
           <Link className={styles.footerIcon} to="">
--- MagicSushiLora/MagicSushiLoraReact/src/main.jsx
+++ MagicSushiLora/MagicSushiLoraReact/src/main.jsx
@@ -2,11 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
       <App />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </React.StrictMode>
   </BrowserRouter>
 );
