5f0fd036d1032132848e3a6eac5b5bca968584e8
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-01-31 14:46:46 +0200

    Update navigation

--- MagicSushiLora/MagicSushiLoraReact/src/components/header/navigation-welcome/NavigationWelcome.jsx
+++ MagicSushiLora/MagicSushiLoraReact/src/components/header/navigation-welcome/NavigationWelcome.jsx
@@ -1,37 +1,95 @@
 import { Link } from "react-router-dom";
 import styles from "./NavigationWelcome.module.css";
 import { Logout } from "../../logout/Logout";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {} from "@fortawesome/free-brands-svg-icons";
+import {
+  faBars,
+  faTimes,
+  faHouse,
+  faCaretDown,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 export const NavigationWelcome = () => {
+  const [isDropdownVisible, setDropdownVisibility] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisibility(!isDropdownVisible);
+  };
+
   return (
-    <nav className={styles.navigationWelcome}>
-      <Link to="" className={styles.sushiLogoWelcome}>
-        Logo
-      </Link>
-      <ul role="list" className={styles.navBarWelcome}>
-        <li>
-          <Link className={styles.linkWelcome} to="">
-            Home
-          </Link>
-        </li>
-        <li>
+    <>
+      <nav className={styles.navigationWelcome}>
+        <Link to="" className={styles.sushiLogoWelcome}>
+          Logo
+        </Link>
+        <div className={styles.navBarWelcome}>
           <Link className={styles.linkWelcome} to="">
             Recipes
           </Link>
-        </li>
-        <li>
-          <Link className={styles.linkWelcome} to="">
-            Contacts
-          </Link>
-        </li>
-        <li>
-          <Link className={styles.linkWelcome} to="">
-            Profile
-          </Link>
-        </li>
-        <li>
+        </div>
+        <Link className={styles.linkWelcome} to="">
+          <FontAwesomeIcon icon={faUser} />
+          <FontAwesomeIcon icon={faCaretDown} />
           <Logout />
-        </li>
-      </ul>
-    </nav>
+        </Link>
+      </nav>
+
+      <nav className={styles.navigationWelcomeMobile}>
+        <div className={styles.contentDropDownButton}>
+          <button className={styles.dropdownButton} onClick={toggleDropdown}>
+            <FontAwesomeIcon
+              className={styles.dropdownButtonIcon}
+              icon={faBars}
+            />
+          </button>
+          {isDropdownVisible && (
+            <div className={styles.dropdown}>
+              <div className={styles.contentOnCloseButton}>
+                <button
+                  className={styles.onCloseButton}
+                  onClick={toggleDropdown}
+                >
+                  <FontAwesomeIcon icon={faTimes} />
+                </button>
+              </div>
+
+              <Link
+                onClick={toggleDropdown}
+                className={styles.downLinkWelcome}
+                to="/"
+              >
+                <FontAwesomeIcon className={styles.iconHome} icon={faHouse} />
+              </Link>
+
+              <Link
+                onClick={toggleDropdown}
+                className={styles.downLinkWelcome}
+                to=""
+              >
+                Recipes
+              </Link>
+
+              <Link
+                onClick={toggleDropdown}
+                className={styles.downLinkWelcome}
+                to=""
+              >
+                Profile
+              </Link>
+
+              <Logout
+                className={styles.downLinkWelcome}
+                onClick={toggleDropdown}
+              />
+            </div>
+          )}
+        </div>
+        <Link to="" className={styles.sushiLogoWelcome}>
+          Logo
+        </Link>
+      </nav>
+    </>
   );
 };
