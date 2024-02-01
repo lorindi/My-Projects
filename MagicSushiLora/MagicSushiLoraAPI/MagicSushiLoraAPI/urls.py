50ea1c3916ab379cd0a7826093d4344a76c38d89
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-02-01 23:15:35 +0200

    Updating main urls

--- MagicSushiLora/MagicSushiLoraAPI/MagicSushiLoraAPI/urls.py
+++ MagicSushiLora/MagicSushiLoraAPI/MagicSushiLoraAPI/urls.py
@@ -5,6 +5,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include([
         path('auth/', include('users.urls')),
-        path('sushi/', include('sushi.urls')),
+        path('blog/', include('blog.urls')),
+        path('kinds/', include('kinds.urls')),
+        path('recipes/', include('recipes.urls')),
+        path('courses/', include('courses.urls')),
+        path('shop/', include('shop.urls')),
+        path('cart/', include('cart.urls')),
+        path('payment/', include('payment.urls')),
     ])),
 ]
