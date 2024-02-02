35990552528febb25eaef512c61ce550a586f1af
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-02-02 20:01:41 +0200

    Add animation to any picture

--- MagicSushiLora/MagicSushiLoraReact/src/components/pages/home/Home.jsx
+++ MagicSushiLora/MagicSushiLoraReact/src/components/pages/home/Home.jsx
@@ -2,20 +2,30 @@ import styles from "./Home.module.css";
 import { Link } from "react-router-dom";
 import leftImg from "../home/left.png";
 import rightImg from "../home/right.png";
+
+import leftMobile from "../home/leftMobile.png";
+import rightMobile from "../home/rightMobile.png";
+
 import sushirightImg from "../home/pngwing.com (8).png";
 import sushileftImg from "../home/leftImg.png";
 export const Home = () => {
   return (
     <div className={styles.containerHomePage}>
       <section className={styles.sectionCover}>
-        <div className={styles.contentCoverImageMobile}>
-          <img className={styles.coverImageMobile} src={leftImg} alt="" />
-          <img className={styles.additionalImageMobile} src={leftImg} alt="" />
 
+        <div className={styles.contentCoverImageMobileLeft}>
+          <img className={styles.coverImageMobile} src={leftImg} alt="" />
+          <img
+            className={styles.additionalImageMobileLeft}
+            src={leftMobile}
+            alt=""
+          />
         </div>
+
         <div className={styles.contentCoverImage}>
           <img className={styles.coverImageLeft} src={sushileftImg} alt="" />
         </div>
+
         <div className={styles.contentCoverInfo}>
           <h1 className={styles.coverTitle}>Discover the magic of sushi</h1>
           <p className={styles.coverDescription}>
@@ -25,14 +35,20 @@ export const Home = () => {
           </p>
           <Link className={styles.coverButton}>Get Started</Link>
         </div>
+
         <div className={styles.contentCoverImage}>
           <img className={styles.coverImageRight} src={sushirightImg} alt="" />
         </div>
-        <div className={styles.contentCoverImageMobile}>
-          <img className={styles.coverImageMobile} src={rightImg} alt="" />
-          <img className={styles.additionalImageMobile} src={rightImg} alt="" />
 
+        <div className={styles.contentCoverImageMobileRight}>
+          <img className={styles.coverImageMobile} src={rightImg} alt="" />
+          <img
+            className={styles.additionalImageMobileRight}
+            src={rightMobile}
+            alt=""
+          />
         </div>
+
       </section>
     </div>
   );
