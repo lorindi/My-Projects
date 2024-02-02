fe31de40a6ec1a6fd9e3fb4d0c8a04c8b780bae3
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-02-02 19:34:24 +0200

    Adding a section to the home component

--- MagicSushiLora/MagicSushiLoraReact/src/components/pages/home/Home.jsx
+++ MagicSushiLora/MagicSushiLoraReact/src/components/pages/home/Home.jsx
@@ -1,10 +1,21 @@
-import styles from './Home.module.css'
-// import { Link } from "react-router-dom";
-// import infoImg from "./pngwing.com (8).png";
+import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
+import leftImg from "../home/left.png";
+import rightImg from "../home/right.png";
+import sushirightImg from "../home/pngwing.com (8).png";
+import sushileftImg from "../home/leftImg.png";
 export const Home = () => {
   return (
     <div className={styles.containerHomePage}>
-      {/* <section className={styles.sectionCover}>
+      <section className={styles.sectionCover}>
+        <div className={styles.contentCoverImageMobile}>
+          <img className={styles.coverImageMobile} src={leftImg} alt="" />
+          <img className={styles.additionalImageMobile} src={leftImg} alt="" />
+
+        </div>
+        <div className={styles.contentCoverImage}>
+          <img className={styles.coverImageLeft} src={sushileftImg} alt="" />
+        </div>
         <div className={styles.contentCoverInfo}>
           <h1 className={styles.coverTitle}>Discover the magic of sushi</h1>
           <p className={styles.coverDescription}>
@@ -15,9 +26,14 @@ export const Home = () => {
           <Link className={styles.coverButton}>Get Started</Link>
         </div>
         <div className={styles.contentCoverImage}>
-          <img className={styles.coverImage} src={infoImg} alt="" />
+          <img className={styles.coverImageRight} src={sushirightImg} alt="" />
         </div>
-      </section> */}
+        <div className={styles.contentCoverImageMobile}>
+          <img className={styles.coverImageMobile} src={rightImg} alt="" />
+          <img className={styles.additionalImageMobile} src={rightImg} alt="" />
+
+        </div>
+      </section>
     </div>
   );
 };
