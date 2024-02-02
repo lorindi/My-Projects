48de06db2f8577fe50caec133b9b50dac2ca3a1f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-02-02 13:43:53 +0200

    Creating an editable path for a user

--- MagicSushiLora/MagicSushiLoraAPI/users/urls.py
+++ MagicSushiLora/MagicSushiLoraAPI/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from users.views import RegisterApiView, LoginApiView, LogoutApiView
+from users.views import RegisterApiView, LoginApiView, LogoutApiView, UpdateUserApiView
 
 urlpatterns = [
     path('register/', RegisterApiView.as_view(), name='register'),
     path('login/', LoginApiView.as_view(), name='login'),
     path('logout/', LogoutApiView.as_view(), name='logout'),
+    path('<int:pk>/update/', UpdateUserApiView.as_view(), name='update'),
 ]
