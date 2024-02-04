308aa4b61604c640d86410a4781f11e1a718f4d9
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-02-04 22:23:26 +0200

    Adding styling for the user edit page

--- MagicSushiLora/MagicSushiLoraReact/src/components/users/edit/EditUser.jsx
+++ MagicSushiLora/MagicSushiLoraReact/src/components/users/edit/EditUser.jsx
@@ -1,33 +1,35 @@
-import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { useContext } from "react";
 import { Contexts } from "../../../contexts/Contexts";
+import styles from "./EditUser.module.css";
 export const EditUser = () => {
-  const { axiosInstance} = useContext(Contexts);
+  const { axiosInstance } = useContext(Contexts);
   const { id } = useParams();
   const [userData, setUserData] = useState({
-    username: '',
-    email: '',
-    first_name: '',
-    last_name: '',
-    description: '',
-    profile_picture: '',
+    username: "",
+    email: "",
+    first_name: "",
+    last_name: "",
+    description: "",
+    profile_picture: "",
   });
 
   useEffect(() => {
-    axiosInstance.get(`http://127.0.0.1:8000/api/auth/${id}/update/`)
-      .then(response => {
+    axiosInstance
+      .get(`http://127.0.0.1:8000/api/auth/${id}/update/`)
+      .then((response) => {
         console.log(response.data);
         setUserData(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching user data:', error);
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
       });
   }, [id]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setUserData(prevData => ({
+    setUserData((prevData) => ({
       ...prevData,
       [name]: value,
     }));
@@ -37,78 +39,79 @@ export const EditUser = () => {
     e.preventDefault();
 
     // Send a POST request with the updated user data
-    axiosInstance.post(`http://127.0.0.1:8000/api/auth/${id}/update/`, userData)
-      .then(response => {
-        console.log('User data updated successfully:', response.data);
+    axiosInstance
+      .post(`http://127.0.0.1:8000/api/auth/${id}/update/`, userData)
+      .then((response) => {
+        console.log("User data updated successfully:", response.data);
         // Optionally, you can redirect the user or perform other actions upon successful update
       })
-      .catch(error => {
-        console.error('Error updating user data:', error);
+      .catch((error) => {
+        console.error("Error updating user data:", error);
       });
   };
 
   return (
-    <div>
-      <h1>Edit your profile</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <input
-            name="username"
-            placeholder="Username"
-            type="text"
-            value={userData.username}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <input
-            name="email"
-            placeholder="Email"
-            type="text"
-            value={userData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <input
-            name="first_name"
-            placeholder="First name"
-            type="text"
-            value={userData.first_name}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <input
-            name="last_name"
-            placeholder="Last name"
-            type="text"
-            value={userData.last_name}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <input
-            name="description"
-            placeholder="Description"
-            type="text"
-            value={userData.description}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <input
-            name="profile_picture"
-            placeholder="Profile picture"
-            type='text'
-            value={userData.profile_picture}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <button type="submit">Save Changes</button>
-        </div>
-      </form>
-    </div>
+    <form className={styles.containerEditUserForm} onSubmit={handleSubmit}>
+      <input
+        className={styles.editUserFormInput}
+        name="username"
+        placeholder="Username"
+        type="text"
+        value={userData.username}
+        onChange={handleChange}
+      />
+
+      <input
+        className={styles.editUserFormInput}
+        name="email"
+        placeholder="Email"
+        type="text"
+        value={userData.email}
+        onChange={handleChange}
+      />
+
+      <input
+        className={styles.editUserFormInput}
+        name="first_name"
+        placeholder="First name"
+        type="text"
+        value={userData.first_name}
+        onChange={handleChange}
+      />
+
+      <input
+        className={styles.editUserFormInput}
+        name="last_name"
+        placeholder="Last name"
+        type="text"
+        value={userData.last_name}
+        onChange={handleChange}
+      />
+
+      <input
+        className={styles.editUserFormInput}
+        name="description"
+        placeholder="Description"
+        type="text"
+        value={userData.description}
+        onChange={handleChange}
+      />
+
+      <input
+        className={styles.editUserFormInput}
+        name="profile_picture"
+        placeholder="Profile picture"
+        type="text"
+        value={userData.profile_picture}
+        onChange={handleChange}
+      />
+
+      <div className={styles.contentEditUserFormButton}>
+        <button className={styles.editUserFormButton} type="submit">
+          Save
+        </button>
+       
+      </div>
+    </form>
   );
 };
