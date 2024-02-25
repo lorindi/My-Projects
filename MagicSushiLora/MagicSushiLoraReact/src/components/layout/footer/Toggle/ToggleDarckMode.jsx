1f45ede265d6d1cf81612c3e2c14fed7c9ac1b0e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-02-25 16:46:19 +0200

    Update styles  in Toggle component

--- MagicSushiLora/MagicSushiLoraReact/src/components/layout/footer/Toggle/ToggleDarckMode.jsx
+++ MagicSushiLora/MagicSushiLoraReact/src/components/layout/footer/Toggle/ToggleDarckMode.jsx
@@ -1,26 +1,42 @@
 import styles from "./ToggleDarkMode.module.css";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Contexts } from "../../../../contexts/Contexts";
 export const ToggleDarkMode = () => {
-  const { isDarkMode, toggleDarkMode } = useContext(Contexts);
+  // const { isDarkMode, toggleDarkMode } = useContext(Contexts);
+  const { theme, toggleTheme } = useContext(Contexts);
 
-  const handleCheckboxChange = () => {
-    toggleDarkMode(); 
+  // const handleCheckboxChange = () => {
+  //   toggleDarkMode();
+  // };
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const handleThemeToggle = () => {
+    toggleTheme();
   };
-
   return (
-
-
-        <input
-          id="theme"
-          className={styles.themeToggle}
-          type="checkbox"
-          role="switch"
-          name="theme"
-          value="dark"
-          checked={isDarkMode}
-          onChange={handleCheckboxChange}
-        />
-
+    // <input
+    //   id="theme"
+    //   className={styles.themeToggle}
+    //   type="checkbox"
+    //   role="switch"
+    //   name="theme"
+    //   value="dark"
+    //   // checked={isDarkMode}
+    //   // onChange={handleCheckboxChange}
+    // />
+    <input
+        id="theme"
+        className={styles.themeToggle}
+        type="checkbox"
+        role="switch"
+        name="theme"
+        value={theme === 'dark' ? 'dark' : 'light'}
+        checked={theme === 'dark'}
+        onChange={handleThemeToggle}
+      />
   );
 };
+
+
