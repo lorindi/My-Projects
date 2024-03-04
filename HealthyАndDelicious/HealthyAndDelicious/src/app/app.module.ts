0692b00c1ecfb310bbd1cca036572a34ccd7ae6b
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-04 10:26:46 +0200

    Аdding a header component

--- HealthyАndDelicious/HealthyAndDelicious/src/app/app.module.ts
+++ HealthyАndDelicious/HealthyAndDelicious/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreComponent } from './core/core.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoreComponent
+    CoreComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
