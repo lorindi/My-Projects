1b104fabe7fa73313ea1e4bf5b5c851e44b35e2c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-04 22:48:34 +0200

    Create shared mudule

---
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoaderComponent } from './loader/loader.component';
+
+
+
+@NgModule({
+  declarations: [
+    LoaderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
