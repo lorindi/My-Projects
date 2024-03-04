4b64411b17df869373e62752f02994fa2eb86abd
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-04 22:49:58 +0200

    Update app module

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/app.module.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreComponent } from './core/core.component';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
+
 import { SharedComponent } from './shared/shared.component';
 import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoreComponent,
-    HeaderComponent,
-    FooterComponent,
+
     SharedComponent,
     MainComponent
   ],
