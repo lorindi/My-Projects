24f079d33ccff0f012b404afce532f4efb4799c4
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-04 21:42:32 +0200

    Add files via upload

--- HealthyAndDelicious/HealthyAndDeliciousDjangoRestApi/HealthyAndDeliciousDjangoApi/HealthyAndDeliciousDjangoApi/urls.py
+++ HealthyAndDelicious/HealthyAndDeliciousDjangoRestApi/HealthyAndDeliciousDjangoApi/HealthyAndDeliciousDjangoApi/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for djangoProject project.
+URL configuration for HealthyAndDeliciousDjangoApi project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
