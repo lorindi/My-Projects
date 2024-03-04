4dcd65f242bf6d1997e6c3c811943fdaae61613b
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-04 22:25:18 +0200

    Creating a core component

---
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/core/core.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-core',
+  templateUrl: './core.component.html',
+  styleUrls: ['./core.component.css']
+})
+export class CoreComponent {
+
+}
