960bfd2876d6b547926e9a7510359c3b57538344
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-11 16:52:05 +0200

    Update What component

--- AssociationCysticFibrosisBulgaria65Roses/AssociationCysticFibrosisBulgaria65RosesReact/src/components/pages/home/what/What.jsx
+++ AssociationCysticFibrosisBulgaria65Roses/AssociationCysticFibrosisBulgaria65RosesReact/src/components/pages/home/what/What.jsx
@@ -1,109 +1,109 @@
 import styles from './What.module.css'
 export const What = () => {
   return (
-    <section className="cystic-fibrosis-section">
-      <button className="cystic-fibrosis-title">CYSTIC FIBROSIS?</button>
-      <div className="cystic-fibrosis-dropdown-content">
-        <div className="cystic-fibrosis-details">
-          <div className="cystic-fibrosis-what">
-            <h2 className="cystic-fibrosis-subtitle">WHAT</h2>
+    <section className={styles.cysticFibrosisSection}>
+      <button className={styles.cysticFibrosisTitle}>CYSTIC FIBROSIS?</button>
+      <div className={styles.cysticFibrosisDropdownContent}>
+        <div className={styles.cysticFibrosisDetails}>
+          <div className={styles.cysticFibrosisWhat}>
+            <h2 className={styles.cysticFibrosisSubtitle}>WHAT</h2>
 
             <div>
-              <p className="cystic-fibrosis-description">
+              <p className={styles.cysticFibrosisDescription}>
                 AN AUTOSOMAL RECESSIVE GENETIC DISORDER{" "}
               </p>
-              <p className="cystic-fibrosis-description">
+              <p className={styles.cysticFibrosisDescription}>
                 THAT INVOLVES ABNORMAL TRANSPORT OF
               </p>
-              <p className="cystic-fibrosis-description">
+              <p className={styles.cysticFibrosisDescription}>
                 CHLORIDE AND SODIUM ACROSS THE CELL
               </p>
-              <p className="cystic-fibrosis-description">
+              <p className={styles.cysticFibrosisDescription}>
                 MEMBRANE CAUSING A THICK MUCUS
               </p>
-              <p className="cystic-fibrosis-description">TO BUILD UP</p>
+              <p className={styles.cysticFibrosisDescription}>TO BUILD UP</p>
             </div>
           </div>
-          <div className="cystic-fibrosis-affects">
-            <h2 className="cystic-fibrosis-subtitle">AFFECTS</h2>
-            <div className="cystic-fibrosis-organs">
-              <div className="cystic-fibrosis-organ">
+          <div className={styles.cysticFibrosisAffects}>
+            <h2 className={styles.cysticFibrosisSubtitle}>AFFECTS</h2>
+            <div className={styles.cysticFibrosisOrgans}>
+              <div className={styles.cysticFibrosisOrgan}>
                 <img
-                  className="cystic-fibrosis-organ-lungs-image"
+                  className={styles.cysticFibrosisOrganLungsImage}
                   src="{% static 'images/organs/pngwing.com (7).png' %}"
                   alt=""
                 />
-                <h3 className="cystic-fibrosis-organ-title">LUNGS</h3>
+                <h3 className={styles.cysticFibrosisOrganTitle}>LUNGS</h3>
               </div>
-              <div className="cystic-fibrosis-organ">
+              <div className={styles.cysticFibrosisOrgan}>
                 <img
-                  className="cystic-fibrosis-organ-pancreas-image"
+                  className={styles.cysticFibrosisOrganPancreasImage}
                   src="{% static 'images/organs/pngwing.com (9).png' %}"
                   alt=""
                 />
-                <h3 className="cystic-fibrosis-organ-title">PANCREAS</h3>
+                <h3 className={styles.cysticFibrosisOrganTitle}>PANCREAS</h3>
               </div>
-              <div className="cystic-fibrosis-organ">
+              <div className={styles.cysticFibrosisOrgan}>
                 <img
-                  className="cystic-fibrosis-organ-liver-image"
+                  className={styles.cysticFibrosisOrganLiverImage}
                   src="{% static 'images/organs/pngwing.com (4).png' %}"
                   alt=""
                 />
-                <h3 className="cystic-fibrosis-organ-title">LIVER</h3>
+                <h3 className={styles.cysticFibrosisOrganTitle}>LIVER</h3>
               </div>
-              <div className="cystic-fibrosis-organ">
+              <div className={styles.cysticFibrosisOrgan}>
                 <img
-                  className="cystic-fibrosis-organ-intestine-image"
+                  className={styles.cysticFibrosisOrganIntestineImage}
                   src="{% static 'images/organs/pngwing.com (8).png' %}"
                   alt=""
                 />
-                <h3 className="cystic-fibrosis-organ-title">INTESTINE</h3>
+                <h3 className={styles.cysticFibrosisOrganTitle}>INTESTINE</h3>
               </div>
             </div>
           </div>
         </div>
-        <div className="cystic-fibrosis-symptoms">
-          <div className="cystic-fibrosis-symptoms-info">
-            <h2 className="cystic-fibrosis-subtitle">SYMPTOMS</h2>
+        <div className={styles.cysticFibrosisSymptoms}>
+          <div className={styles.cysticFibrosisSymptomsInfo}>
+            <h2 className={styles.cysticFibrosisSubtitle}>SYMPTOMS</h2>
 
-            <ul className="cystic-fibrosis-symptoms-list" role="list">
-              <li className="cystic-fibrosis-symptom">DIFFICULTY BREATHING</li>
-              <li className="cystic-fibrosis-symptom">PERSISTENT COUGH</li>
-              <li className="cystic-fibrosis-symptom">POOR GROWTH</li>
-              <li className="cystic-fibrosis-symptom">LUNG AND SINUS</li>
-              <li className="cystic-fibrosis-symptom">INFERTILITY</li>
-              <li className="cystic-fibrosis-symptom">BOWEL ISSUES</li>
-              <li className="cystic-fibrosis-symptom">DIABETES</li>
+            <ul className={styles.cysticFibrosisSymptomsList} role="list">
+              <li className={styles.cysticFibrosisSymptom}>DIFFICULTY BREATHING</li>
+              <li className={styles.cysticFibrosisSymptom}>PERSISTENT COUGH</li>
+              <li className={styles.cysticFibrosisSymptom}>POOR GROWTH</li>
+              <li className={styles.cysticFibrosisSymptom}>LUNG AND SINUS</li>
+              <li className={styles.cysticFibrosisSymptom}>INFERTILITY</li>
+              <li className={styles.cysticFibrosisSymptom}>BOWEL ISSUES</li>
+              <li className={styles.cysticFibrosisSymptom}>DIABETES</li>
             </ul>
           </div>
-          <div className="cystic-fibrosis-cause-info">
-            <h2 className="cystic-fibrosis-subtitle">CAUSED BY</h2>
-            <p className="cystic-fibrosis-cause">
+          <div className={styles.cysticFibrosisCauseInfo}>
+            <h2 className={styles.cysticFibrosisSubtitle}>CAUSED BY</h2>
+            <p className={styles.cysticFibrosisCause}>
               MUTATIONS IN THE GENE ENCODING FOR THE CFTR PROTEIN
             </p>
           </div>
         </div>
-        <div className="cystic-fibrosis-facts">
-          <div className="cystic-fibrosis-facts-info">
-            <h2 className="cystic-fibrosis-subtitle">DID YOU KNOW?</h2>
-            <ul className="cystic-fibrosis-facts-list">
-              <li className="cystic-fibrosis-fact">
+        <div className={styles.cysticFibrosisFacts}>
+          <div className={styles.cysticFibrosisFactsInfo}>
+            <h2 className={styles.cysticFibrosisSubtitle}>DID YOU KNOW?</h2>
+            <ul className={styles.cysticFibrosisFactsList}>
+              <li className={styles.cysticFibrosisFact}>
                 DIFFERENT MUTATIONS IN THE CTFR GENE CAN CAUSE DIFFERENT
                 SYMPTOMS
               </li>
-              <li className="cystic-fibrosis-fact">
+              <li className={styles.cysticFibrosisFact}>
                 THANKS TO GENETIC SCREENING AT BIRTH, MOST CHILDREN ARE
                 DIAGNOSED BEFORE THE AGE OF TWO
               </li>
-              <li className="cystic-fibrosis-fact">
+              <li className={styles.cysticFibrosisFact}>
                 ADVANCED WITH CF WILL GO ON TO LIVE FULFILLING LIVES, INTO THEIR
                 LATE 305 AND BEYOND
               </li>
             </ul>
           </div>
-          <div className="cystic-fibrosis-cftr-info">
-            <h2 className="cystic-fibrosis-subtitle">WHAT is CFTR ?</h2>
-            <p className="cystic-fibrosis-cftr">
+          <div className={styles.cysticFibrosisCftrInfo}>
+            <h2 className={styles.cysticFibrosisSubtitle}>WHAT is CFTR ?</h2>
+            <p className={styles.cysticFibrosisCftr}>
               Regulates sweat, digestive fluids, and mucus Controls the movement
               of chloride and sodium through cell membranes.
             </p>
