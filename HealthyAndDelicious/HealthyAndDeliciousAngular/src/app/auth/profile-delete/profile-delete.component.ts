8d7d03390b761c6ac1275e987b0cc84e9d2cae54
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-12 17:53:57 +0200

    Create profile-delete.component.ts

---
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/auth/profile-delete/profile-delete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-delete',
+  templateUrl: './profile-delete.component.html',
+  styleUrls: ['./profile-delete.component.css']
+})
+export class ProfileDeleteComponent {
+
+}
