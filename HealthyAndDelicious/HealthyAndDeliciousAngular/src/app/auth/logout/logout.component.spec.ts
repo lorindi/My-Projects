36f015684d0b03a5ace8d58377556d70fa8d3d68
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-12 17:54:08 +0200

    Create logout.component.spec.ts

---
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/auth/logout/logout.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogoutComponent } from './logout.component';
+
+describe('LogoutComponent', () => {
+  let component: LogoutComponent;
+  let fixture: ComponentFixture<LogoutComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [LogoutComponent]
+    });
+    fixture = TestBed.createComponent(LogoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
