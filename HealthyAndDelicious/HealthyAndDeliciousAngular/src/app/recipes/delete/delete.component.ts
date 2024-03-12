dc8b7d73b74e8c0e753e46832349db5b2848708e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-12 17:53:13 +0200

    Create delete.component.ts

---
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/recipes/delete/delete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete',
+  templateUrl: './delete.component.html',
+  styleUrls: ['./delete.component.css']
+})
+export class DeleteComponent {
+
+}
