c67426659045de21105f4a4d5a2c5a6657bc7400
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-12 17:53:47 +0200

    Create profile-edit.component.ts

---
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/auth/profile-edit/profile-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-edit',
+  templateUrl: './profile-edit.component.html',
+  styleUrls: ['./profile-edit.component.css']
+})
+export class ProfileEditComponent {
+
+}
