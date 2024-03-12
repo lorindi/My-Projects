bf2a209d9d8e5b8d6787a04722997227f85e70f2
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-12 17:50:46 +0200

    Create contacts.component.spec.ts

---
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/pages/contacts/contacts.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ContactsComponent } from './contacts.component';
+
+describe('ContactsComponent', () => {
+  let component: ContactsComponent;
+  let fixture: ComponentFixture<ContactsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ContactsComponent]
+    });
+    fixture = TestBed.createComponent(ContactsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
