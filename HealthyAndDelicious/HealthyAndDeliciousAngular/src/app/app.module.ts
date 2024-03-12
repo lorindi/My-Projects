5fb3809fab79982c302d13d54b3e878af94c1d20
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-12 17:50:08 +0200

    Update app.module.ts

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/app.module.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/app.module.ts
@@ -5,13 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
-import { MainComponent } from './main/main.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
   ],
   imports: [
     BrowserModule,
