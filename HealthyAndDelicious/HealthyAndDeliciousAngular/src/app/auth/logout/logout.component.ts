813b74a79b303148008d30660ae27e31a640d4ce
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-12 17:54:06 +0200

    Create logout.component.ts

---
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/auth/logout/logout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logout',
+  templateUrl: './logout.component.html',
+  styleUrls: ['./logout.component.css']
+})
+export class LogoutComponent {
+
+}
