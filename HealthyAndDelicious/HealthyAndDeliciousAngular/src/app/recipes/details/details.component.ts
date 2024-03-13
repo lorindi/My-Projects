e5ac4419c28ceff01e9c0b85826e1f88188d02a8
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-13 16:47:18 +0200

    Create details.component.ts

---
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/recipes/details/details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent {
+
+}
