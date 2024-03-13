d2c89a186bfb4c8ce704a2d8e7e8a0c4b46d34d5
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-13 09:52:30 +0200

    Update app-routing.module.ts

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/app-routing.module.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/app-routing.module.ts
@@ -1,10 +1,73 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ProfileComponent } from './auth/profile/profile.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { RecipesModule } from './recipes/recipes.module';
+import { DashboardComponent } from './recipes/dashboard/dashboard.component';
+import { CreateComponent } from './recipes/create/create.component';
+import { EditComponent } from './recipes/edit/edit.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    // canActivate: [AuthGuard],
+    // data: {
+    //   guest: true,
+    // },
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    // canActivate: [AuthGuard],
+    // data: {
+    //   guest: true,
+    // },
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+
+  },
+  {
+    path: 'recipes',
+    component: DashboardComponent,
+
+  },
+  {
+    path: 'create-recipe',
+    component: CreateComponent,
+
+  },
+  {
+    path: 'edit-recipe',
+    component: EditComponent,
+
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
