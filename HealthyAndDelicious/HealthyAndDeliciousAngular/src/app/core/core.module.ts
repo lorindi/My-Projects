36cf01ad6e34ed6481710e2450631550674e7284
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-13 09:52:38 +0200

    Update core.module.ts

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/core/core.module.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/core/core.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    CommonModule, MatIconModule, 
-  ],
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [RouterModule, CommonModule, MatIconModule],
   exports: [HeaderComponent, FooterComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
