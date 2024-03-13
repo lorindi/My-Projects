41e60420acc031885e891ee665258351b2f03895
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-13 16:47:40 +0200

    Update app-routing.module.ts

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/app-routing.module.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/app-routing.module.ts
@@ -6,12 +6,11 @@ import { LoginComponent } from './auth/login/login.component';
 import { ProfileComponent } from './auth/profile/profile.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { ContactsComponent } from './pages/contacts/contacts.component';
-import { RecipesModule } from './recipes/recipes.module';
 import { DashboardComponent } from './recipes/dashboard/dashboard.component';
 import { CreateComponent } from './recipes/create/create.component';
 import { EditComponent } from './recipes/edit/edit.component';
 import { ErrorComponent } from './pages/error/error.component';
-
+import { DetailsComponent } from './recipes/details/details.component';
 
 const routes: Routes = [
   {
@@ -38,39 +37,39 @@ const routes: Routes = [
   {
     path: 'about-us',
     component: AboutUsComponent,
-
   },
   {
     path: 'contacts',
     component: ContactsComponent,
-
   },
   {
     path: 'profile',
     component: ProfileComponent,
-
   },
   {
     path: 'recipes',
     component: DashboardComponent,
-
   },
   {
     path: 'create-recipe',
     component: CreateComponent,
-
   },
   {
     path: 'edit-recipe',
     component: EditComponent,
-
+  },
+  {
+    path: 'recipe/:id',
+    component: DetailsComponent,
   },
   { path: '**', redirectTo: '/404' },
   { path: '404', component: ErrorComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
