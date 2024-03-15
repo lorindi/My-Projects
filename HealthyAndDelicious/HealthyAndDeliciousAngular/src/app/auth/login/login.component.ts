03a9dd465faa097e179ce2d90e5357de315b5c03
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-15 15:20:42 +0200

    Update login.component.ts

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/auth/login/login.component.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/auth/login/login.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(private router: Router, private authService: AuthService) {}
 
+  login(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.authService.login();
+    this.router.navigate(['/home']);
+  }
 }
