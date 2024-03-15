0fbffc5a5db335b4da262bb929edca5b1cc61725
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-15 21:52:50 +0200

    Update urls.py

--- HealthyAndDelicious/HealthyAndDeliciousDjangoRest/users/urls.py
+++ HealthyAndDelicious/HealthyAndDeliciousDjangoRest/users/urls.py
@@ -1,1 +1,11 @@
-urlpatterns = []
+from django.urls import path
+
+from users.views import RegisterApiView, LoginApiView, LogoutApiView, UpdateUserApiView, ProfileDetailsAPIView
+
+urlpatterns = [
+    path('register/', RegisterApiView.as_view(), name='register'),
+    path('login/', LoginApiView.as_view(), name='login'),
+    path('logout/', LogoutApiView.as_view(), name='logout'),
+    path('update/<int:pk>/', UpdateUserApiView.as_view(), name='update'),
+    path('details/<int:pk>/', ProfileDetailsAPIView.as_view(), name='details'),
+]
