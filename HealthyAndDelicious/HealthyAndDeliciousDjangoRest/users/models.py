3425ecde625397db487d992d8f5586eb2b483561
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-15 21:52:53 +0200

    Update models.py

--- HealthyAndDelicious/HealthyAndDeliciousDjangoRest/users/models.py
+++ HealthyAndDelicious/HealthyAndDeliciousDjangoRest/users/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+
+class RecipeUser(AbstractUser):
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    email = models.EmailField(
+        unique=True,
+        null=False,
+        blank=False,
+        # error_messages={"unique": "Email is not unique."}
+    )
+    username = models.CharField(
+        max_length=255,
+        null=False,
+        blank=False,
+        unique=True,
+        # error_messages={"unique": "Username is not unique."}
+    )
+
+    first_name = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True,
+    )
+    last_name = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True,
+    )
+    description = models.TextField(
+        validators=[MinLengthValidator(5)],
+        null=True,
+        blank=True,
+    )
+    profile_picture = models.URLField(
+        null=True,
+        blank=True,
+    )
+
+    def __str__(self):
+        return self.username
