7058288911e313e787a5cded634846029503ff22
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-16 16:43:39 +0200

    Update register.component.ts

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/auth/register/register.component.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/auth/register/register.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { EMAIL_DOMAINS } from 'src/app/constants';
+import { emailValidator } from 'src/app/shared/utils/email-validator';
+import { matchPasswordsValidator } from 'src/app/shared/utils/match-passwords.validator';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {
 
+export class RegisterComponent {
+  domains = EMAIL_DOMAINS;
+  form = this.fb.group({
+    //controls
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, emailValidator(EMAIL_DOMAINS)]],
+    passGroup: this.fb.group(
+      {
+        password: ['', Validators.required],
+        rePassword: ['', Validators.required],
+      },
+      { validators: [matchPasswordsValidator('password', 'rePassword')] }
+    ),
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  register(): void {
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(this.form.value);
+  }
 }
