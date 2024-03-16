8d52f9008133587d0fec8ecb62f7c9cc035b34a2
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-16 16:43:30 +0200

    Update create.component.ts

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/recipes/create/create.component.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/recipes/create/create.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
 })
 export class CreateComponent {
-
+  constructor(private apiService: ApiService) {}
+  addRecipe(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    console.log(form.value);
+    
+  }
 }
