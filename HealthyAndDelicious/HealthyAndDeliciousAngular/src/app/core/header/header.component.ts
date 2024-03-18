923365fc8f8ad6c5cb58f59775801f90c1482c12
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-18 14:07:27 +0200

    Update header.component.ts

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/core/header/header.component.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/core/header/header.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  get isLogged(): boolean {
+    return this.authService.isLogged;
+  }
+  
+
+  logout() {
+    this.authService.logout();
+  }
 }
