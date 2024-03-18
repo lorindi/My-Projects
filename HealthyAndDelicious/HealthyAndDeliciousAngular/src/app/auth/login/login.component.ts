721c2bf98c002953f85ca30975a576f39cb2ecb6
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-18 14:07:39 +0200

    Update login.component.ts

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/auth/login/login.component.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/auth/login/login.component.ts
@@ -1,20 +1,34 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from '../auth.service';
+import { AuthService } from '../../services/auth.service';
+import { HttpErrorResponse } from '@angular/common/http'; // Импортиране на HttpErrorResponse
+import { handleError } from 'src/app/error/error.Handler';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  constructor(private router: Router, private authService: AuthService) {}
 
-  login(form: NgForm) {
-    if (form.invalid) {
-      return;
-    }
-    this.authService.login();
-    this.router.navigate(['/home']);
+  isLoading: boolean = false;
+
+  constructor(private authService: AuthService, private router: Router){}
+
+  errors: string | undefined = undefined;
+
+  login(form: NgForm): void {
+    this.isLoading = true;
+    this.authService.login(form.value.username, form.value.password).subscribe({
+      next: () => {
+        this.isLoading = false;
+        this.router.navigate(['/'])
+      },
+      error: (err: HttpErrorResponse) => {
+        this.isLoading = false; 
+        this.errors = handleError(err); 
+      }
+    })
   }
 }
