c1bc6d7497cec56fe3c2bbc92f9921028e198b29
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-26 15:12:45 +0200

    Update recipes.service.ts

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/services/recipes.service.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/services/recipes.service.ts
@@ -7,7 +7,7 @@ import { IRecipe } from '../types/recipe';
 @Injectable({
   providedIn: 'root',
 })
-export class ApiService {
+export class RecipesService {
   constructor(private http: HttpClient) {}
 
   //   THEMES
