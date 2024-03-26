abc39c37d6a40e5be8e40fe5335a7cfbe49029ea
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-26 13:10:51 +0200

    Update recipes.service.ts

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/services/recipes.service.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/services/recipes.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { IRecipe } from '../types/recipe';
+// import { Post } from './types/post';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class RecipesService {
+export class ApiService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  //   THEMES
+  getRecipes() {
+    const { apiUrl } = environment;
+    return this.http.get<IRecipe[]>(`${apiUrl}/recipe`);
+  }
+
+  getRecipe(id: string) {
+    const { apiUrl } = environment;
+    return this.http.get<IRecipe>(`${apiUrl}/recipe/${id}`);
+  }
+
+  createRecipe(recipeName: string, postText: string) {
+    return this.http.post<IRecipe>(`/api/recipe`, { recipeName, postText });
+  }
+
+  updateRecipe(RecipeId: string, postId: string, postText: string) {
+    return this.http.put<IRecipe>(`/api/recipe/${RecipeId}/posts/${postId}`, {
+      postText,
+    });
+  }
+
+  deleteRecipe(themeId: string, postId: string) {
+    return this.http.delete<IRecipe>(`/api/recipe/${themeId}/posts/${postId}`);
+  }
+
 }
