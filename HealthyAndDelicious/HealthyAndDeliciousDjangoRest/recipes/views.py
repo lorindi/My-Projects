84e977b47159caeca5124958bec52b3cd8e3391c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-26 13:52:06 +0200

    Update views.py

--- HealthyAndDelicious/HealthyAndDeliciousDjangoRest/recipes/views.py
+++ HealthyAndDelicious/HealthyAndDeliciousDjangoRest/recipes/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import Recipe
+from .serializers import RecipeSerializer
 
-# Create your views here.
+def get_user(model, **kwargs):
+    try:
+        return model.objects.get(**kwargs)
+    except model.DoesNotExist:
+        return None
+
+
+class RecipeCreateApiView(generics.CreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    def post(self, request, *args, **kwargs):
+        serializer = RecipeSerializer(data=request.data)
+        if serializer.is_valid() :
+
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request, *args, **kwargs):
+        print(request.user)
+        recipes = self.get_queryset()
+        serializer = RecipeSerializer(recipes, many=True)
+        return Response(serializer.data)
+
+class RecipeListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class RecipeRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
