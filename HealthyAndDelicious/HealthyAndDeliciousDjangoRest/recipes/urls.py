e3f35d45c0bd5eb7f3ea3567d61dfce0575ed6a6
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-26 13:52:04 +0200

    Update urls.py

--- HealthyAndDelicious/HealthyAndDeliciousDjangoRest/recipes/urls.py
+++ HealthyAndDelicious/HealthyAndDeliciousDjangoRest/recipes/urls.py
@@ -1,1 +1,4 @@
-urlpatterns = []
+urlpatterns = [
+    path('recipes/', RecipeListCreateAPIView.as_view(), name='recipe-list-create'),
+    path('recipes/<int:pk>/', RecipeRetrieveUpdateDestroyAPIView.as_view(), name='recipe-detail'),
+]
