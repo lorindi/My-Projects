347ae67e5f3b2db9d45fe46a6b1aeabb73fdc9fc
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-26 11:35:25 +0200

    Update pages.module.ts

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/pages/pages.module.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/pages/pages.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactsComponent } from './contacts/contacts.component';
-import { ErrorComponent } from './error/error.component';
+import { ErrorComponent } from './not-found/not-found.component';
 
 
 
