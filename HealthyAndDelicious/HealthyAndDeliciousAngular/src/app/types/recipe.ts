e4706dd63a4462590a6df89dcfd6436987c690d6
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-26 15:12:47 +0200

    Update recipe.ts

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/types/recipe.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/types/recipe.ts
@@ -1,1 +1,13 @@
-export interface IRecipe {id: number;}
+export interface IRecipe {
+  id: number;
+  image: string | null;
+  title: string;
+  description: string;
+  category: string;
+  ingredients: string;
+  prep_time: string;
+  cook_time: string;
+  servings: string;
+  creation_time: string;
+  creator: number;
+}
