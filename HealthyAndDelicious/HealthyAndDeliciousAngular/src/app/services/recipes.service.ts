b7d9d964f04821f84e2e327c21e16f8ad9dfec64
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-03-29 16:11:20 +0200

    Update recipes.service.ts

--- HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/services/recipes.service.ts
+++ HealthyAndDelicious/HealthyAndDeliciousAngular/src/app/services/recipes.service.ts
@@ -1,17 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipesService {
   private apiUrl = environment.apiUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   addRecipe(recipeData: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/create/`, recipeData);
+    const authToken = this.authService.getToken();
+
+    if (!authToken) {
+      throw new Error('Authentication token not found');
+    }
+
+    return this.http.post<any>(`${this.apiUrl}/recipe/create/`, recipeData, {
+      headers: { Authorization: `Token ${authToken}` },
+    });
   }
 }
