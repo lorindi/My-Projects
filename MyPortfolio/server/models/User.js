c58d7e65a8743aceb74bf5fe8d86f1a5a409db82
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-10 13:52:08 +0300

    Update User.js

--- MyPortfolio/server/models/User.js
+++ MyPortfolio/server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
   image: {
@@ -21,5 +22,11 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.pre("save", async function () {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+});
+
 const User = mongoose.model("User", userSchema);
 module.exports = User;
