7cab884ceea6cb0adc3a1d6769deac6a2bcecf78
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-10 14:00:03 +0300

    Update homeController.js

--- MyPortfolio/server/controllers/homeController.js
+++ MyPortfolio/server/controllers/homeController.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
+
 const {homeManager} = require('../managers/homeManager')
+
 router.get('/', async (req, res)=> {
     const skills = await homeManager.getAll(req.query)
     res.json(skills)
