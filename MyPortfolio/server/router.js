6d8d0ad8e52949ad7b7060c76949f1deac468215
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-10 13:52:12 +0300

    Update router.js

--- MyPortfolio/server/router.js
+++ MyPortfolio/server/router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 
 const userController = require("./controllers/userController");
+const homeController = require("./controllers/homeController");
 
 router.use(userController);
+router.use(homeController);
+
 module.exports = router;
