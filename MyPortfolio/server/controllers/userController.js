a21302fd9d8a2cfe446a7e8b04c94f3fcbfddc3d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-10 13:51:55 +0300

    Update userController.js

--- MyPortfolio/server/controllers/userController.js
+++ MyPortfolio/server/controllers/userController.js
@@ -1,10 +1,30 @@
 const router = require("express").Router();
+const userManager = require("../managers/userManager");
 
-router.post("/register", (req, res) => {
-  //
+router.post("/register", async (req, res) => {
+  try {
+    const result = await userManager.register(req.body);
+    res.json(result);
+  } catch (err) {
+    res.status(400).json({
+      message: "Some error",
+    });
+  }
 });
-router.post("/login", (req, res) => {
-    //
-  });
 
-module.exports = router
+router.post("/login", async (req, res) => {
+  try {
+    const result = await userManager.login(req.body);
+    res.json(result);
+  } catch (err) {
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  res.end();
+});
+
+module.exports = router;
