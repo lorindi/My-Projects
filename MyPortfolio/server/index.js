e62708de4613c78a75dc1cab2557e0c8e60bb752
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-11 11:05:25 +0300

    Update index.js

--- MyPortfolio/server/index.js
+++ MyPortfolio/server/index.js
@@ -14,13 +14,12 @@ mongoose
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use(cors());
-app.use(express.json());
+app.use(auth)
 
-app.get("/", (req, res) => {
-  res.send("Restful service");
-});
+// app.get("/", (req, res) => {
+//   res.send("Restful service");
+// });
 
 app.use("/portfolio", router);
 
