f6d9c66a901d39272e2d4d57ae170a3e163ca664
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-11 16:18:29 +0300

    Update App.jsx

--- MyPortfolio/client/src/App.jsx
+++ MyPortfolio/client/src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
+import "./App.css";
+import {Header} from './components/header/Header'
+import  {Footer} from './components/footer/Footer'
 
 function App() {
-
   return (
-    <>
-    
-     
-    </>
-  )
+    <div className="body">
+      <Header/>
+      <main>
+
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
