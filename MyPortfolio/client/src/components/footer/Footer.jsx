86bb8c6fc61c3322890885c42e1dd79ca8f64ca8
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-14 16:37:42 +0300

    Update Footer.jsx

--- MyPortfolio/client/src/components/footer/Footer.jsx
+++ MyPortfolio/client/src/components/footer/Footer.jsx
@@ -1,3 +1,10 @@
+import styles from "./Footer.module.css";
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <p className={styles.rightsReservedText}>© Copyright 2024</p>
+      <p className={styles.rightsReservedTextSpan}>Lora Mitova</p>
+      <p className={styles.rightsReservedText}>All rights reserved.</p>
+    </footer>
+  );
 };
