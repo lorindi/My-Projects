bc0fc90e2803683bea46585699e96d5cdff97837
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-14 16:37:31 +0300

    Update App.jsx

--- MyPortfolio/client/src/App.jsx
+++ MyPortfolio/client/src/App.jsx
@@ -1,13 +1,20 @@
 import "./App.css";
-import {Header} from './components/header/Header'
-import  {Footer} from './components/footer/Footer'
-
+import { Footer } from "./components/footer/Footer";
+import { Header } from "./components/header/Header";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/home/Home";
+import { Login } from "./components/users/login/Login";
+import { Register } from "./components/users/register/Register";
 function App() {
   return (
     <div className="body">
-      <Header/>
+      <Header />
       <main>
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </main>
       <Footer />
     </div>
