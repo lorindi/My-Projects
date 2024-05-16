fa7c0f215fc3ae7c7cccb26ae00466fbe2656038
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-16 23:35:49 +0300

    Update TicketList.jsx

--- DojoHelpdesk/dojo-helpdesk/app/tickets/TicketList.jsx
+++ DojoHelpdesk/dojo-helpdesk/app/tickets/TicketList.jsx
@@ -1,35 +1,27 @@
-async function getTickets() {
-    try {
-        const res = await fetch('http://localhost:4000/tickets', {
-            next: {
-                revalidate: 30
-            }
-        });
-        if (!res.ok) {
-            throw new Error(`HTTP error! status: ${res.status}`);
+async function getTickets(){
+
+    const res = await fetch('http://localhost:4000/tickets', {
+        next: {
+            revalidate: 30
         }
-        return await res.json();
-    } catch (error) {
-        console.error('Fetch failed:', error);
-        return []; // Return an empty array or handle the error as needed
-    }
-}
+    })
+    return res.json()
+} 
 
 export default async function TicketList() {
-    const tickets = await getTickets();
-    return (
-        <>
-            {tickets.length > 0 ? (
-                tickets.map((ticket) => (
-                    <div key={ticket.id} className="card my-5">
-                        <h3>{ticket.title}</h3>
-                        <p>{ticket.body.slice(0, 200)}...</p>
-                        <div>{ticket.priority} priority</div>
-                    </div>
-                ))
-            ) : (
-                <p className="text-content">There are no open tickets, yay!</p>
-            )}
-        </>
-    );
+    const tickets = await getTickets()
+  return (
+    <>
+    {tickets.map((ticket) => (
+        <div key={ticket.id} className="card my-5">
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+             <div>{ticket.priority} priority</div>
+        </div>
+    ))}
+    {tickets.length === 0 && (
+        <p className="text-content">There are no open tickets, yay!</p>
+    )}
+    </>
+  )
 }
