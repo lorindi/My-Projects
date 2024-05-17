81092a92f0c627be84d0444d674235fd7edfc35c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-17 17:12:45 +0300

    Update not-found.jsx

--- DojoHelpdesk/dojo-helpdesk/app/not-found.jsx
+++ DojoHelpdesk/dojo-helpdesk/app/not-found.jsx
@@ -1,14 +1,11 @@
-import Link from "next/link";
+import Link from "next/link"
 
 export default function NotFound() {
   return (
     <main className="text-center">
-      <h2 className="text-3x1">There was a problem.</h2>
+      <h2 className="text-3xl">There was a problem.</h2>
       <p>We could not find the page you were looking for.</p>
-      <p>
-        Go back to the <Link href="/" />
-        Dashboard
-      </p>
+      <p>Go back to the <Link href="/">dashboard</Link>.</p>
     </main>
-  );
+  )
 }
