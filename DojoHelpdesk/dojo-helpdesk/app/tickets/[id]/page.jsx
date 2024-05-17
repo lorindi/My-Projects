0c19af52be2c1f4c9e5808967062df297e9fbf59
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-17 13:37:00 +0300

    Create page.jsx

---
+++ DojoHelpdesk/dojo-helpdesk/app/tickets/[id]/page.jsx
@@ -0,0 +1,45 @@
+export const dynamicParams = true
+
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:4000/tickets");
+  const tickets = await res.json();
+  return tickets.map((tickets) => ({
+    id: ticket.id,
+  }));
+}
+
+async function getTicket(id) {
+  await new Promise((resolve) => setTimeout(resolve, 3000 ));
+
+  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
+    next: {
+      revalidate: 60,
+    },
+  });
+  if (!res.ok) {
+    notFound()
+  }
+
+  return res.json();
+}
+
+export default async function TicketDetails({ params }) {
+  // const id = params.id
+  const ticket = await getTicket(params.id);
+
+  return (
+    <main>
+      <nav>
+        <h2>Ticket Details</h2>
+      </nav>
+      <div className="card">
+        <h3>{ticket.title}</h3>
+        <small>Created by {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority} priority
+        </div>
+      </div>
+    </main>
+  );
+}
