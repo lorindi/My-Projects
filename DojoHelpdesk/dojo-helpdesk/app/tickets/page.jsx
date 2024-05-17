253f4f979fc812470055d858960f2391b9b0458b
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-17 13:37:07 +0300

    Update page.jsx

--- DojoHelpdesk/dojo-helpdesk/app/tickets/page.jsx
+++ DojoHelpdesk/dojo-helpdesk/app/tickets/page.jsx
@@ -1,15 +1,21 @@
+import { Suspense } from "react";
+import Loading from "../loading";
 import TicketList from "./TicketList";
 
 export default function Tickets() {
-    return (
-        <main>
-            <nav>
-                <div>
-                    <h2>Tickets</h2>
-                    <p>Currently open tickets.</p>
-                </div>
-            </nav>
-            <TicketList />
-        </main>
-    );
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently open tickets.</small>
+          </p>
+        </div>
+      </nav>
+      <Suspense fallback={<Loading />}>
+        <TicketList />
+      </Suspense>
+    </main>
+  );
 }
