8d3258709ca5df251330fb3fbedfd7de56577b45
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-17 13:37:05 +0300

    Create paje.jsx

---
+++ DojoHelpdesk/dojo-helpdesk/app/tickets/create/paje.jsx
@@ -0,0 +1,10 @@
+import CreateForm from "./CreateForm";
+
+export default function CreateTickets() {
+  return (
+    <div>
+      <h2 className="text-primary text-center">Add a New Ticket</h2>
+      <CreateForm />
+    </div>
+  );
+}
