92926ee242f21b0a79bb789e923a4a37f672799a
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-17 17:12:57 +0300

    Update page.jsx

--- DojoHelpdesk/dojo-helpdesk/app/tickets/page.jsx
+++ DojoHelpdesk/dojo-helpdesk/app/tickets/page.jsx
@@ -1,6 +1,7 @@
-import { Suspense } from "react";
-import Loading from "../loading";
-import TicketList from "./TicketList";
+import { Suspense } from "react"
+import TicketList from "./TicketList"
+import Loading from "../loading"
+import Link from "next/link"
 
 export default function Tickets() {
   return (
@@ -8,14 +9,15 @@ export default function Tickets() {
       <nav>
         <div>
           <h2>Tickets</h2>
-          <p>
-            <small>Currently open tickets.</small>
-          </p>
+          <p><small>Currently open tickets.</small></p>
         </div>
+        <Link href="/tickets/create" className="ml-auto">
+          <button className="btn-primary">New Ticket</button>
+        </Link>
       </nav>
       <Suspense fallback={<Loading />}>
         <TicketList />
       </Suspense>
     </main>
-  );
+  )
 }
