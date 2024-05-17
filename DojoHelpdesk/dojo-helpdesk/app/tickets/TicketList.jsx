cf7f523d4607581caa67721ac8a9bad98b77c837
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-17 13:37:09 +0300

    Update TicketList.jsx

--- DojoHelpdesk/dojo-helpdesk/app/tickets/TicketList.jsx
+++ DojoHelpdesk/dojo-helpdesk/app/tickets/TicketList.jsx
@@ -1,27 +1,36 @@
-async function getTickets(){
+import Link from "next/link";
 
-    const res = await fetch('http://localhost:4000/tickets', {
-        next: {
-            revalidate: 0
-        }
-    })
-    return res.json()
-} 
+async function getTickets() {
+  await new Promise((resolve) => setTimeout(resolve, 3000 ));
+
+  const res = await fetch("http://localhost:4000/tickets", {
+    next: {
+      revalidate: 0,
+    },
+  });
+
+  return res.json();
+}
 
 export default async function TicketList() {
-    const tickets = await getTickets()
+  const tickets = await getTickets();
+
   return (
     <>
-    {tickets.map((ticket) => (
+      {tickets.map((ticket) => (
         <div key={ticket.id} className="card my-5">
+          <Link href={`/tickets/${ticket.id}`}>
             <h3>{ticket.title}</h3>
             <p>{ticket.body.slice(0, 200)}...</p>
-            <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
         </div>
-    ))}
-    {tickets.length === 0 && (
-        <p className="text-content">There are no open tickets, yay!</p>
-    )}
+      ))}
+      {tickets.length === 0 && (
+        <p className="text-center">There are no open tickets, yay!</p>
+      )}
     </>
-  )
+  );
 }
