4573fdfad322dfa5936281d8494e4b62160d4e6f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-24 14:36:55 +0300

    Update Links.jsx

--- Portfolio/portfolio/src/components/sidebar/links/Links.jsx
+++ Portfolio/portfolio/src/components/sidebar/links/Links.jsx
@@ -1,14 +1,14 @@
-import "../Sidebar.scss";
-
-export const Links = () => {
+const Links = () => {
   const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
+
   return (
     <div className="links">
-      {items.map((item) => {
-        <a href={`#S{item}`} key={item}>
+      {items.map((item) => (
+        <a href={`#${item}`} key={item}>
           {item}
-        </a>;
-      })}
+        </a>
+      ))}
     </div>
   );
 };
+export default Links;
