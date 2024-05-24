b0912993bb4122f7218e5351a1d127fab02950b2
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-24 21:33:05 +0300

    Update Heroine.jsx

--- Portfolio/portfolio/src/components/heroine/Heroine.jsx
+++ Portfolio/portfolio/src/components/heroine/Heroine.jsx
@@ -1,11 +1,80 @@
-import "./Heroine.scss"
-import loraImg from './1712475819082.png'
+import "./Heroine.scss";
+import loraImg from "./IMG_20231225_205932.jpg";
+import scrollImg from "./pngwing.com.png";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 15,
+    },
+  },
+};
+
 export const Heroine = () => {
-    return (
-        <div className="heroine">
-          <div className="imageContainer">
-            <img src={loraImg} alt="" />
-          </div>
-        </div>
-    )
-}
+  return (
+    <div className="heroine">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>Lora Mitova</motion.h2>
+          <motion.h1 variants={textVariants}>
+            Web developer and UI designer
+          </motion.h1>
+          <motion.div variants={textVariants} className="buttons">
+            <motion.button variants={textVariants}>
+              See the Latest Works
+            </motion.button>
+            <motion.button variants={textVariants}>Contact Me</motion.button>
+          </motion.div>
+        </motion.div>
+        <motion.img
+          className="scrollImg"
+          variants={textVariants}
+          animate="scrollButton"
+          src={scrollImg}
+          alt=""
+        />
+      </div>
+      <motion.div className="slidingTextContainer" variants={sliderVariants} initial="initial" animate="animate">
+        Writer Content Creator Influencer
+      </motion.div>
+      <div className="imageContainer">
+        <img src={loraImg} alt="" />
+      </div>
+    </div>
+  );
+};
