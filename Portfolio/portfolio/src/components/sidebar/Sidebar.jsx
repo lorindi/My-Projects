ce41633e6e0e9075c4ac4e05bb8b1b488656373f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-24 15:06:30 +0300

    Update Sidebar.jsx

--- Portfolio/portfolio/src/components/sidebar/Sidebar.jsx
+++ Portfolio/portfolio/src/components/sidebar/Sidebar.jsx
@@ -4,27 +4,29 @@ import { motion, spring } from "framer-motion";
 import Links  from "./links/Links";
 import { ToggleButton } from "./toggleButton/ToggleButton";
 
+const variants = {
+  open: {
+      clipPath: "circle(1200px at 50px 50px)",
+      transition: {
+          type: "spring",
+          stiffness: 20,
+        },
+  },
+  closed: {
+    clipPath: "circle(30px at 50px 50px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
 export const Sidebar = () => {
   const [open, setOpen] = useState(false);
 
-  const variants = {
-    open: {
-        clipPath: "circle(1200px at 50px 50px)",
-        transition: {
-            type: "spring",
-            stiffness: 20,
-          },
-    },
-    closed: {
-      clipPath: "circle(30px at 50px 50px)",
-      transition: {
-        delay: 0.5,
-        type: "spring",
-        stiffness: 400,
-        damping: 40,
-      },
-    },
-  };
+  
   return (
     <motion.div className="sidebar" animate={open ? "open" : "closed"}>
       <motion.div className="bg" variants={variants}>
