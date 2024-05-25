86aa9320828d78c79e507f7800e40c3be64c1800
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-25 23:34:25 +0300

    Update App.jsx

--- Portfolio/portfolio/src/App.jsx
+++ Portfolio/portfolio/src/App.jsx
@@ -1,17 +1,22 @@
 import styles from "./app.module.scss";
 import { Heroine } from "./components/heroine/Heroine";
 import { Navbar } from "./components/navbar/Navbar";
+import { Parallax } from "./components/parallax/Parallax";
 function App() {
   return (
     <>
       <div>
         <section id="Homepage">
           <Navbar />
-          <Heroine/>
+          <Heroine />
         </section>
-        <section id="Services">Parallax</section>
+        <section id="Services">
+          <Parallax type="services" />
+        </section>
         <section>Services</section>
-        <section id="Portfolio">Parallax</section>
+        <section id="Portfolio">
+          <Parallax type="portfolio" />
+        </section>
         <section>Portfolio1</section>
         <section>Portfolio2</section>
         <section>Portfolio3</section>
