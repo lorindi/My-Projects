0c655f65c0f2dfc8dfa55139f8a581c2f5518f84
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-26 21:50:44 +0300

    Update App.jsx

--- Portfolio/portfolio/src/App.jsx
+++ Portfolio/portfolio/src/App.jsx
@@ -2,6 +2,7 @@ import styles from "./app.module.scss";
 import { Heroine } from "./components/heroine/Heroine";
 import { Navbar } from "./components/navbar/Navbar";
 import { Parallax } from "./components/parallax/Parallax";
+import { Services } from "./components/services/Services";
 function App() {
   return (
     <>
@@ -13,7 +14,9 @@ function App() {
         <section id="Services">
           <Parallax type="services" />
         </section>
-        <section>Services</section>
+        <section>
+          <Services />
+        </section>
         <section id="Portfolio">
           <Parallax type="portfolio" />
         </section>
