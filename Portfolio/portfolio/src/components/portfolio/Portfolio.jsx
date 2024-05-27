875aa26653d32a3d4627d7437fc045512135e652
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-27 17:48:58 +0300

    Update Portfolio.jsx

--- Portfolio/portfolio/src/components/portfolio/Portfolio.jsx
+++ Portfolio/portfolio/src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,5 @@
 import "./Portfolio.scss";
-import { motion, useScroll, useSpring } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import { useRef } from "react";
 
 const items = [
@@ -30,11 +30,28 @@ const items = [
 ];
 
 const Single = ({ item }) => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    // offset: ["end end", "start start"],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
   return (
     <section>
-      <h2>{item.title}</h2>
-      <img src={item.img} alt={item.title} />
-      <p>{item.desc}</p>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer">
+            <img src={item.img} alt={item.title} ref={ref} />
+          </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>See Demo</button>
+          </motion.div>
+        </div>
+      </div>
     </section>
   );
 };
