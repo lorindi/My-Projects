4fb2a7753bc6a03e7d13bb4a8268ea7439930227
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-28 11:32:21 +0300

    Update main.jsx

--- Portfolio/portfolio/src/main.jsx
+++ Portfolio/portfolio/src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+        theme="light"
+      />
   </React.StrictMode>,
 )
