5c6c385de321967e27ee7f07ba18ff0b7af07d74
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-28 16:06:05 +0300

    Create Cursor.jsx

---
+++ Portfolio/portfolio/src/components/cursor/Cursor.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import "./Cursor.scss";
+import { motion } from "framer-motion";
+export const Cursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+  console.log(position);
+  return (
+    <motion.div
+      className="cursor"
+      animate={{ x: position.x+10, y: position.y+20 }}
+    ></motion.div>
+  );
+};
