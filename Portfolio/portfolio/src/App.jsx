991689bc641bb8460551c28a3db25caa11d54366
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-05-29 13:46:11 +0300

    Update App.jsx

--- Portfolio/portfolio/src/App.jsx
+++ Portfolio/portfolio/src/App.jsx
@@ -18,12 +18,12 @@ function App() {
         <section id="Services">
           <Parallax type="services" />
         </section>
-        {/* <section>
+        <section>
           <Services />
-        </section> */}
-        {/* <section id="Portfolio">
+        </section>
+        <section id="Portfolio">
           <Parallax type="portfolio" />
-        </section> */}
+        </section>
         {/* <section>
           <Portfolio />
         </section> */}
