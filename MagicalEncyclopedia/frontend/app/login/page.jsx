7e0189c5dd80826ca937a47ca99a4b85705fe4ac
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-02 17:54:17 +0300

    Update page.jsx

---
+++ MagicalEncyclopedia/frontend/app/login/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LoginForm from "../components/loginForm/LoginForm";
+
+export default function Login() {
+  return (
+    <main>
+      <LoginForm />
+    </main>
+  );
+}
