1d22d8081a230725836c424482ff00892e059923
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-02 23:44:46 +0300

    Update LoginForm.jsx

--- MagicalEncyclopedia/frontend/app/components/loginForm/LoginForm.jsx
+++ MagicalEncyclopedia/frontend/app/components/loginForm/LoginForm.jsx
@@ -11,7 +11,7 @@ const LoginForm = () => {
           src="/images/peron.png"
           alt="Your Company"
         />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Sign in to your account
         </h2>
       </div>
