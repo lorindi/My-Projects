00c017aa9da6376f8bffdb6f1cd9cf786c8050a4
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-04 14:43:07 +0300

    Update Services.jsx

--- Portfolio/portfolio/src/components/services/Services.jsx
+++ Portfolio/portfolio/src/components/services/Services.jsx
@@ -1,102 +1,68 @@
 import "./Services.scss";
 import { motion, useInView } from "framer-motion";
-import {useRef} from "react"
-import serviceImg from './Screenshot 2024-05-28 121853.png'
-
-// const variants = {
-//   initial: {
-//     x: -500,
-//     y: 100,
-//     opacity: 0,
-//   },
-//   animate: {
-//     x: 0,
-//     opacity: 1,
-//     y: 0,
-//     transition: {
-//       duration: 1,
-//       staggerChildren: 0.1,
-//     },
-//   },
-// };
+import serviceImg from "./Screenshot 2024-05-28 121853.png";
 
 export const Services = () => {
-  // const ref = useRef();
-  // const isInView = useInView(ref, { margin: "-100px" });
   return (
-    <motion.div
-      className="services"
-      // variants={variants}
-      // initial="initial"
-      //   whileInView="animate"
-      // ref={ref}
-      // animate={isInView && "animate"}
-    >
-      <motion.div className="textContainer" 
-      // variants={variants}
-      >
+    <motion.div className="services">
+      <motion.div className="textContainer">
         <p>
-          I focus helping your brand grow <br />
+          I focus on helping your brand grow <br />
           and move forward
         </p>
         <hr />
       </motion.div>
-      <motion.div className="titleContainer" 
-      // variants={variants}
-      >
+      <motion.div className="titleContainer">
         <div className="title">
           <img src={serviceImg} alt="" />
           <h1>
-            <motion.b
-             whileHover={{color:"#98C1F4"}}
-             >Unique</motion.b> Ideas
+            <motion.b whileHover={{ color: "#98C1F4" }}>Unique</motion.b> Ideas
           </h1>
         </div>
         <div className="title">
           <h1>
-            <motion.b whileHover={{color:"#98C1F4"}}>For Your</motion.b> Business.
+            <motion.b whileHover={{ color: "#98C1F4" }}>For Your</motion.b>{" "}
+            Business.
           </h1>
-          <button>What we do?</button>
+          <button>What I do?</button>
         </div>
       </motion.div>
-      <motion.div className="listContainer" 
-      // variants={variants}
-      >
+      <motion.div className="listContainer">
         <motion.div
           className="box"
           whileHover={{ background: "white", color: "gray" }}
         >
-          <h2></h2>
+          <h2>Visual Design</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum amet
-            illo optio! Quia illum cum ducimus officia ipsum odit vel harum
-            repellendus! Esse officia error accusamus fuga dolorum illo labore.
+            Hi, I'm Lora, a passionate designer specializing in creating unique
+            visual solutions. I love combining art with functionality to create
+            websites that impress and inspire.
           </p>
-          <button>Go</button>
+          <button>^ ^</button>
         </motion.div>
         <motion.div
           className="box"
           whileHover={{ background: "white", color: "gray" }}
         >
-          <h2></h2>
+          <h2>Functional Websites</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum amet
-            illo optio! Quia illum cum ducimus officia ipsum odit vel harum
-            repellendus! Esse officia error accusamus fuga dolorum illo labore.
+            As an experienced web developer, I create functional and interactive
+            websites. I always strive to offer the best user experience by
+            combining modern technologies with creative solutions.
           </p>
-          <button>Go</button>
+          <button>^ ^</button>
         </motion.div>
         <motion.div
           className="box"
           whileHover={{ background: "white", color: "gray" }}
         >
-          <h2></h2>
+          <h2>Artistic Approach</h2>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum amet
-            illo optio! Quia illum cum ducimus officia ipsum odit vel harum
-            repellendus! Esse officia error accusamus fuga dolorum illo labore.
+            My artistic nature drives me to create designs that tell stories and
+            evoke emotions. I believe in the power of details and a personalized
+            approach to each project to make it unique and memorable.
           </p>
-          <button>Go</button>
+          <button>^ ^</button>
         </motion.div>
       </motion.div>
     </motion.div>
