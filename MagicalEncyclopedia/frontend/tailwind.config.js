0d85c860c1de64381115f83b4234513e29be2be0
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-04 09:37:43 +0300

    Update tailwind.config.js

--- MagicalEncyclopedia/frontend/tailwind.config.js
+++ MagicalEncyclopedia/frontend/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "custom-image": "url('/images/pngwing.com.png')",
       },
     },
   },
