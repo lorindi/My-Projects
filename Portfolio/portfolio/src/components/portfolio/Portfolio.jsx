a143e1f71955d571da597c4e658663fa61e01c45
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-04 09:37:46 +0300

    Update Portfolio.jsx

--- Portfolio/portfolio/src/components/portfolio/Portfolio.jsx
+++ Portfolio/portfolio/src/components/portfolio/Portfolio.jsx
@@ -4,31 +4,35 @@ import { useRef } from "react";
 import CFamily from "./cfamily.png";
 import MagicSushiLora from "./MagicSushiLora.png";
 import WebCrafters from "./WebCrafters.png";
-import PortfolioImg from './Portfolio.png'
+import PortfolioImg from "./Portfolio.png";
 const items = [
   {
     id: 1,
     title: "CFamily",
     img: CFamily,
     desc: "CFamily is a Django-based web application that provides a virtual center for information and community for people affected by cystic fibrosis in Bulgaria. The project offers various functionalities such as managing campaigns, events, news, initiatives, charity walks, lists of benefactors, personal stories, donations, and a discussion forum. Its goal is to unite people and facilitate the exchange of information and support within the community.",
+    link: "https://github.com/lorindi/My-Projects/tree/main/CFamily",
   },
   {
     id: 2,
     title: "Magic Sushi Lora",
     img: MagicSushiLora,
     desc: "MagicSushiLora is an online platform centered around sharing sushi recipes and discovering the world of sushi, leveraging Django Rest Framework for the backend and ReactJS for the frontend. The project incorporates technologies such as Axios for handling HTTP requests, React for building dynamic user interfaces, Toastify for displaying notifications, and EmailJS for email functionality. Its features include recipe sharing, a sushi gallery, educational resources about sushi, and a seamless sushi ordering experience.",
+    link: "https://github.com/lorindi/My-Projects/tree/main/MagicSushiLora",
   },
   {
     id: 3,
     title: "Web Crafters",
     img: WebCrafters,
     desc: "Web Crafters is a platform designed for users to create and manage descriptions for modern and functional websites, offering authentication features for users and admins. Built with React, JavaScript, HTML, and CSS, it integrates technologies like Toastify, EmailJS, and Fontawesome, along with APIs such as the Weather API and a custom backend server in Node.js. Users can access various pages like the homepage, About Us, login, registration, and site management, while admins have additional privileges including managing IT specialists and their descriptions.",
+    link: "https://lorindi-webcrafters-react-999.web.app/",
   },
   {
     id: 4,
     title: "Portfolio",
     img: PortfolioImg,
     desc: "The portfolio is built using React and incorporates animations with Framer Motion, along with styling using Sass. It integrates various functionalities such as Toastify for notifications, EmailJS for message sending, and Fontawesome for icons, presenting the author's work and skills in an interactive and stylish manner.",
+    link: "https://lorindi.vercel.app/",
   },
 ];
 
@@ -42,15 +46,17 @@ const Single = ({ item }) => {
 
   return (
     <section className="portfolioContainer">
-      <div className="container" >
+      <div className="container">
         <div className="wrapper">
-          <div className="imageContainer"  ref={ref}>
-            <img src={item.img} alt={item.title}  />
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt={item.title} />
           </div>
           <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button>See Demo</button>
+            <a href={item.link} target="_blank" rel="noopener noreferrer">
+              <button>See Demo</button>
+            </a>
           </motion.div>
         </div>
       </div>
