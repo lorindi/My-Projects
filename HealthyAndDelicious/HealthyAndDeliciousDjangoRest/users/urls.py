0c70f3c5b24a6ce259a2c9338d55c47bfb0633e8
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-07 00:40:43 +0300

    Update urls.py

--- HealthyAndDelicious/HealthyAndDeliciousDjangoRest/users/urls.py
+++ HealthyAndDelicious/HealthyAndDeliciousDjangoRest/users/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from users.views import RegisterApiView, LoginApiView, LogoutApiView, UpdateUserApiView, ProfileDetailsAPIView, profile
+# from users.views import RegisterApiView, LoginApiView, LogoutApiView, UpdateUserApiView, ProfileDetailsAPIView, profile
 
-urlpatterns = [
-    path('register/', RegisterApiView.as_view(), name='register'),
-    path('login/', LoginApiView.as_view(), name='login'),
-    path('logout/', LogoutApiView.as_view(), name='logout'),
-    path('update/<int:pk>/', UpdateUserApiView.as_view(), name='update'),
-    path('details/<int:pk>/', ProfileDetailsAPIView.as_view(), name='details'),
-    path('profile/', profile, name='profile'),
+# urlpatterns = [
+    # path('register/', RegisterApiView.as_view(), name='register'),
+    # path('login/', LoginApiView.as_view(), name='login'),
+    # path('logout/', LogoutApiView.as_view(), name='logout'),
+    # path('update/<int:pk>/', UpdateUserApiView.as_view(), name='update'),
+    # path('details/<int:pk>/', ProfileDetailsAPIView.as_view(), name='details'),
+    # path('profile/', profile, name='profile'),
 
-]
+# ]
