9ddded2b9ebf5c0a85c6aaf4094cd1584e4253a1
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-07 00:40:25 +0300

    Update urls.py

--- HealthyAndDelicious/HealthyAndDeliciousDjangoRest/HealthyAndDeliciousDjangoRest/urls.py
+++ HealthyAndDelicious/HealthyAndDeliciousDjangoRest/HealthyAndDeliciousDjangoRest/urls.py
@@ -20,7 +20,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include([
-        path('auth/', include('users.urls')),
-        path('recipe/', include('recipes.urls')),
+        # path('auth/', include('users.urls')),
+        # path('recipe/', include('recipes.urls')),
+        path('auth/', include('accounts.urls')),
+
     ])),
 ]
