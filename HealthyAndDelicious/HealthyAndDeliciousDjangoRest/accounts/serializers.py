6a413f0fe867ea7496ad89db24953e1a97623c89
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-08 14:24:04 +0300

    Update serializers.py

--- HealthyAndDelicious/HealthyAndDeliciousDjangoRest/accounts/serializers.py
+++ HealthyAndDelicious/HealthyAndDeliciousDjangoRest/accounts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate, login, logout
 
 from accounts.models import User
+from rest_framework.exceptions import AuthenticationFailed
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
@@ -27,3 +29,31 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         )
         return user
 
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255, min_length=6)
+    password = serializers.CharField(max_length=100, write_only=True)
+    full_name = serializers.CharField(max_length=255, read_only=True)
+    access_token = serializers.CharField(max_length=255, read_only=True)
+    refresh_token = serializers.CharField(max_length=255, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'full_name', 'access_token', 'refresh_token']
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+        request = self.context.get('request')
+        user = authenticate(request, email=email, password=password)
+        if not user:
+            raise AuthenticationFailed('Invalid credentials')
+        if not user.is_verified:
+            raise AuthenticationFailed('Email is not verified')
+        user_tokens = user.tokens()
+        return {
+            'email': user.email,
+            'full_name': user.get_full_name,
+            'access_token': str(user_tokens.get('access')),
+            'refresh_token': str(user_tokens.get('refresh')),
+        }
