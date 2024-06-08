d5e408f131d9e625f203ad2b6b1c125ddff6134b
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-08 14:24:07 +0300

    Update urls.py

--- HealthyAndDelicious/HealthyAndDeliciousDjangoRest/accounts/urls.py
+++ HealthyAndDelicious/HealthyAndDeliciousDjangoRest/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from accounts.views import UserRegisterView
+from accounts.views import UserRegisterView, VerifyUserEmail, LoginUserView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('verify-email/', VerifyUserEmail.as_view(), name='verify_email'),
+    path('login/', LoginUserView.as_view(), name='login'),
 ]
