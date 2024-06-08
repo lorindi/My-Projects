e4cc03117fcebddf42e4e6a02a918272e42ef4b8
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-08 14:24:15 +0300

    Update urls.py

--- HealthyAndDelicious/HealthyAndDeliciousDjangoRest/HealthyAndDeliciousDjangoRest/urls.py
+++ HealthyAndDelicious/HealthyAndDeliciousDjangoRest/HealthyAndDeliciousDjangoRest/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include([
         # path('auth/', include('users.urls')),
         # path('recipe/', include('recipes.urls')),
+        path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+        path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
         path('auth/', include('accounts.urls')),
 
     ])),
