cb9506434ba51bf61842d3ac5c6f1e9ee24742b6
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-08 14:24:09 +0300

    Update views.py

--- HealthyAndDelicious/HealthyAndDeliciousDjangoRest/accounts/views.py
+++ HealthyAndDelicious/HealthyAndDeliciousDjangoRest/accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.generics import GenericAPIView
-from .serializers import UserRegisterSerializer
+from .serializers import UserRegisterSerializer, LoginSerializer
 from rest_framework.response import Response
+from .models import OneTimePassword
 
 from .utils import send_code_to_user
 
@@ -22,3 +23,33 @@ class UserRegisterView(GenericAPIView):
             return Response({'data': user, 'message': f' thanks for registering! '},
                             status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class VerifyUserEmail(GenericAPIView):
+    def post(self, request):
+        otpcode = request.data('otp')
+        try:
+            user_code_obj = OneTimePassword.objects.get(code=otpcode)
+            user = user_code_obj.user
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+                return Response({
+                    'message': f'Your account has been verified!',
+                }, status=status.HTTP_200_OK)
+            return Response({
+                'message': f'Your account has already been verified!',
+
+            }, status=status.HTTP_204_NO_CONTENT)
+
+        except OneTimePassword.DoesNotExist:
+            return Response({'message': f'Your account does not exist!'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginUserView(GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
