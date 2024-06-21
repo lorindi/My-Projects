8413b7db5772cc76e8c4fb17ea4075d9751f49d8
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-21 23:24:10 +0300

    Create page.jsx

---
+++ HiddenTrails/adventures/app/register/page.jsx
@@ -0,0 +1,10 @@
+import RegisterForm from "../components/register/RegisterForm";
+import React from "react";
+
+export default function Register() {
+  return (
+    <section>
+      <RegisterForm />
+    </section>
+  );
+}
