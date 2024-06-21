cd813032f198ac8adde5b552f17de9c6171d67d4
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-21 23:24:35 +0300

    Create tailwind.config.js

---
+++ HiddenTrails/adventures/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./utils/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        'base-phone': ['16px', '24px'],
+        'base-tablet': ['18px', '26px'],
+        'base-web': ['20px', '28px'],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "custom-image": "url('../public/images/headerImg.jpeg')",
+      },
+    },
+    screens: {
+      phone: "393px",
+      tablet: "768px",
+      web: "1280px",
+    },
+  },
+  plugins: [],
+};
