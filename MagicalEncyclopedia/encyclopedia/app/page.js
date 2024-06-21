07b8a734cdcfe3323fd38b17ffa193126c627665
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-21 16:20:46 +0300

    Add files via upload

--- MagicalEncyclopedia/encyclopedia/app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="bg-teal-950 p-20 text-white font-semibold text-3xl">
-      <h1>Home</h1>
-    </div>
-  );
-}
