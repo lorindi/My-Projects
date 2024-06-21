d595990754fa119bff56f7a11f09bd1301fba545
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-21 23:24:05 +0300

    Create page.jsx

---
+++ HiddenTrails/adventures/app/login/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LoginForm from '../components/login/LoginForm'
+
+export default function Login() {
+  return (
+    <section className="">
+      <LoginForm/>
+    </section>
+  );
+}
