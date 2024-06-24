2c83725ba26157d9df7ee650fe3093f595e9b168
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-24 20:11:31 +0300

    Create Web.jsx

---
+++ HiddenTrails/adventures/app/components/header/web/Web.jsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react'
+import Link from 'next/link';
+function Web() {
+  return (
+    <div>
+      <Link href="/"></Link>
+    </div>
+  )
+}
+
+export default Web
