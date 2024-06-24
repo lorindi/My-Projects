717d342628bba745a3c6cf36eb1183f971db00ed
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-24 20:22:48 +0300

    Update Web.jsx

--- HiddenTrails/adventures/app/components/header/web/Web.jsx
+++ HiddenTrails/adventures/app/components/header/web/Web.jsx
@@ -1,12 +1,72 @@
 "use client";
-import React from 'react'
-import Link from 'next/link';
-function Web() {
-  return (
-    <div>
-      <Link href="/"></Link>
-    </div>
-  )
-}
+import Link from "next/link";
+import Btn from "../../buttons/Buttons";
+import { usePathname } from "next/navigation";
 
-export default Web
+export default function Web() {
+  const pathname = usePathname();
+
+  const signInButtonClass =
+    pathname === "/"
+      ? "h-11 px-4 py-3 justify-center items-center flex text-center font-medium text-gray-900 rounded-full border-2 border-neutral-50 bg-neutral-50"
+      : "h-11 px-4 py-3 justify-center items-center flex text-center font-medium bg-neutral text-gray-900 border-2 rounded-full border-blue-950";
+
+  return (
+    <nav className="flex items-center w-full h-full justify-around">
+      <Link
+        className="text-center text-gray-900 text-2xl font-medium font-['Inter Tight']"
+        href="/"
+      >
+        LOGO
+      </Link>
+
+      <div className="flex items-center">
+        <Btn
+          type="button"
+          variant="transparent"
+          text="Explore"
+          link="/explore"
+        />
+        <div className="mr-[30px]">
+          <Btn type="button" variant="transparent" text="Menu" link="/menu" />
+        </div>
+        <Btn type="button" variant="transparent" text="Create" link="/create" />
+        <Btn
+          type="button"
+          variant="transparent"
+          text="My tours"
+          link="/my-tours"
+        />
+        <div>
+          <Btn type="button" variant="transparent" text="Menu" link="/menu" />
+        </div>
+        {/* <div className="mx-2.5">
+          <Button
+            variant="secondary-outlined"
+            text="Logout"
+            type={"submit"}
+            onClick={logoutUser}
+          />
+        </div> */}
+
+        <div className="mx-2.5">
+          <Btn
+            type="button"
+            variant="outlined"
+            text="Sign in"
+            link="/login"
+            className={signInButtonClass}
+          />
+        </div>
+        <div className="mx-2.5">
+          <Btn
+            type="button"
+            variant="filled"
+            text="Create Account"
+            link="/register"
+          />
+        </div>
+      </div>
+    </nav>
+  );
+}
