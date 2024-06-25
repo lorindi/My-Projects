dad8395c53ee6166a1053623b833e61da90db4e2
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-06-25 16:41:21 +0300

    Update LoginForm.jsx

--- HiddenTrails/adventures/app/components/login/LoginForm.jsx
+++ HiddenTrails/adventures/app/components/login/LoginForm.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Btn from "../buttons/Buttons";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import InputField from "../input-fields/InputFields";
@@ -31,31 +32,64 @@ function LoginForm() {
 
   return (
     <div className="flex flex-col items-center justify-start w-full">
-      <h2 className="mb-[40px] text-center text-white font-medium font-['Inter'] text-[24px] 
-      web:text-[32px] tablet:text-[32px] 
-      phone:text-[24px]">
+      <h2 className="mb-[40px] text-center  text-white font-medium text-[24px]
+      web:text-[32px]
+      tablet:text-[32px]
+      phone:text-[24px]
+      ">
         Sign In
       </h2>
       <form
-        className="flex items-center justify-evenly flex-col bg-neutral-50 rounded-[5px] min-h-[451px] w-full 
-        web:w-[582px] web:min-h-[602px] 
+        className="flex  items-center justify-evenly flex-col bg-neutral-50 w-full min-h-[451px] rounded-[5px] 
+        web:rounded-[10px] web:w-[582px] web:min-h-[602px] 
         tablet:w-[582px] tablet:min-h-[602px] 
-        phone:w-[361px] phone:min-h-[441px]"
+        phone:w-[361px] phone:min-h-[441px] 
+        "
         onSubmit={handleSubmit}
       >
         <div
-          className="
-            web:w-[400px] web:h-[60px] 
-            tablet:w-[400px] tablet:h-[60px]
-            phone:w-[320px] phone:h-[43px] 
-            smallPhone:w-[288px] smallPhone:h-[48px]
-            sm:w-72 sm:h-[43px]
-            md:w-80 md:h-[43px]
-            lg:w-[400px] lg:h-[60px]
-            w-[400px] h-[60px]
-          "
+          className="h-[48px] w-[288px]
+          web:w-[400px] web:h-[60px] 
+          tablet:w-[400px] tablet:h-[60px]
+          phone:w-[320px] phone:h-[43px] 
+          sm:w-72 sm:h-[43px]
+          md:w-80 md:h-[43px]
+          lg:w-[400px] lg:h-[60px]
+        "
         >
           <GoogleButton />
+        </div>
+        <div
+          className="justify-center items-center gap-2 inline-flex w-[400px] h-[17px]
+          web:w-[400px] web:h-[17px]
+          tablet:w-[400px] tablet:h-[17px] 
+          "
+        >
+          <div
+            className="border  border-zinc-400 w-[126px] h-[1px]
+            web:w-[183px] web:h-[1px] 
+            tablet:w-[183px] tablet:h-[1px]
+            phone:w-[145px] phone:h-[1px]
+          "
+          ></div>
+
+          <div
+            className="text-center text-zinc-400 font-medium  text-[10px]
+          web:text-sm 
+          tablet:text-sm 
+          phone:text-[10px] 
+          "
+          >
+            OR
+          </div>
+
+          <div
+            className="border  border-zinc-400 w-[126px] h-[1px]
+            web:w-[183px] web:h-[1px] 
+            tablet:w-[183px] tablet:h-[1px]
+            phone:w-[145px] phone:h-[1px]
+          "
+          ></div>
         </div>
 
         <InputField
@@ -78,15 +112,14 @@ function LoginForm() {
           onChange={(e) => setPassword(e.target.value)}
           required
         />
-        <div className="h-[43px] w-72 
+        <div className="h-[43px] w-72
         web:w-[400px] web:h-[43px] 
-        tablet:w-[400px] tablet:h-[43px] 
-        phone:w-80 phone:h-[43px]">
-          <button type="submit" className="btn btn-primary w-full">
-            Login
-          </button>
+        tablet:w-[400px] tablet:h-[43px]
+        phone:w-80 phone:h-[43px] 
+        ">
+          <Btn type="submit" variant="filled" text="Login" fullWidth />
         </div>
-        {error && <p className="text-red-500">{error}</p>}
+        {/* {error && <p className="text-red-500">{error}</p>} */}
       </form>
     </div>
   );
