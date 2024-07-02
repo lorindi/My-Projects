ce94ecf802df08629ae8158a44a875672efc290d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-02 23:18:27 +0300

    Update app.js

--- EstateApp/api/app.js
+++ EstateApp/api/app.js
@@ -1,20 +1,23 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 
 import mongoose from "mongoose";
+import { PORT, MONGO_DB_CONNECTION_STRING } from "./constraints/constraints.js";
 
 const app = express();
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/estate")
+  .connect(MONGO_DB_CONNECTION_STRING)
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log(err));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cookieParser())
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -24,7 +27,7 @@ app.get("/", (req, res) => {
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
 
-app.listen(5000, () => {
-  console.log("Restful server is listening on port 5000...")
+app.listen(PORT, () => {
+  console.log(`Restful server is listening on port ${PORT}...`)
 
 });
