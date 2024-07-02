feda8cf2840e3ae120de6e8248ceb253a0510eb7
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-02 22:26:23 +0300

    Create User.js

---
+++ EstateApp/api/models/User.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [validator.isEmail, 'Invalid email'],
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 3,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  avatar: {
+    type: String,
+    default: null,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+  }
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+export default User;
