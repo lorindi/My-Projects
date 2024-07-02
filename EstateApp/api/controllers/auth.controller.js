2ee4fa2d99351e2b13b50ac6fb7b836d6a4a380f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-02 14:14:28 +0300

    Update auth.controller.js

--- EstateApp/api/controllers/auth.controller.js
+++ EstateApp/api/controllers/auth.controller.js
@@ -1,11 +1,22 @@
-import bcrypt from "bcrypt"
-export const register = (req, res) => {
-  const {username, email, password} = req.body
- 
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
   // HASH THE PASSWORD
+  const hashPassword = await bcrypt.hash(password, 10);
+  console.log(hashPassword);
 
   //CREATE A NEW USER AND SAVE TO DB
-
+  const newUser = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashPassword,
+    },
+  });
+  console.log(newUser);
 };
 export const login = (req, res) => {
   //db operations
