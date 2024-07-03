4693805f41b1eedb7539f18fbc04b7e31d2ee5bc
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-03 18:10:49 +0300

    Update auth.controller.js

--- EstateApp/api/controllers/auth.controller.js
+++ EstateApp/api/controllers/auth.controller.js
@@ -14,14 +14,13 @@ export const register = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password.trim(), 10);
+    // Hash the password in User model
 
     // Create a new user
     const newUser = new User({
       name,
       email,
-      password: hashedPassword,
+      password,
     });
 
     // Save the user to the database
@@ -51,56 +50,28 @@ export const login = async (req, res) => {
     if (!user) return res.status(401).json({ message: "User not found!" });
 
     // Check if the password is correct
-    const isPasswordValid = await bcrypt.compare(password.trim(), user.password);
+    const isPasswordValid = await bcrypt.compare(
+      password.trim(),
+      user.password
+    );
     console.log(password, user.password);
     console.log(await bcrypt.compare(password, user.password));
-    if (!isPasswordValid) return res.status(401).json({ message: "Invalid Credential" });
-    
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid Credential" });
+
     // Generate cookie token and send to the user
-    res.setHeader("Set-Cookie", "test=" + "myValue").json("success");
+    // res.setHeader("Set-Cookie", "test=" + "myValue").json("success");
+    res
+      .cookie("test2", "myValue2", {
+        httpOnly: true,
+        // secure:true,
+      })
+      .status(200)
+      .json({ message: "Login Successful" });
   } catch (err) {
     console.error("Login error:", err);
     res.status(500).json({ message: "Invalid Credentials!" });
   }
-
-  // const { email, password } = req.body;
-  // console.log("login:", email, password);
-
-  // try {
-  //   // Check if the user exists
-  //   const user = await User.findOne({ email });
-  //   if (!user) {
-  //     console.log("User does not exist");
-  //     return res.status(400).json({ message: "User does not exist" });
-  //   }
-
-  //   // Log the provided password and stored hashed password for debugging
-  //   console.log("Provided password:", password);
-  //   console.log("Stored hashed password:", user.password);
-
-  //   // Check if the password is correct
-  //   const isPasswordValid = await bcrypt.compare(password, user.password);
-  //   console.log("Password comparison result:", isPasswordValid); // Debug statement
-
-  //   if (!isPasswordValid) {
-  //     console.log("Invalid password");
-  //     return res.status(400).json({ message: "Invalid password" });
-  //   }
-
-  //   // Generate a JWT token
-  //   const token = jwt.sign(
-  //     { userId: user._id, name: user.name, email: user.email },
-  //     JWT_SECRET, // Use a secure secret or store it in environment variables
-  //     { expiresIn: "1h" }
-  //   );
-
-  //   // Send the token as a cookie or in the response body
-  //   res.cookie("token", token, { httpOnly: true });
-  //   res.status(200).json({ message: "Login successful", token });
-  // } catch (error) {
-  //   console.error("Login error:", error);
-  //   res.status(500).json({ message: "Server error" });
-  // }
 };
 
 // Logout function
