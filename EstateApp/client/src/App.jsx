339d67e8bcf14abd6dee4ac523d07c519759d360
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-04 16:28:57 +0300

    Update App.jsx

--- EstateApp/client/src/App.jsx
+++ EstateApp/client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./layout.scss";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar/Navbar";
 function App() {
   const [count, setCount] = useState(0);
 
