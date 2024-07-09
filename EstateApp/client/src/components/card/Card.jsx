1b8482b58202bf1a61daa6d90698c741576e2dd3
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-09 17:22:54 +0300

    Update Card.jsx

--- EstateApp/client/src/components/card/Card.jsx
+++ EstateApp/client/src/components/card/Card.jsx
@@ -1,8 +1,43 @@
-import './Card.scss'
-function Card() {
+import { Link } from "react-router-dom";
+import "./Card.scss";
+function Card({ item }) {
   return (
-    <div className='card'>Card</div>
-  )
+    <div className="card">
+      <Link to={`/${item.id}`} className="imageContainer">
+        <img src={item.img} alt="" />
+      </Link>
+      <div className="textContainer">
+        <h2 className="title">
+          <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className="address">
+          <img src="/pin.png" alt="" />
+          <span>{item.address}</span>
+        </p>
+        <p className="price">$ {item.price}</p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src="/bedroom.png" alt="" />
+              <span>{item.bedroom} bedroom</span>
+            </div>
+            <div className="feature">
+              <img src="/bathroom.png" alt="" />
+              <span>{item.bathroom} bathroom</span>
+            </div>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <img src="/save.png" alt="" />
+            </div>
+            <div className="icon">
+              <img src="/chat.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
