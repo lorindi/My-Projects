b920c3a9898d68cab980629784bccadf76fd7493
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-09 13:05:30 +0300

    Update ListPage.jsx

--- EstateApp/client/src/components/routes/listPage/ListPage.jsx
+++ EstateApp/client/src/components/routes/listPage/ListPage.jsx
@@ -1,8 +1,22 @@
-import './ListPage.scss'
+import "./ListPage.scss";
+import { listData } from "../../../lib/dummydata";
+import Filter from "../../filter/Filter";
+import Card from "../../card/Card";
 function ListPage() {
+  const data = listData;
   return (
-    <div className='listPage'>ListPage</div>
-  )
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item}/>
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">Map</div>
+    </div>
+  );
 }
 
-export default ListPage
+export default ListPage;
