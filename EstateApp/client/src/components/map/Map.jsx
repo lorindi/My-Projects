95112726f05a9ec121447cbd311aed0cdf85882c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-10 18:09:00 +0300

    Update Map.jsx

---
+++ EstateApp/client/src/components/map/Map.jsx
@@ -0,0 +1,24 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+import "./Map.scss";
+import "leaflet/dist/leaflet.css";
+import Pin from "../pin/Pin";
+function Map({ items }) {
+  return (
+    <MapContainer
+      center={[42.66901741998718, 23.601097516441737]}
+      zoom={13}
+      scrollWheelZoom={false}
+      className="map"
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {items.map((item) => (
+        <Pin item={item} key={item.id} />
+      ))}
+    </MapContainer>
+  );
+}
+
+export default Map;
