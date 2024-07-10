da42e1fa050d75960cd0582ab4be4091d9a025ed
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-10 22:08:27 +0300

    Update SinglePage.jsx

--- EstateApp/client/src/components/routes/singlePage/SinglePage.jsx
+++ EstateApp/client/src/components/routes/singlePage/SinglePage.jsx
@@ -1,11 +1,39 @@
-import './SinglePage.scss'
+import Slider from "../../slider/Slider";
+import "./SinglePage.scss";
+import { singlePostData, userData } from "../../../lib/dummydata";
+
 function SinglePage() {
   return (
-    <div className='singlePage'>
-      <div className="details"></div>
-      <div className="features"></div>
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">$ {singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">
+              {singlePostData.description}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SinglePage
+export default SinglePage;
