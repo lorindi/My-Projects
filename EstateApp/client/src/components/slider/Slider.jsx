803783973294d5d990ca9edbcf01daa6b79c69ce
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-11 08:20:24 +0300

    Adding slider functionality

--- EstateApp/client/src/components/slider/Slider.jsx
+++ EstateApp/client/src/components/slider/Slider.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import "./Slider.scss";
 function Slider({ images }) {
+  const [imageIndex, setImageIndex] = useState(null);
+  const changeSlide = (direction) => {
+    if (direction === "left") {
+      if (imageIndex === 0) {
+        setImageIndex(images.length - 1);
+      } else {
+        setImageIndex(imageIndex - 1);
+      }
+    } else {
+      if (imageIndex === images.length - 1) {
+        setImageIndex(0);
+      } else {
+        setImageIndex(imageIndex + 1);
+      }
+    }
+  };
   const CloseIcon = () => (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       width="50"
       height="50"
+      onClick={() => setImageIndex(null)}
     >
       <path
         d="M6 6L18 18"
@@ -59,28 +77,33 @@ function Slider({ images }) {
 
   return (
     <div className="slider">
-      <div className="fullSlider">
-        <div className="arrow">
-          {/* <img src="/arrowl.png" alt="" /> */}
-          <ArrowLeftIcon />
+      {imageIndex !== null && (
+        <div className="fullSlider">
+          <div className="arrow" onClick={() => changeSlide("left")}>
+            <ArrowLeftIcon />
+          </div>
+          <div className="imgContainer">
+            <img src={images[imageIndex]} alt="" />
+          </div>
+          <div className="arrow" onClick={() => changeSlide("right")}>
+            <ArrowRightIcon />
+          </div>
+          <div className="close">
+            <CloseIcon />
+          </div>
         </div>
-        <div className="imgContainer">
-          <img src={images[0]} alt="" />
-        </div>
-        <div className="arrow">
-          {/* <img src="/arrowr.png" alt="" /> */}
-          <ArrowRightIcon />
-        </div>
-        <div className="close">
-          <CloseIcon />
-        </div>
-      </div>
+      )}
       <div className="bigImage">
-        <img src={images[0]} alt="" />
+        <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
       </div>
       <div className="smallImages">
         {images.slice(1).map((img, index) => (
-          <img src={img} key={index} alt="" />
+          <img
+            src={img}
+            key={index}
+            alt=""
+            onClick={() => setImageIndex(index + 1)}
+          />
         ))}
       </div>
     </div>
