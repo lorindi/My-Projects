80bd41d3dce3d4e6ad546e7705488b5a06ac4874
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-11 08:28:39 +0300

    Create ArrowRightIcon.jsx

---
+++ EstateApp/client/src/components/slider/svg/ArrowRightIcon.jsx
@@ -0,0 +1,21 @@
+const ArrowRightIcon = ({ onClick }) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="50"
+      height="50"
+      onClick={onClick}
+    >
+      <path
+        d="M12 4L20 12L12 20"
+        stroke="white"
+        strokeWidth="2"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+  
+  export default ArrowRightIcon;
+  
