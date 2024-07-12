bf9c001cf528a56f1dc39131776b969382a1d0c3
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-12 11:26:04 +0300

    Update App.jsx

--- EstateApp/client/src/App.jsx
+++ EstateApp/client/src/App.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./components/routes/home/HomePage";
 import ListPage from "./components/routes/listPage/ListPage";
 import SinglePage from "./components/routes/singlePage/SinglePage";
+import ProfilePage from "./components/routes/profilePage/ProfilePage";
 
 function App() {
   const router = createBrowserRouter([
@@ -11,17 +12,21 @@ function App() {
       element: <Layout />,
       children: [
         {
-          path:'/',
-          element: <HomePage/>
+          path: "/",
+          element: <HomePage />,
         },
         {
-          path:'/list',
-          element: <ListPage/>
+          path: "/list",
+          element: <ListPage />,
         },
         {
-          path:'/:id',
-          element: <SinglePage/>
-        }
+          path: "/:id",
+          element: <SinglePage />,
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />,
+        },
       ],
     },
   ]);
