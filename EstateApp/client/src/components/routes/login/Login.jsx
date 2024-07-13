fca57e8d86f915786d7fb61eae4b358140d6b87c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-13 19:50:37 +0300

    Update Login.jsx

--- EstateApp/client/src/components/routes/login/Login.jsx
+++ EstateApp/client/src/components/routes/login/Login.jsx
@@ -1,7 +1,70 @@
-import "./Login.scss";
+import { useState } from "react";
+import "./login.scss";
+import { Link, useNavigate } from "react-router-dom";
+import apiRequest from "../../../lib/apiRequest";
 
 function Login() {
-  return <div>Login</div>;
+  const [error, serError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    serError("")
+
+    const formData = new FormData(e.target);
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const res = await apiRequest.post(
+        "/auth/login",
+        {
+          email,
+          password,
+        }
+      );
+      navigate('/')
+    } catch (err) {
+      serError(err.response.data.message )
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <h1>Welcome back</h1>
+          <input
+            name="email"
+            required
+            minLength={3}
+            type="text"
+            placeholder="Email"
+          />
+          <input
+            name="password"
+            type="password"
+            required
+            placeholder="Password"
+          />
+          <button disabled={isLoading}>Login</button>
+          {error && (<span>{error}</span>)}
+          <Link to="/register">{"Don't"} you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Login;
