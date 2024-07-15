f6c961f34419bdb041136db2d8c873d29b71edbd
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-15 09:43:39 +0300

    Create Description.jsx

---
+++ HealthyAndDelicious/client/src/components/description/Description.jsx
@@ -0,0 +1,12 @@
+
+import './Description.scss';
+
+const Description = ({ children }) => {
+  return (
+    <div className="description">
+      {children}
+    </div>
+  );
+};
+
+export default Description;
