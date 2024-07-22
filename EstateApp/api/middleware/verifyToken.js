11aeb6c987a66511aec268fbcd4e0d7022d442ae
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-22 13:41:05 +0300

    Create verifyToken.js

---
+++ EstateApp/api/middleware/verifyToken.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRET_KEY } from "../constraints/constraints.js";
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) return res.status(401).json({ message: "Not Authenticated!" });
+
+  jwt.verify(token, JWT_SECRET_KEY, async (err, payload) => {
+    if (err) return res.status(403).json({ message: "Token is not Valid!" });
+    req.userId = payload.id;
+    console.log(req.userId, payload.id, req);
+    next();
+  });
+};
