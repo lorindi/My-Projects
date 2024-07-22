2742829ef8198308a7a2c2417e16bb0f912c1d6d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-22 13:41:59 +0300

    Update main.jsx

--- EstateApp/client/src/main.jsx
+++ EstateApp/client/src/main.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
 // import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  // </BrowserRouter>
+      <React.StrictMode>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </React.StrictMode>,
 );
