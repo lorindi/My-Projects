d178df3ac0cd975910d37ca71ee15b13840f41dd
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-22 13:42:02 +0300

    Create Footer.jsx

---
+++ HealthyAndDelicious/client/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.scss';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>&copy; 2024 Healthy and Delicious</p>
+    </div>
+  );
+}
+
+export default Footer;
