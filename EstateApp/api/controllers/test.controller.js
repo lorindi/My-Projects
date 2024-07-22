156b68709772a38edcfc7b64f2b46eac831574e2
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-22 13:41:03 +0300

    Create test.controller.js

---
+++ EstateApp/api/controllers/test.controller.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRET_KEY } from "../constraints/constraints.js";
+
+export const shouldBeLoggedIn = async (req, res) => {
+  console.log(req.userId);
+  res.status(200).json({ message: "You are Authenticated" });
+};
+export const shouldBeAdmin = async (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) return res.status(401).json({ message: "Not Authenticated!" });
+
+  jwt.verify(token, JWT_SECRET_KEY, async (err, payload) => {
+    if (err) return res.status(403).json({ message: "Token is not Valid!" });
+    if (!payload.isAdmin) {
+      return res.status(403).json({ message: "Not authorized" });
+    }
+  });
+  res.status(200).json({ message: "You are Authenticated" });
+};
