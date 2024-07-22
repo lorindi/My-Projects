582c2aaeda1cbed36e8c5ab0368fdf9f7b5af1a5
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-22 13:41:54 +0300

    Create AuthContext.jsx

---
+++ EstateApp/client/src/context/AuthContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(() => {
+    const user = localStorage.getItem("user");
+    try {
+      return JSON.parse(user) || null;
+    } catch (e) {
+      return null;
+    }
+  });
+
+  const updateUser = (data) => {
+    setCurrentUser(data);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser: currentUser, updateUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
