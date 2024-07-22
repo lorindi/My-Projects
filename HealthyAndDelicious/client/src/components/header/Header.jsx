b35fc69d12c46e1c6a8c8612e8d016d83c702fb2
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-22 13:42:08 +0300

    Create Header.jsx

---
+++ HealthyAndDelicious/client/src/components/header/Header.jsx
@@ -0,0 +1,21 @@
+import "./Header.scss";
+import Web from "./web/Web";
+import Tablet from "./tablet/Tablet";
+import Mobile from "./mobile/Mobile";
+function Header() {
+  return (
+    <header className="header">
+      <div className="web">
+        <Web />
+      </div>
+      <div className="tablet">
+        <Tablet />
+      </div>
+      <div className="mobile">
+        <Mobile />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
