434662bd7fe2e43f9a03efd4931df68f9e62e541
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-23 10:29:31 +0300

    Update ProfilePage.jsx

--- EstateApp/client/src/components/routes/profilePage/ProfilePage.jsx
+++ EstateApp/client/src/components/routes/profilePage/ProfilePage.jsx
@@ -2,46 +2,51 @@ import Chat from "../../chat/Chat";
 import List from "../../list/List";
 import "./ProfilePage.scss";
 import apiRequest from "../../../lib/apiRequest";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../../context/AuthContext";
 
 function ProfilePage() {
-  
-const {updateUser, currentUser} = useContext(AuthContext)
-
+  const { updateUser, currentUser } = useContext(AuthContext);
   const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   if (!currentUser) {
+  //     navigate("/login");
+  //   }
+  // }, [currentUser, navigate]);
+
   const handleLogout = async () => {
     try {
       await apiRequest.post("/auth/logout");
-      // localStorage.removeItem("user")
-      updateUser(null)
+      updateUser(null);
       navigate("/");
     } catch (err) {
       console.log(err);
     }
   };
+
   return (
+    // currentUser && (
     <div className="profilePage">
       <div className="details">
         <div className="wrapper">
           <div className="title">
             <h1>User Information</h1>
-            <button>Update Profile</button>
+           <Link to="/profile/update">
+           <button>Update Profile</button>
+           </Link>
           </div>
           <div className="info">
             <span>
               Avatar:
-              <img
-                src={currentUser._doc.avatar || "/noavatar.png"}
-                alt=""
-              />
+              <img src={currentUser?._doc?.avatar || "/noavatar.png"} alt="" />
             </span>
             <span>
-              Name: <b>{currentUser._doc.name}</b>
+              Name: <b>{currentUser?._doc?.name}</b>
             </span>
             <span>
-              E-mail: <b>{currentUser._doc.email}</b>
+              E-mail: <b>{currentUser?._doc?.email}</b>
             </span>
             <button onClick={handleLogout}>Logout</button>
           </div>
@@ -62,6 +67,7 @@ const {updateUser, currentUser} = useContext(AuthContext)
         </div>
       </div>
     </div>
+    // )
   );
 }
 
