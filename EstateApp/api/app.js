1100eb0416ca5b699ba1e6969c2ab13cdd25c36b
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-23 10:29:15 +0300

    Update app.js

--- EstateApp/api/app.js
+++ EstateApp/api/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
-
+import userRoute from "./routes/user.route.js";
 import mongoose from "mongoose";
 import { PORT, MONGO_DB_CONNECTION_STRING } from "./constraints/constraints.js";
 
@@ -25,8 +25,9 @@ app.get("/", (req, res) => {
   res.send("Restful service");
 });
 
-app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 
 app.listen(PORT, () => {
