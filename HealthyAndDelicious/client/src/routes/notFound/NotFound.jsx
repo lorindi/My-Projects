1117165777149fba0aa887e9e2bec4f235251229
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-24 13:25:19 +0300

    Create NotFound.jsx

---
+++ HealthyAndDelicious/client/src/routes/notFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import "./NotFound.scss";
+
+function NotFound() {
+  return (
+    <div className="errorContainer">
+      <img className="errorImage" src="/404.png" alt="" />
+    </div>
+  );
+}
+
+export default NotFound;
