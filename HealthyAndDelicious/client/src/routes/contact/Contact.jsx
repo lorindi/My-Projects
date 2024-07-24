6dfb6a5343600ab99beb30fc1778d3473dfdd90e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-24 13:07:33 +0300

    Update Contact.jsx

--- HealthyAndDelicious/client/src/routes/contact/Contact.jsx
+++ HealthyAndDelicious/client/src/routes/contact/Contact.jsx
@@ -1,7 +1,28 @@
+import { Email } from "../../components/email/Email";
 import "./Contact.scss";
 
 function Contact() {
-  return <div className="contact">Contact</div>;
+  return (
+    <div className="containerContacts">
+      <div className="contentContacts">
+        <div className="contentContactsInfo">
+          <h1 className="contactInfoTitle">Contact Us</h1>
+          <div className="contactInfo">
+            <p>
+              <span>Address:</span> Your address, City, Country
+            </p>
+            <p>
+              <span>Phone:</span> Your phone number
+            </p>
+            <p>
+              <span>Email:</span> Your email address
+            </p>
+          </div>
+        </div>
+        <Email />
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
