335b4797f6d67dca3eead4aefafea729ac09cbcf
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-24 13:07:26 +0300

    Update About.jsx

--- HealthyAndDelicious/client/src/routes/about/About.jsx
+++ HealthyAndDelicious/client/src/routes/about/About.jsx
@@ -1,9 +1,42 @@
-import './About.scss'
+import "./About.scss";
 
 function About() {
   return (
-    <div className='about'>About</div>
-  )
+    <div className="containerAboutUsSection">
+      <div className="contentAboutUs">
+        <div className="aboutUsSection">
+          <h1 className="aboutUsSectionTitle">About Us</h1>
+          <p className="aboutUsSectionDescription aboutUsSectionDescriptionFirst">
+            Welcome to Healthy Delicious - your ultimate destination for
+            nutritious and mouthwatering recipes. Founded by Lora Mitova, a
+            passionate chef and nutrition enthusiast, our mission is to inspire
+            people to lead healthier lives through wholesome cooking.
+          </p>
+          <br />
+          <p className="aboutUsSectionDescription">
+            At Healthy Delicious, we believe that eating well should never mean
+            sacrificing flavor. That's why we're dedicated to bringing you a
+            diverse collection of recipes that are both good for your body and
+            tantalizing for your taste buds.
+          </p>
+          <br />
+          <p className="aboutUsSectionDescription">
+            Whether you're a seasoned chef or a kitchen novice, we've got
+            something for everyone. From quick and easy weekday meals to
+            impressive dishes for special occasions, our recipes are designed to
+            be accessible and enjoyable for all.
+          </p>
+          <br />
+
+          <p className="aboutUsSectionDescription">
+            Join us on this culinary journey towards a healthier, more vibrant
+            you. Let's make every meal a celebration of good food and good
+            health!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
