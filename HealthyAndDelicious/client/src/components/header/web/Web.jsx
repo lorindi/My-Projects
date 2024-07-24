1e5287c450c2bbe5adcca35a6b7af1c5d1483c8c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-24 14:22:03 +0300

    Update Web.jsx

--- HealthyAndDelicious/client/src/components/header/web/Web.jsx
+++ HealthyAndDelicious/client/src/components/header/web/Web.jsx
@@ -1,9 +1,41 @@
-import './Web.scss'
-
+import "./Web.scss";
+import { Link } from "react-router-dom";
 function Web() {
   return (
-    <nav>Web</nav>
-  )
+    <nav className="navigation">
+      <div className="contentLogo">
+        <Link className="logoLink" to="/">
+          Healthy Delicious
+        </Link>
+      </div>
+      <div className="contentLinks">
+        <Link className="link" to="/create-recipe">
+          Create Recipe
+        </Link>
+        <Link className="link" to="/recipes">
+          Recipes
+        </Link>
+        <Link className="link" to="/about">
+          About Us
+        </Link>
+        <Link className="link" to="/contact">
+          Contacts
+        </Link>
+
+        <Link className="linkSignIn" to="/sign-in">
+          Sign In
+        </Link>
+        <Link className="linkSignIn" to="/create-account">
+          Create Account
+        </Link>
+      </div>
+      <div className="contentLinksIcon">
+        <Link className="link" to="/profile">
+          <i className="fa-solid fa-user"></i>Profile
+        </Link>
+      </div>
+    </nav>
+  );
 }
 
-export default Web
+export default Web;
