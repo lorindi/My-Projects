d1e70e87c4c868d51e9cc20f100f1b82e8b6005e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-24 13:07:42 +0300

    Update Home.jsx

--- HealthyAndDelicious/client/src/routes/home/Home.jsx
+++ HealthyAndDelicious/client/src/routes/home/Home.jsx
@@ -1,9 +1,20 @@
-import './Home.scss'
+import "./Home.scss";
 
 function Home() {
   return (
-    <div className='home'>Home</div>
-  )
+    <div class="cover">
+      <div class="cover-title">
+        <p class="cover-title-text">All new recipes,</p>
+        <p class="cover-title-text">you can find </p>
+        <p class="cover-title-text">
+          only <span>here</span>!
+        </p>
+        <a class="about-recipe-link" routerLink="/dashboard">
+          Recipes
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
