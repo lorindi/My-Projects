189cbfa8b6891ef9fb3935ac3f325b3d834ecaeb
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-24 17:27:19 +0300

    Update Header.jsx

--- HealthyAndDelicious/client/src/components/header/Header.jsx
+++ HealthyAndDelicious/client/src/components/header/Header.jsx
@@ -1,7 +1,12 @@
 import "./Header.scss";
 import Web from "./web/Web";
 import Mobile from "./mobile/Mobile";
+import { useLocation } from "react-router-dom";
 function Header() {
+  const location = useLocation()
+  if (location.pathname === '/admin'){
+    return null
+  }
   return (
     <header className="header">
       <div className="web">
