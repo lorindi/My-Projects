a2f35bb65778a183cbb7aee37fe52bbc041de800
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-24 14:21:56 +0300

    Update Header.jsx

--- HealthyAndDelicious/client/src/components/header/Header.jsx
+++ HealthyAndDelicious/client/src/components/header/Header.jsx
@@ -1,16 +1,13 @@
 import "./Header.scss";
 import Web from "./web/Web";
-import Tablet from "./tablet/Tablet";
 import Mobile from "./mobile/Mobile";
 function Header() {
   return (
     <header className="header">
-      {/* <div className="web">
+      <div className="web">
         <Web />
       </div>
-      <div className="tablet">
-        <Tablet />
-      </div> */}
+ 
       <div className="mobile">
         <Mobile />
       </div>
