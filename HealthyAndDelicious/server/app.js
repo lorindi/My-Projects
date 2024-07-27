ff1301c22384f3f6036f0a305f23906ec1eecf72
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-28 00:20:03 +0300

    Update app.js

--- HealthyAndDelicious/server/app.js
+++ HealthyAndDelicious/server/app.js
@@ -2,5 +2,5 @@ import express from "express";
 const app = express();
 
 app.listen(5000, () => {
-  console.log("Server listening");
+  console.log("Restful server is listening on port 5000...");
 });
