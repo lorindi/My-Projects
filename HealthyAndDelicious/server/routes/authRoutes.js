b2d1febb5a381dcb8ceb66f99345b39e8f30e18f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-30 14:39:39 +0300

    Update authRoutes.js

--- HealthyAndDelicious/server/routes/authRoutes.js
+++ HealthyAndDelicious/server/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import {createAccount, signIn} from '../controllers/authController.js'
+import {createAccount, signIn, logout} from '../controllers/authController.js'
 const router = express.Router()
 
 router.use('/create-account', createAccount)
 router.use('/sign-in', signIn)
+router.use('logout', logout)
 
 export default router
