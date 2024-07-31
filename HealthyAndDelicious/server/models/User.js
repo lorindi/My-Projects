e424b1115e94fd4fc8203c65903f5406d0f8b49b
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-31 18:49:21 +0300

    Update User.js

--- HealthyAndDelicious/server/models/User.js
+++ HealthyAndDelicious/server/models/User.js
@@ -5,13 +5,34 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    minlength: 5,
+    match:  /^[a-zA-Z0-9]+@.[a-zA-Z0-9]+\.[a-zA-Z0-9]+(?:\.[a-zA-Z]+)*$/,
   },
   name: {
     type: String,
     required: true,
+    minlength: 2,
+    match: /\S{2,}/,
   },
   password: {
     type: String,
     required: true,
   },
+  avatar: {
+    type: String,
+    default: null,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+  }
+  next();
+});
+const User = mongoose.model("User", userSchema);
+export default User;
