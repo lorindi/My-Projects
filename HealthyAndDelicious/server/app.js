ec712572554b9f04b868ce64e17a2a7f8b06463f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-31 22:54:34 +0300

    Update app.js

--- HealthyAndDelicious/server/app.js
+++ HealthyAndDelicious/server/app.js
@@ -1,15 +1,19 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import router from "./routes/routes.js";
 import mongoose from "mongoose";
-
+import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/healthy-delicious-db")
   .then(() => console.log("DB Connected"))
   .catch((err) => console.log(err));
-  
+
+app.use(urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api", router);
 
