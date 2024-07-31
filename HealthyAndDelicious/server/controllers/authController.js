e993b2aa3812117f446f4f0f3461d57b8398c566
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-31 18:49:08 +0300

    User creation logic

--- HealthyAndDelicious/server/controllers/authController.js
+++ HealthyAndDelicious/server/controllers/authController.js
@@ -1,10 +1,36 @@
-export const createAccount = () => {
+import bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
+import User from "../models/User.js";
+// import {JWT_SECRET_KEY} from '../constraints/constraints.js'
 
+export const createAccount = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    //Check if the User already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    //Create a new user
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
+
+    //Save the user to the database
+    const saveUser = await newUser.save();
+
+    //log the save user for debugging
+    console.log("User created account successfully");
+
+    res.status(201).json({ message: "User created account successfully" });
+  } catch (err) {
+    console.log("Create account error", err);
+    res.status(500).json({ message: "Failed to create user" });
+  }
 };
 
-export const signIn = () => {
-    
-};
-export const logout = () => {
-    
-};
+export const signIn = (req, res) => {};
+export const logout = (req, res) => {};
