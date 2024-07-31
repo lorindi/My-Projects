4113e3f6660eeb9b21d726b4766289a5f0c3720a
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-07-31 22:33:19 +0300

    Update app.js

--- HealthyAndDelicious/server/app.js
+++ HealthyAndDelicious/server/app.js
@@ -1,11 +1,17 @@
 import express from "express";
 import router from "./routes/routes.js";
+import mongoose from "mongoose";
+
 const app = express();
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/healthy-delicious-db")
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
+  
 app.use(express.json());
 
-
-app.use('/api', router);
+app.use("/api", router);
 
 app.listen(5000, () => {
   console.log("Restful server is listening on port 5000...");
