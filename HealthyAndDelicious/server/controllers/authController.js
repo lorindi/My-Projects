01149067dcf67dbc77272130e083cc7b47beae33
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-02 13:07:12 +0300

    Update authController.js

--- HealthyAndDelicious/server/controllers/authController.js
+++ HealthyAndDelicious/server/controllers/authController.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
-import jsonwebtoken from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-// import {JWT_SECRET_KEY} from '../constraints/constraints.js'
+import { JWT_SECRET_KEY } from "../constraints/constraints.js";
 
 export const createAccount = async (req, res) => {
   try {
@@ -32,5 +32,35 @@ export const createAccount = async (req, res) => {
   }
 };
 
-export const signIn = (req, res) => {};
+export const signIn = async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).json({ message: "User is not found!" });
+
+    const passwordValid = await bcrypt.compare(password, user.password);
+    if (!passwordValid)
+      return res.status(401), json({ message: "Password is not valid!" });
+
+    const oneWeek = 1000 * 60 * 60 * 24 * 7;
+    const token = jwt.sign(
+      {
+        _id: user._id,
+        isAdmin: false,
+      },
+      JWT_SECRET_KEY,
+      { expiresIn: oneWeek }
+    );
+
+    const { password: userPassword, ...userInfo } = user;
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: oneWeek,
+    }).status(200).json(userInfo)
+  } catch (err) {
+    console.log(err);
+  }
+};
 export const logout = (req, res) => {};
