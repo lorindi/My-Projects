90bde31c528fff51ce4e75503675c8ede78362dc
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-07 16:12:57 +0300

    Create Restaurant.js

---
+++ HealthyAndDelicious/server/models/Restaurant.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+export const restaurantSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    unique: true,
+  },
+  location: {
+    type: String,
+    required: true,
+    minLength: 2,
+  },
+  address: {
+    type: String,
+    required: true,
+    minLength: 2,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: String,
+    default: Date.now,
+  },
+});
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
+export default Restaurant;
