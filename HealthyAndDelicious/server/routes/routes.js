20c074477a95a8e120971ca3cc9a86622b1d0542
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-08 17:59:48 +0300

    Update routes.js

--- HealthyAndDelicious/server/routes/routes.js
+++ HealthyAndDelicious/server/routes/routes.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import authRoutes from './authRoutes.js'
+import restaurantsRoutes from './restaurantsRoutes.js'
 import recipesRoutes from './recipesRoutes.js'
-
 const router = express.Router()
 
 router.use('/auth', authRoutes)
 router.use('/recipes', recipesRoutes)
-router.use('/restaurants', recipesRoutes)
+router.use('/restaurants', restaurantsRoutes)
 
 export default router
