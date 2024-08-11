849d56e470a804b0373ec99962001f345a050030
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-12 00:29:44 +0300

    Update user.controller.js

--- EstateApp/api/controllers/user.controller.js
+++ EstateApp/api/controllers/user.controller.js
@@ -52,52 +52,6 @@ export const updateUser = async (req, res) => {
 };
 
 
-// export const updateUser = async (req, res) => {
-//   const id = req.params.id;
-//   const tokenUserId = req.userId;
-//   const updates = req.body;
-
-//   // Authorization check
-//   if (id !== tokenUserId) {
-//     return res.status(403).json({ message: "Not Authorized" });
-//   }
-
-//   try {
-//     // Check if the updates include a password
-//     if (updates.password) {
-//       // Validate password (example: minimum length of 6)
-//       if (updates.password.length < 6) {
-//         return res.status(400).json({ message: "Password must be at least 6 characters long" });
-//       }
-
-//       // Hash the new password
-//       const hashedPassword = await bcrypt.hash(updates.password, 10);
-//       updates.password = hashedPassword;
-//     }
-
-//     // Update the user
-//     const updatedUser = await User.findByIdAndUpdate(
-//       id,
-//       { $set: updates }, // Only update fields provided in the request body
-//       { new: true, runValidators: true } // Return the updated document and apply schema validations
-//     );
-
-//     // Check if the user was found
-//     if (!updatedUser) {
-//       return res.status(404).json({ message: "User not found" });
-//     }
-//     //** User update password */
-
-//     // Send success response with updated user data
-//     res.status(200).json(updatedUser);
-//   } catch (err) {
-//     console.error("Update user error:", err);
-//     res.status(500).json({ message: "Failed to update user!" });
-//   }
-// };
-
-
-
 export const deleteUser = async (req, res) => {
   const id = req.params.id;
   const tokenUserId = req.userId;
