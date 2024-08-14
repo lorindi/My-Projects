ef57d7e843ff9648d8a46f4434ccb6ab9f9b1d29
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-14 20:27:58 +0300

    Create PostDetail.js

---
+++ EstateApp/api/models/PostDetail.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const postDetailSchema = new mongoose.Schema({
+  desc: {
+    type: String,
+    required: true,
+  },
+  utilities: {
+    type: String,
+  },
+  pet: {
+    type: String,
+  },
+  income: {
+    type: String,
+  },
+  size: {
+    type: Number,
+  },
+  school: {
+    type: Number,
+  },
+  bus: {
+    type: Number,
+  },
+  restaurant: {
+    type: Number,
+  },
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+  },
+});
+const PostDetail = mongoose.model("PostDetail", postDetailSchema);
+export default PostDetail;
