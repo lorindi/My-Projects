f603db8ba8065e3fd7a899ae9f1d86a06be22fe0
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-15 22:06:12 +0300

    Update Contact.jsx

--- EstateApp/client/src/components/routes/contact/Contact.jsx
+++ EstateApp/client/src/components/routes/contact/Contact.jsx
@@ -1,8 +1,28 @@
+import { Email } from "../../../components/email/Email";
+import "./Contact.scss";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="containerContacts">
+      <div className="contentContacts">
+        <div className="contentContactsInfo">
+          <h1 className="contactInfoTitle">Contact Us</h1>
+          <div className="contactInfo">
+            <p>
+              <span>Address:</span> Your address, City, Country
+            </p>
+            <p>
+              <span>Phone:</span> Your phone number
+            </p>
+            <p>
+              <span>Email:</span> Your email address
+            </p>
+          </div>
+        </div>
+        <Email />
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
