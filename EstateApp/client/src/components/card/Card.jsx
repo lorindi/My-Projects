df27e56ab5fcdb174ab62929d0ed9b1f7ff516dd
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-19 19:14:05 +0300

    Update Card.jsx

--- EstateApp/client/src/components/card/Card.jsx
+++ EstateApp/client/src/components/card/Card.jsx
@@ -4,7 +4,7 @@ function Card({ item }) {
   return (
     <div className="card">
       <Link to={`/${item.id}`} className="imageContainer">
-        <img src={item.img} alt="" />
+        <img src={item.images[0]} alt="" />
       </Link>
       <div className="textContainer">
         <h2 className="title">
