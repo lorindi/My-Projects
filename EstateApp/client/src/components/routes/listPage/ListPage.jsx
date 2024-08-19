829341f9d99ed313ae140450aa5c4ba6303a9b0f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-19 14:58:07 +0300

    Update ListPage.jsx

--- EstateApp/client/src/components/routes/listPage/ListPage.jsx
+++ EstateApp/client/src/components/routes/listPage/ListPage.jsx
@@ -3,19 +3,23 @@ import { listData } from "../../../lib/dummydata";
 import Filter from "../../filter/Filter";
 import Card from "../../card/Card";
 import Map from "../../map/Map";
+import { useLoaderData } from "react-router-dom";
+
 function ListPage() {
-  const data = listData;
+  const posts = useLoaderData();
   return (
     <div className="listPage">
       <div className="listContainer">
         <div className="wrapper">
           <Filter />
-          {data.map((item) => (
-            <Card key={item.id} item={item}/>
+          {posts.map((item) => (
+            <Card key={item.id} item={item} />
           ))}
         </div>
       </div>
-      <div className="mapContainer"><Map items={data}/></div>
+      <div className="mapContainer">
+        <Map items={posts} />
+      </div>
     </div>
   );
 }
