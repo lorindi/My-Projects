15dc82c6231638b0a454afd7271eeeb6bbc220ac
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-22 20:53:09 +0300

    Update SavedPost.js

--- EstateApp/api/models/SavedPost.js
+++ EstateApp/api/models/SavedPost.js
@@ -1,21 +1,23 @@
 import mongoose from "mongoose";
 
-const savedPost = new mongoose.Schema({
-  ownerId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  postId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Post",
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
-// savedPost.index({ ownerId: 1, postId: 1 }, { unique: true });
+const savedPost = new mongoose.Schema(
+  {
+    ownerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    postId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  }
+);
+
 const SavedPost = mongoose.model("SavedPost", savedPost);
 export default SavedPost;
