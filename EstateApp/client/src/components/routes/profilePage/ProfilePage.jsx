4514b39add666b4eab0412fa59df178f7ce0e937
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-22 20:53:35 +0300

    Update ProfilePage.jsx

--- EstateApp/client/src/components/routes/profilePage/ProfilePage.jsx
+++ EstateApp/client/src/components/routes/profilePage/ProfilePage.jsx
@@ -2,14 +2,18 @@ import Chat from "../../chat/Chat";
 import List from "../../list/List";
 import "./ProfilePage.scss";
 import apiRequest from "../../../lib/apiRequest";
-import { Link, useNavigate } from "react-router-dom";
-import { useContext, useEffect } from "react";
+import { Await, Link, useLoaderData, useNavigate } from "react-router-dom";
+import { Suspense, useContext, useEffect } from "react";
 import { AuthContext } from "../../../context/AuthContext";
 
 function ProfilePage() {
+  const data = useLoaderData();
+  console.log(data);
+  
+  
+
   const { updateUser, currentUser } = useContext(AuthContext);
   const navigate = useNavigate();
-console.log(updateUser, currentUser );
 
   useEffect(() => {
     if (!currentUser) {
@@ -29,45 +33,62 @@ console.log(updateUser, currentUser );
 
   return (
     currentUser && (
-    <div className="profilePage">
-      <div className="details">
-        <div className="wrapper">
-          <div className="title">
-            <h1>User Information</h1>
-           <Link to="/profile/update">
-           <button>Update Profile</button>
-           </Link>
+      <div className="profilePage">
+        <div className="details">
+          <div className="wrapper">
+            <div className="title">
+              <h1>User Information</h1>
+              <Link to="/profile/update">
+                <button>Update Profile</button>
+              </Link>
+            </div>
+            <div className="info">
+              <span>
+                Avatar:
+                <img src={currentUser?.avatar || "/noavatar.png"} alt="" />
+              </span>
+              <span>
+                Name: <b>{currentUser?.name}</b>
+              </span>
+              <span>
+                E-mail: <b>{currentUser?.email}</b>
+              </span>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+            <div className="title">
+              <h1>My List</h1>
+              <Link to="/add">Create New Post</Link>
+            </div>
+
+            <Suspense fallback={<p>Loading...</p>}>
+              <Await
+                resolve={data.postResponse}
+                errorElement={<p>Error loading posts!</p>}
+              >
+                {(postResponse) => <List posts = {postResponse.data.userPosts} />}
+                
+              </Await>
+            </Suspense>
+
+            <div className="title">
+              <h1>Saved List</h1>
+            </div>
+            <Suspense fallback={<p>Loading...</p>}>
+              <Await
+                resolve={data.postResponse}
+                errorElement={<p>Error loading posts!</p>}
+              >
+                {(postResponse) => <List posts = {postResponse.data.savedPosts} />}
+              </Await>
+            </Suspense>
           </div>
-          <div className="info">
-            <span>
-              Avatar:
-              <img src={currentUser?.avatar || "/noavatar.png"} alt="" />
-            </span>
-            <span>
-              Name: <b>{currentUser?.name}</b>
-            </span>
-            <span>
-              E-mail: <b>{currentUser?.email}</b>
-            </span>
-            <button onClick={handleLogout}>Logout</button>
+        </div>
+        <div className="chatContainer">
+          <div className="wrapper">
+            <Chat />
           </div>
-          <div className="title">
-            <h1>My List</h1>
-            <Link to="/add">Create New Post</Link>
-          </div>
-          <List />
-          <div className="title">
-            <h1>Saved List</h1>
-          </div>
-          <List />
         </div>
       </div>
-      <div className="chatContainer">
-        <div className="wrapper">
-          <Chat />
-        </div>
-      </div>
-    </div>
     )
   );
 }
