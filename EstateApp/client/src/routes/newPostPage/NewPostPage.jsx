c45d0dd7c0821bac63956a018816911d63bd463c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-22 22:46:03 +0300

    Change of location

--- EstateApp/client/src/routes/newPostPage/NewPostPage.jsx
+++ EstateApp/client/src/routes/newPostPage/NewPostPage.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-import apiRequest from "../../../lib/apiRequest";
-import UploadWidget from "../../uploadWidget/UploadWidget";
+import apiRequest from "../../lib/apiRequest";
+import UploadWidget from "../../components/uploadWidget/UploadWidget";
 import { useNavigate } from "react-router-dom";
 import './NewPostPage.scss'
 
