b5e9b37f4bd0947d8b67ec7722d5d09155b49301
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-22 20:53:13 +0300

    Update user.route.js

--- EstateApp/api/routes/user.route.js
+++ EstateApp/api/routes/user.route.js
@@ -1,17 +1,19 @@
 import express from "express";
 import { verifyToken } from "../middleware/verifyToken.js";
 import {
-  getUser,
+  // getUser,
   getUsers,
   updateUser,
   deleteUser,
   savePost,
+  profilePosts,
 } from "../controllers/user.controller.js";
 const router = express.Router();
 
 router.get("/", getUsers);
-router.get("/:id", verifyToken, getUser);
+// router.get("/search/:id", verifyToken, getUser);
 router.put("/:id", verifyToken, updateUser);
 router.delete("/:id", verifyToken, deleteUser);
 router.post("/save", verifyToken, savePost);
+router.get("/profilePosts/:id", verifyToken, profilePosts);
 export default router;
