22bdfafba1955bdecc8ac4a1836dba15ea57ac41
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-22 12:41:34 +0300

    Update loaders.js

--- EstateApp/client/src/lib/loaders.js
+++ EstateApp/client/src/lib/loaders.js
@@ -7,8 +7,11 @@ export const singlePageLoader = async ({ request, params }) => {
 
 export const listPageLoader = async ({ request, params }) => {
   const query = request.url.split("?")[1];
-  const postPromise = await apiRequest("/posts?" + query);
+  const postPromise = apiRequest("/posts?" + query);
   return defer({
     postResponse: postPromise,
   });
 };
+
+
+
