a12e642d93e6df757c29d396a72626145f4f3ea0
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-22 22:45:35 +0300

    Change of location

--- EstateApp/client/src/routes/listPage/ListPage.jsx
+++ EstateApp/client/src/routes/listPage/ListPage.jsx
@@ -1,7 +1,7 @@
 import "./listPage.scss";
-import Filter from "../../../components/filter/Filter";
-import Card from "../../../components/card/Card";
-import Map from "../../../components/map/Map";
+import Filter from "../../components/filter/Filter";
+import Card from "../../components/card/Card";
+import Map from "../../components/map/Map";
 import { Await, useLoaderData } from "react-router-dom";
 import { Suspense } from "react";
 
