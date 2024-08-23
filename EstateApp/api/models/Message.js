1f1ac912baa791b3b1843133e7f8a4ce318abf20
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-08-23 21:02:34 +0300

    Create Message.js

---
+++ EstateApp/api/models/Message.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  chatId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Chat",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
