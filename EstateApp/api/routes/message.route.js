78b2ae9030cfcc5b2079a1629c5e85f151e0c51c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-01 14:14:21 +0300

    Update message.route.js

--- EstateApp/api/routes/message.route.js
+++ EstateApp/api/routes/message.route.js
@@ -1,5 +1,8 @@
 import express from "express";
-
+import {verifyToken} from '../middleware/verifyToken.js'
+import {addMessage} from '../controllers/message.controllers.js'
 const router = express.Router();
+
+router.post("/:chatId", verifyToken, addMessage)
 
 export default router;
