5f98ad9f77046e9c17371a1f23595f5f350f0b9e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-01 14:14:16 +0300

    Update chat.route.js

--- EstateApp/api/routes/chat.route.js
+++ EstateApp/api/routes/chat.route.js
@@ -1,5 +1,16 @@
 import express from "express";
-
+import { verifyToken } from "../middleware/verifyToken.js";
+import {
+  getChats,
+  getChat,
+  addChat,
+  readChat,
+} from "../controllers/chat.controllers.js";
 const router = express.Router();
+
+router.get("/", verifyToken, getChats);
+router.get("/:id", verifyToken, getChat);
+router.post("/", verifyToken, addChat);
+router.put("/read/:id", verifyToken, readChat);
 
 export default router;
