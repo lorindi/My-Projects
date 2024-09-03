b98ec4223106845ee9859dd501c3014bd69f17d9
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-03 12:57:49 +0300

    Update SocketContext.jsx

--- EstateApp/client/src/context/SocketContext.jsx
+++ EstateApp/client/src/context/SocketContext.jsx
@@ -1,21 +1,25 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
+import { AuthContext } from "./AuthContext";
 const SocketContext = createContext();
 
 const SocketContextProvider = ({ children }) => {
+  const { currentUser } = useContext(AuthContext);
   const [socket, setSocket] = useState(null);
 
-
   useEffect(() => {
-    setSocket(io("http://localhost:4000"))
+    setSocket(io("http://localhost:4000"));
   }, []);
 
+  useEffect(() => {
+    currentUser && socket?.emit("newUser", currentUser._id);
+  }, [currentUser, socket]);
+
   return (
-    <SocketContext.Provider value={{socket}}>
+    <SocketContext.Provider value={{ socket }}>
       {children}
     </SocketContext.Provider>
   );
 };
 
-
-export {SocketContext, SocketContextProvider};
+export { SocketContext, SocketContextProvider };
