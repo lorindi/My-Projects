a8180eaf7d01557c52d42b7506081c95ec2c2a19
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-03 12:43:09 +0300

    Update app.js

--- EstateApp/socket/app.js
+++ EstateApp/socket/app.js
@@ -5,6 +5,36 @@ const io = new Server({
     origin: "http://localhost:5173",
   },
 });
+
+let onlineUser = [];
+
+const addUser = (userId, socketId) => {
+  const userExits = onlineUser.find((user) => user.userId === userId);
+  if (!userExits) {
+    onlineUser.push({ userId, socketId });
+  }
+};
+
+const removeUser = (socketId) => {
+  onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return onlineUser.find((user) => user.userId === userId);
+};
+
 io.on("connection", (socket) => {
-  console.log(socket);
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+  });
+
+  socket.on("sendMessage", ({receiverId, data})=>{
+    const receiver = getUser(receiverId)
+    io.to(receiver.socketId).emit("getMessage", data)
+  })
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
 });
+io.listen("4000");
