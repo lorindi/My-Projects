c9b3ca833b1549fd0c51ecbc71426d2e59dd0be9
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-03 13:58:18 +0300

    Create notificationStore.js

---
+++ EstateApp/client/src/lib/notificationStore.js
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+
+export const useNotificationStore = create((set) => ({
+  number: 0,
+  fetch: async () => {
+    const res = await apiRequest("/users/notification");
+    set({ number: res.data });
+  },
+  decrease: () => {
+    set((prev) => ({ number: prev.number - 1 }));
+  },
+  reset: () => {
+    set({ number: 0 });
+  },
+}));
