70d96b0036d98fa1de61abb055a49fae6e4f0221
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-04 15:18:29 +0300

    Create ArrowUndoOutline.jsx

---
+++ EstateApp/client/src/components/Svg/ArrowUndoOutline.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function ArrowUndoOutline() {
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="icons / arrow-undo-outline">
+        <path
+          id="Vector"
+          d="M14.0625 24.8438V19.2188C20.8828 19.2188 23.4018 21.1969 26.25 24.8438C26.25 17.8576 23.9314 10.7812 14.0625 10.7812V5.15625L3.75 15L14.0625 24.8438Z"
+          stroke="#13294B"
+          strokeLinejoin="round"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default ArrowUndoOutline;
