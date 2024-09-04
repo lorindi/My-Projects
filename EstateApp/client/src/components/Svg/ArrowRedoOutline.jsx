211d4de0438f41ba214d8d0bffabd1e12894ce53
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-04 15:18:26 +0300

    Create ArrowRedoOutline.jsx

---
+++ EstateApp/client/src/components/Svg/ArrowRedoOutline.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function ArrowRedoOutline() {
+  return (
+    <svg
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="icons / arrow-redo-outline">
+        <path
+          id="Vector"
+          d="M26.25 15L15.9375 5.15625V10.7812C6.06856 10.7812 3.75 17.8576 3.75 24.8438C6.59824 21.1969 9.11719 19.2188 15.9375 19.2188V24.8438L26.25 15Z"
+          stroke="#13294B"
+          strokeLinejoin="round"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default ArrowRedoOutline;
