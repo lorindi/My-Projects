db3616f33833d63937171698021f3ea1a2efd484
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-04 15:19:08 +0300

    Create SeeMoreSvgHomePage.jsx

---
+++ EstateApp/client/src/components/Svg/SeeMoreSvgHomePage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function SeeMoreSvgHomePage({className}) {
+  return (
+    <svg
+      className={className}
+      viewBox="0 0 25 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="icons / chevron-forward">
+        <path
+          id="Vector (Stroke)"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M8.3295 4.4545C8.76884 4.01517 9.48116 4.01517 9.9205 4.4545L16.6705 11.2045C17.1098 11.6438 17.1098 12.3562 16.6705 12.7955L9.9205 19.5455C9.48116 19.9848 8.76884 19.9848 8.3295 19.5455C7.89017 19.1062 7.89017 18.3938 8.3295 17.9545L14.284 12L8.3295 6.0455C7.89017 5.60616 7.89017 4.89384 8.3295 4.4545Z"
+          fill="#081120"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default SeeMoreSvgHomePage;
