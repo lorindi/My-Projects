d2f881ae6bf6fb40493fa160a73561311cacd872
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-04 15:18:21 +0300

    Update Footer.jsx

--- EstateApp/client/src/components/footer/Footer.jsx
+++ EstateApp/client/src/components/footer/Footer.jsx
@@ -1,9 +1,109 @@
-import React from 'react'
-
+import "./Footer.scss";
+import CallOutline from "../Svg/CallOutline";
+import MailOutline from "../Svg/MailOutline";
+import HelpCircleOutline from "../Svg/HelpCircleOutline";
+import MapOutline from "../Svg/MapOutline";
+import Instagram from "../Svg/Instagram";
+import Facebook from "../Svg/Facebook";
+import Linkedin from "../Svg/Linkedin";
+import { Link } from "react-router-dom";
 function Footer() {
   return (
-    <div className='footer'>Footer</div>
-  )
+    <div className="footer">
+      <div className="contentContactsLogoAddress">
+        <ul className="contacts">
+          <li className="">
+            <CallOutline />
+            <p className="">Call +359 899 999 999</p>
+          </li>
+          <li className="">
+            <MailOutline />
+            <p className="">support@estate.com</p>
+          </li>
+          <li className="">
+            <HelpCircleOutline />
+            <p className="">Help</p>
+          </li>
+          <li className="">
+            <MapOutline />
+            <p className="">About</p>
+          </li>
+        </ul>
+
+        <div className="contentLogoSocial">
+          <Link class="logo" href="/">
+            <img src="/logo.png" alt="Logo" />
+            <span>LoEstate</span>
+          </Link>
+
+          <div className="social">
+            <a href="#">
+              <Instagram />
+            </a>
+            <a href="#">
+              <Facebook />
+            </a>
+            <a href="#">
+              <Linkedin />
+            </a>
+          </div>
+        </div>
+        <div className="address">
+
+        </div>
+      </div>
+      {/* <div className="instagramImgs">
+        <img
+          src="https://images.unsplash.com/photo-1570129477492-45c003edd2be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1580587771525-78b9dba3b914?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1582268611958-ebfd161ef9cf?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1505843795480-5cfb3c03f6ff?q=80&w=1933&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1554995207-c18c203602cb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1492889971304-ac16ab4a4a5a?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1489370321024-e0410ad08da4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1524813686514-a57563d77965?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1613490493576-7fde63acd811?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1464146072230-91cabc968266?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1558036117-15d82a90b9b1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <img
+          src="https://images.unsplash.com/photo-1556912173-3bb406ef7e77?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+      </div> */}
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
