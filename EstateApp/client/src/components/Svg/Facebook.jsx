1dbc06f13c612df4e8495ad36d8d5346a2236831
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-04 15:18:43 +0300

    Create Facebook.jsx

---
+++ EstateApp/client/src/components/Svg/Facebook.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Facebook() {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="icons / logo-facebook">
+        <path
+          id="Vector"
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M37.5 20.106C37.5 10.4419 29.6641 2.60596 20 2.60596C10.3359 2.60596 2.5 10.4419 2.5 20.106C2.5 28.8403 8.89844 36.0802 17.2656 37.3942V25.1661H12.8211V20.106H17.2656V16.2505C17.2656 11.8653 19.8789 9.44111 23.8758 9.44111C25.7906 9.44111 27.7938 9.7833 27.7938 9.7833V14.0903H25.5859C23.4133 14.0903 22.7336 15.4388 22.7336 16.8247V20.106H27.5867L26.8117 25.1661H22.7344V37.3958C31.1016 36.0825 37.5 28.8427 37.5 20.106Z"
+          fill="black"
+       
+          
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default Facebook;
