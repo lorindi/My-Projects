f340b60e5cc4ecf9a55c4167aab5afa6bf5c0219
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-08 16:18:49 +0300

    Update HomePage.jsx

--- EstateApp/client/src/routes/homePage/HomePage.jsx
+++ EstateApp/client/src/routes/homePage/HomePage.jsx
@@ -1,9 +1,9 @@
-import { useContext } from "react";
+// import { useContext } from "react";
 import SearchBar from "../../components/searchBar/SearchBar";
 import "./HomePage.scss";
-import { AuthContext } from "../../context/AuthContext";
+// import { AuthContext } from "../../context/AuthContext";
 function HomePage() {
-  const {currentUser} = useContext(AuthContext)
+  // const {currentUser} = useContext(AuthContext)
   // console.log(currentUser);
   return (
     <div className="homePage">
