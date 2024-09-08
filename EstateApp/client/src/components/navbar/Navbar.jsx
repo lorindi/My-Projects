598864c33f3a016d579ac52dd1f1d939bcc3ec61
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-08 16:18:32 +0300

    Update Navbar.jsx

--- EstateApp/client/src/components/navbar/Navbar.jsx
+++ EstateApp/client/src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { useNotificationStore } from "../../lib/notificationStore";
 function Navbar() {
   const [open, setOpen] = useState(false);
   const { currentUser } = useContext(AuthContext);
-  const fetch = useNotificationStore((state) => state.fetch);
+  // const fetch = useNotificationStore((state) => state.fetch);
   const number = useNotificationStore((state) => state.fetch);
 
   return (
