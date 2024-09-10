79ea47000208c3d51c8aca2cea71297438f86497
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-10 22:56:36 +0300

    Update vite.config.js

--- EstateApp/client/vite.config.js
+++ EstateApp/client/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import sassDts from 'vite-plugin-sass-dts';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(), 
+    sassDts()
+  ],
+});
