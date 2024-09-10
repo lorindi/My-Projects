cc56329406f68b37aed465e7e8e2a941649a5059
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-10 12:51:54 +0300

    Create TypeCard.jsx

---
+++ EstateApp/client/src/components/cards/TypeCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './TypeCard.scss'
+
+function TypeCard({ cardClass, imageSrc, title, city, price, content, type }) {
+  return (
+    <div className={`card ${cardClass}`}>
+      <img src={imageSrc} alt="" />
+      <h2 className="cardTitle">{title}</h2>
+      <p className="cardContent">{content}</p>
+    </div>
+  );
+}
+
+export default TypeCard;
