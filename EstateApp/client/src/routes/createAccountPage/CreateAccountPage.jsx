b13c2766975a632c4730234f04fdc45de92ea32d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-11 16:11:54 +0300

    Create CreateAccountPage.jsx

---
+++ EstateApp/client/src/routes/createAccountPage/CreateAccountPage.jsx
@@ -0,0 +1,56 @@
+import "./CreateAccountPage.scss";
+import { Link, useNavigate } from "react-router-dom";
+import apiRequest from "../../lib/apiRequest";
+import { useState } from "react";
+
+function CreateAccountPage() {
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setError("")
+
+    const formData = new FormData(e.target);
+
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const res = await apiRequest.post("/auth/register", {
+        name,
+        email,
+        password,
+      });
+      console.log(res.data);
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+      setError(err.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className="registerPage">
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <h1>Create an Account</h1>
+          <input name="name" type="text" placeholder="Name" />
+          <input name="email" type="text" placeholder="Email" />
+          <input name="password" type="password" placeholder="Password" />
+          <button disabled={isLoading}>Register</button>
+          {error && <span>{error}</span>}
+          <Link to="/login">Do you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+export default CreateAccountPage;
