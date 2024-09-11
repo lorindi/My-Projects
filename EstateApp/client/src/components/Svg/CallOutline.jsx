0ad94eab948f85f988cbe5b2f23a49746b462da9
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-11 18:03:21 +0300

    Update CallOutline.jsx

--- EstateApp/client/src/components/Svg/CallOutline.jsx
+++ EstateApp/client/src/components/Svg/CallOutline.jsx
@@ -1,8 +1,7 @@
-function CallOutline() {
+function CallOutline({ className }) {
   return (
     <svg
-      width="24"
-      height="24"
+      className={className}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
