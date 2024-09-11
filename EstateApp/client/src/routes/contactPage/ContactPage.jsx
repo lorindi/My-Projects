47cebcb900d60c8f677775ea90487a42eb4b7b0e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-11 18:03:25 +0300

    Update ContactPage.jsx

--- EstateApp/client/src/routes/contactPage/ContactPage.jsx
+++ EstateApp/client/src/routes/contactPage/ContactPage.jsx
@@ -1,29 +1,32 @@
 import { Email } from "../../components/email/Email";
 import "./ContactPage.scss";
-
+import CallOutline from "../../components/Svg/CallOutline";
+import Instagram from "../../components/Svg/Instagram";
+import Facebook from "../../components/Svg/Facebook";
+import Linkedin from "../../components/Svg/Linkedin";
 function Contact() {
   return (
     <div className="containerContacts">
       <div className="contentContacts">
-
-        <div className="contentContactsInfo">
-          <h1 className="contactInfoTitle">Contact Us</h1>
-          <div className="contactInfo">
-            <p>
-              <span>Address:</span> Your address, City, Country
-            </p>
-            <p>
-              <span>Phone:</span> Your phone number
-            </p>
-            <p>
-              <span>Email:</span> Your email address
-            </p>
-          </div>
-        </div>
-        
-        <div className="emailContainer">
+        <h1>Contact Us</h1>
         <Email />
+        <div className="linksBlank">
+          <a href="#" target="_blank">
+            <CallOutline className="callOutlineContacts" />
+          </a>
+          <a href="#" target="_blank">
+            <Instagram />
+          </a>
+          <a href="#" target="_blank">
+            <Facebook />
+          </a>
+          <a href="#" target="_blank">
+            <Linkedin />
+          </a>
         </div>
+      </div>
+      <div className="contentImg">
+        <img src="/bg.png" alt="" />
       </div>
     </div>
   );
