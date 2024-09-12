5deb5a406b83df6167112a1b687870c2754664a7
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-12 19:45:28 +0300

    Update HomePage.jsx

--- EstateApp/client/src/routes/homePage/HomePage.jsx
+++ EstateApp/client/src/routes/homePage/HomePage.jsx
@@ -1,10 +1,48 @@
 // import { useContext } from "react";
 import SearchBar from "../../components/searchBar/SearchBar";
 import "./HomePage.scss";
-// import { AuthContext } from "../../context/AuthContext";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import apiRequest from '../../lib/apiRequest'
 function HomePage() {
-  // const {currentUser} = useContext(AuthContext)
-  // console.log(currentUser);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get("/api/posts", {
+          params: {
+            maxPrice: maxPrice,
+          },
+        });
+        setPosts(response.data);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      }
+    };
+
+    fetchPosts();
+  }, [maxPrice]);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // Number of slides to show at once
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
   return (
     <div className="homePage">
       <div className="textContainer">
