ced1d4c1fd12ce2011e30603de2e7a15236ee474
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-15 23:59:36 +0300

    Update TypeCard.jsx

--- EstateApp/client/src/components/cards/TypeCard.jsx
+++ EstateApp/client/src/components/cards/TypeCard.jsx
@@ -1,14 +1,30 @@
 import "./TypeCard.scss";
+import { motion } from "framer-motion";
 
+const cardVariants = {
+  hidden: {
+    opacity: 0,
+    y: 50,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      ease: "easeOut",
+    },
+  },
+};
 function TypeCard({ post, className, onClick }) {
 
   return (
-    <div className={`${className}`} onClick={onClick}>
+    <motion.div className={`${className}`} onClick={onClick} variants={cardVariants}
+    whileHover={{ scale: 1.05 }}>
       <img src={post.images[0]} alt="" />
       <div>
         <span>{post.city}</span>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
