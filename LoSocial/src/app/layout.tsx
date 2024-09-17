f456ca32f3bfc76bd067ba347a9e1238672cc0fa
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-17 22:51:06 +0300

    Update layout.tsx

--- LoSocial/src/app/layout.tsx
+++ LoSocial/src/app/layout.tsx
@@ -6,7 +6,7 @@ import Navbar from "./components/navbar/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev Social Media App",
+  title: "Social Media App",
   description: "Social media app built with Next.js",
 };
 
