49db815d30abd5bccb46e86b7b13bf613ff6e8f7
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-17 21:37:02 +0300

    Create MobileMenu.tsx

---
+++ LoSocial/src/app/components/navbar/MobileMenu.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+
+function MobileMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="md:hidden">
+      <div
+        className="flex flex-col gap-[4.5px] cursor-pointer z-20"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "rotate-45" : ""
+          } origin-left ease-in-out duration-500`}
+        />
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "opacity-0" : ""
+          } ease-in-out duration-500`}
+        />
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "-rotate-45" : ""
+          } origin-left ease-in-out duration-500`}
+        />
+      </div>
+      {isOpen && (
+        <div className="absolute left-0 top-[96px] w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10">
+          <Link href="/">Home</Link>
+          <Link href="/">Friends</Link>
+          <Link href="/">Groups</Link>
+          <Link href="/">Stories</Link>
+          <Link href="/">Login</Link>
+          {/* <Link href="/">z</Link>
+            <Link href="/">z</Link> */}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default MobileMenu;
