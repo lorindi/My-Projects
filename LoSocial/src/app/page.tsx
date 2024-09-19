841bd5bf071b71bc412d96e3899a7d513149e908
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-19 15:20:45 +0300

    Update page.tsx

--- LoSocial/src/app/page.tsx
+++ LoSocial/src/app/page.tsx
@@ -1,7 +1,27 @@
+import AddPost from "./components/homepieces/AddPost";
+import Feed from "./components/homepieces/Feed";
+import LeftMenu from "./components/homepieces/LeftMenu";
+import RightMenu from "./components/homepieces/RightMenu";
+import Stories from "./components/homepieces/Stories";
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div className="flex gap-6 pt-6">
+      <div className="hidden xl:block w-[20%]">
+        <LeftMenu />
+      </div>
+      <div className="w-full lg:w-[70%] xl:w-[50%]">
+        <div className="flex flex-col gap-6">
+          <Stories />
+          <AddPost />
+          <Feed />
+        </div>
+      </div>
+      <div className="hidden lg:block w-[30%]">
+        <RightMenu />
+      </div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
