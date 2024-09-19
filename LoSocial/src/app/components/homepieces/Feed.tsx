ee0daea11b923e49be303cc9466a85ffa2e78717
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-19 18:47:28 +0300

    Update Feed.tsx

--- LoSocial/src/app/components/homepieces/Feed.tsx
+++ LoSocial/src/app/components/homepieces/Feed.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Post from "./Post";
 
 function Feed() {
   return (
-    <div>Feed</div>
-  )
+    <div className="flex flex-col gap-1 rounded-lg bg-[#F1F5F9] mb-10">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
