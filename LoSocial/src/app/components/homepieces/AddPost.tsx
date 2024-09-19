3470a628139833c2d86bd39048f8e02c09c18d4b
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-19 16:27:42 +0300

    Styling the post creation component

--- LoSocial/src/app/components/homepieces/AddPost.tsx
+++ LoSocial/src/app/components/homepieces/AddPost.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 function AddPost() {
   return (
-    <div>AddPost</div>
-  )
+    <div className="flex justify-between text-sm p-4 bg-white rounded-sm gap-4">
+      {/* AVATAR */}
+      <Image
+        src="https://images.pexels.com/photos/28210217/pexels-photo-28210217/free-photo-of-a-mountain-covered-in-clouds-and-fog.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+        width={48}
+        height={48}
+        className="w-12 h-12 object-cover rounded-full"
+      />
+
+      {/* POST */}
+      <div className="flex-1">
+        {/* TEXT INPUT */}
+        <div className="flex gap-4 ">
+          <textarea
+            name=""
+            id=""
+            placeholder="What's on your mind?"
+            className="bg-slate-100 rounded-lg flex-1 p-2"
+          ></textarea>
+          <Image
+            src="/emoji.png"
+            alt=""
+            width={20}
+            height={20}
+            className="w-5 h-5 cursor-pointer self-end"
+          />
+        </div>
+
+        {/* POST OPTIONS */}
+        <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addimage.png" alt="" width={20} height={20} />
+            Photo
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addVideo.png" alt="" width={20} height={20} />
+            Video
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addevent.png" alt="" width={20} height={20} />
+            Event
+          </div>
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/poll.png" alt="" width={20} height={20} />
+            Poll
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default AddPost
+export default AddPost;
