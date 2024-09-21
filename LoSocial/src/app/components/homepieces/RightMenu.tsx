1a5e49e3accb1014929aadbfde9525598a0fa102
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-21 14:28:15 +0300

    Update RightMenu.tsx

--- LoSocial/src/app/components/homepieces/RightMenu.tsx
+++ LoSocial/src/app/components/homepieces/RightMenu.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import FriendsRequests from "./FriendsRequests";
+import Birthdays from "./Birthdays";
+import Ad from "./Ad";
 
-function RightMenu() {
+function RightMenu({ userId }: { userId: string }) {
   return (
-    <div>RightMenu</div>
-  )
+    <div className="flex flex-col gap-6">
+      <FriendsRequests />
+      <Birthdays />
+      <Ad size="md"/>
+    </div>
+  );
 }
 
-export default RightMenu
+export default RightMenu;
