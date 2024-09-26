1d090edc8eeb3b11c8646d677a24337b0fe2eb2e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-26 22:33:48 +0300

    Update LeftMenu.tsx

--- LoSocial/src/app/components/LeftMenu.tsx
+++ LoSocial/src/app/components/LeftMenu.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
-function LeftMenu() {
+import React from "react";
+import ProfileCard from "./ProfileCard";
+import Link from "next/link";
+import Image from "next/image";
+function LeftMenu({ type }: { type: "home" | "profile" }) {
   return (
-    <div>LeftMenu</div>
-  )
+    <div className="flex flex-col gap-6">
+      {type === "home" && <ProfileCard />}
+
+      <div className="p-4 bg-white rounded-lg shadow-md text-sm text-gray-500 flex flex-col gap-2">
+        <Link
+          href="/"
+          className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+        >
+          <Image
+            src="/posts.png"
+            alt=""
+            width={20}
+            height={20}
+            className="w-10 h-10 object-cover rounded-full"
+          />
+          <span>My Posts</span>
+        </Link>
+        <hr className="border-t-1 border-gray-50 w-36 self-center"/>
+      </div>
+
+      {/* {type==="profile" && (
+        
+      )} */}
+    </div>
+  );
 }
 
-export default LeftMenu
+export default LeftMenu;
