1bdbb9b6153f76cee3514f457b1f12c95cce4db8
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-09-28 23:17:33 +0300

    Update ProfileCard.jsx

--- LoSocial/src/app/components/ProfileCard.jsx
+++ LoSocial/src/app/components/ProfileCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ProfileCard() {
   return (
-    <div>ProfileCard</div>
+    <div className='p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6'>ProfileCard</div>
   )
 }
 
