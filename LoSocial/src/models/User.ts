29387bb78dd48e8fd9e1a55e89e6037054b1e2b1
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-03 09:55:51 +0300

    Create User.ts

---
+++ LoSocial/src/models/User.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+export interface IUser extends mongoose.Document {
+  username: string;
+  email: string;
+  password?: string;
+  clerkId: string;
+}
+
+const userSchema: mongoose.Schema<IUser> = new mongoose.Schema({
+  username: { 
+    type: String, 
+    required: true 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String,
+  },
+  clerkId: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  }
+});
+
+// Проверка дали моделът вече съществува
+const User = mongoose.models.User || mongoose.model<IUser>("User", userSchema);
+
+export default User;
