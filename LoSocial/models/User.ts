28d523bd3ff34bb788c6600b61760af9afc587ba
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-05 01:39:28 +0300

    Update User.ts

--- LoSocial/models/User.ts
+++ LoSocial/models/User.ts
@@ -1,13 +1,28 @@
 import mongoose from "mongoose";
 
 export interface IUser extends mongoose.Document {
+  clerkId: string;
   username: string;
   email: string;
   password?: string;
-  clerkId: string;
+  avatar?: string;
+  cover?: string;
+  name?: string;
+  surname?: string;
+  description?: string;
+  city?: string;
+  school?: string;
+  work?: string;
+  website?: string;
+  createdAt: Date; 
 }
 
 const userSchema: mongoose.Schema<IUser> = new mongoose.Schema({
+  clerkId: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
   username: { 
     type: String, 
     required: true 
@@ -20,11 +35,37 @@ const userSchema: mongoose.Schema<IUser> = new mongoose.Schema({
   password: { 
     type: String,
   },
-  clerkId: { 
-    type: String, 
-    required: true, 
-    unique: true 
-  }
+  avatar: {
+    type: String,
+  },
+  cover: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  surname: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  school: {
+    type: String,
+  },
+  work: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const User = mongoose.models.User || mongoose.model<IUser>("User", userSchema);
