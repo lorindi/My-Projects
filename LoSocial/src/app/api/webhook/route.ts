2d7f53e2c8801176ebf176807492c016de087538
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-04 21:09:11 +0300

    Update route.ts

--- LoSocial/src/app/api/webhook/route.ts
+++ LoSocial/src/app/api/webhook/route.ts
@@ -2,25 +2,23 @@ import { WebhookEvent } from "@clerk/clerk-sdk-node";
 import { NextRequest, NextResponse } from "next/server";
 import connectionToDatabase from "../../../../lib/mongoose";
 import User from "../../../../models/User";
-
+// lt --port 3000
 export async function POST(req: NextRequest) {
-  try {
-    await connectionToDatabase();
-    
+  await connectionToDatabase();
 
+  try {
     const body = await req.json();
     const event: WebhookEvent = body;
 
     // Обработка на събитие за създаване на потребител
     if (event.type === "user.created") {
-      const { id, email_addresses, first_name, last_name } = event.data;
+      const { id, email_addresses, first_name, last_name, username } = event.data;
       console.log(email_addresses);
-      
 
       const email = email_addresses[0]?.email_address;
 
       const newUser = new User({
-        username: `${first_name} ${last_name}`,
+        username: `${username}`,
         email: email,
         clerkId: id,
       });
@@ -42,7 +40,10 @@ export async function POST(req: NextRequest) {
         { new: true }
       );
 
-      return NextResponse.json({ message: "User updated in MongoDB", user: updatedUser }, { status: 200 });
+      return NextResponse.json(
+        { message: "User updated in MongoDB", user: updatedUser },
+        { status: 200 }
+      );
     }
 
     // Обработка на събитие за изтриване на потребител
@@ -52,12 +53,18 @@ export async function POST(req: NextRequest) {
       await User.deleteOne({ clerkId: id });
 
       // Върни 204 статус без тяло
-      return NextResponse.json({ message: "User updated in MongoDB"}, { status: 204 });
+      return NextResponse.json(
+        { message: "User updated in MongoDB" },
+        { status: 204 }
+      );
     }
 
     return NextResponse.json({ message: "Unhandled event" }, { status: 400 });
   } catch (err) {
     console.error("Error processing Clerk webhook:", err);
-    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
