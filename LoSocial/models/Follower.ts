a41260691cc8fbf47cb4c749354e6e98b44c3e07
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-05 21:08:06 +0300

    Update Follower.ts

--- LoSocial/models/Follower.ts
+++ LoSocial/models/Follower.ts
@@ -2,14 +2,14 @@ import { model, Schema, Types } from "mongoose";
 
 interface IFollower {
   createdAt: Date;
-  follower: Types.ObjectId;
-  following: Types.ObjectId;
+  followerId: Types.ObjectId;
+  followingId: Types.ObjectId;
 }
 
 const followerSchema = new Schema<IFollower>({
   createdAt: { type: Date, default: Date.now },
-  follower: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  following: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  followerId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  followingId: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 const Follower = model<IFollower>("Follower", followerSchema);
 export default Follower;
