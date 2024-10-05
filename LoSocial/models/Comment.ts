ab32604805aa86eb49016560187eb5bdd22c422d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-05 21:02:27 +0300

    Update Comment.ts

--- LoSocial/models/Comment.ts
+++ LoSocial/models/Comment.ts
@@ -4,8 +4,8 @@ interface IComment {
   desc: string;
   createdAt: Date;
   updatedAt: Date;
-  user: Types.ObjectId;
-  post: Types.ObjectId;
+  userId: Types.ObjectId;
+  postId: Types.ObjectId;
   likes: Types.ObjectId[];
 }
 
@@ -13,8 +13,8 @@ const commentSchema = new Schema<IComment>({
   desc: { type: String },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  postId: { type: Schema.Types.ObjectId, ref: "Post", required: true },
   likes: [{ type: Schema.Types.ObjectId, ref: "Like" }]
 });
 const Comment = model<IComment>("Comment", commentSchema);
