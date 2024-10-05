d771db846d636733d4ce86e66eb33a1f95c77079
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-05 21:02:29 +0300

    Update FollowRequest.ts

--- LoSocial/models/FollowRequest.ts
+++ LoSocial/models/FollowRequest.ts
@@ -2,14 +2,14 @@ import { Schema, model, Types } from "mongoose";
 
 interface IFollowRequest {
   createdAt: Date;
-  sender: Types.ObjectId;
-  receiver: Types.ObjectId;
+  senderId: Types.ObjectId;
+  receiverId: Types.ObjectId;
 }
 
 const followRequestSchema = new Schema<IFollowRequest>({
   createdAt: { type: Date, default: Date.now },
-  sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  receiver: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  senderId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  receiverId: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 const FollowRequest = model<IFollowRequest>(
   "FollowRequest",
