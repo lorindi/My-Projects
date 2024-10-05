296430193ed64938c302fce145c8543e6ff9f9fa
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-05 21:02:32 +0300

    Update Like.ts

--- LoSocial/models/Like.ts
+++ LoSocial/models/Like.ts
@@ -2,16 +2,16 @@ import { Schema, model, Types } from "mongoose";
 
 interface ILike {
   createdAt: Date;
-  user: Types.ObjectId;
-  post?: Types.ObjectId;
-  comment?: Types.ObjectId;
+  userId: Types.ObjectId;
+  postId?: Types.ObjectId;
+  commentId?: Types.ObjectId;
 }
 
 const likeSchema = new Schema<ILike>({
   createdAt: { type: Date, default: Date.now },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  post: { type: Schema.Types.ObjectId, ref: "Post" },
-  comment: { type: Schema.Types.ObjectId, ref: "Comment" },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  postId: { type: Schema.Types.ObjectId, ref: "Post" },
+  commentId: { type: Schema.Types.ObjectId, ref: "Comment" },
 });
 
 const Like = model<ILike>("Like", likeSchema);
