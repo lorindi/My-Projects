f887251b40fa4b9bd7da902ba569efd7761bc736
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-05 14:03:32 +0300

    Create Post.ts

---
+++ LoSocial/models/Post.ts
@@ -0,0 +1,24 @@
+import { Schema, model, Types } from "mongoose";
+
+interface IPost {
+  desc: string;
+  img?: string;
+  createdAt: Date;
+  updatedAt: Date;
+  user: Types.ObjectId;
+  comments: Types.ObjectId[];
+  likes: Types.ObjectId[];
+}
+
+const postSchema = new Schema<IPost>({
+  desc: { type: String, required: true },
+  img: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  likes: [{ type: Schema.Types.ObjectId, ref: "Like" }],
+});
+
+const Post = model<IPost>("Post", postSchema);
+export default Post;
