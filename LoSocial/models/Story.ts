332e2906b727523f39a0aa30a240eff5f46a47bb
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-05 21:02:39 +0300

    Update Story.ts

--- LoSocial/models/Story.ts
+++ LoSocial/models/Story.ts
@@ -4,14 +4,14 @@ interface IStory {
   createdAt: Date;
   expiresAt: Date;
   img: string;
-  user: Types.ObjectId;
+  userId: Types.ObjectId;
 }
 
 const storySchema = new Schema<IStory>({
   createdAt: { type: Date, default: Date.now },
   expiresAt: { type: Date, required: true },
   img: { type: String, required: true },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
 });
 
 const Story = model<IStory>("Story", storySchema);
