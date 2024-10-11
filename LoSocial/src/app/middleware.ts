ea43e245ebf14b1427c3b63e7e105fa03ee8dbf6
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-11 14:07:36 +0300

    Update middleware.ts

--- LoSocial/src/app/middleware.ts
+++ LoSocial/src/app/middleware.ts
@@ -1,20 +1,11 @@
-import {
-  auth,
-  clerkMiddleware,
-  createRouteMatcher,
-} from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/settings(.*)", "/"]);
+const isProtectedRoute = createRouteMatcher(["/settings(.*)","/"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
