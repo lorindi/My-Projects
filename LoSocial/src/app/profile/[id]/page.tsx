354c10620e674d74f2ee6d8d2dd4b94a0da70e95
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-11 17:18:32 +0300

    Update page.tsx

--- LoSocial/src/app/profile/[id]/page.tsx
+++ LoSocial/src/app/profile/[id]/page.tsx
@@ -4,7 +4,7 @@ import RightMenu from "@/app/components/RightMenu";
 import Image from "next/image";
 import React from "react";
 import connectionToDatabase from "../../../lib/mongoose";
-import User, { IUser } from "../../../models/User"; 
+import User, { IUser } from "../../../models/User";
 import { auth } from "@clerk/nextjs/server";
 
 async function ProfilePage({ params }: { params: { id: string } }) {
@@ -30,40 +30,44 @@ async function ProfilePage({ params }: { params: { id: string } }) {
           <div className="flex flex-col items-center justify-center">
             <div className="w-full h-64 relative">
               <Image
-                src={user.cover || "/noAvatar.png"}
+                src={user.cover || "/noCover.png"}
                 alt=""
                 fill
-                className="object-cover rounded-md"
+                className="rounded-md object-cover"
               />
               <Image
                 src={user.avatar || "/noAvatar.png"}
                 alt=""
                 width={128}
                 height={128}
-                className="rounded-full w-32 h-32 absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover"
+                className="w-32 h-32 rounded-full absolute left-0 right-0 m-auto -bottom-16 ring-4 ring-white object-cover"
               />
             </div>
-            <h1 className="mt-20 mb-4 text-2xl font-medium">{user.name || "Unknown User"}</h1>
-            <div className="flex items-center justify-center gap-12 bg-4">
+            <h1 className="mt-20 mb-4 text-2xl font-medium">
+              {user.name && user.surname
+                ? user.name + " " + user.surname
+                : user.username}
+            </h1>
+            <div className="flex items-center justify-center gap-12 mb-4">
               <div className="flex flex-col items-center">
-                <span className="font-medium">{user.posts.length}</span>
+                <span className="font-medium">{user._count.posts}</span>
                 <span className="text-sm">Posts</span>
               </div>
               <div className="flex flex-col items-center">
-                <span className="font-medium">{user.followers.length}</span>
+                <span className="font-medium">{user._count.followers}</span>
                 <span className="text-sm">Followers</span>
               </div>
               <div className="flex flex-col items-center">
-                <span className="font-medium">{user.followings.length}</span>
+                <span className="font-medium">{user._count.followings}</span>
                 <span className="text-sm">Following</span>
               </div>
             </div>
           </div>
-          <Feed />
+          <Feed username={user.username} />
         </div>
       </div>
       <div className="hidden lg:block w-[30%]">
-        <RightMenu userId={id} />
+        <RightMenu user={user} />
       </div>
     </div>
   );
