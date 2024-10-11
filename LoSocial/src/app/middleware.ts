ccaa7f1552e94ddd7d9a1182988677b3719d2910
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-11 15:42:10 +0300

    Update middleware.ts

--- LoSocial/src/app/middleware.ts
+++ LoSocial/src/app/middleware.ts
@@ -7,5 +7,8 @@ export default clerkMiddleware((auth, req) => {
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+}
