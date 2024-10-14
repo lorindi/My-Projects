4ad4cc9a35529792f9f32fd9a752a1bed4b93c4f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-15 00:52:02 +0300

    Create middleware.ts

---
+++ LoSocial/src/middleware.ts
@@ -0,0 +1,22 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/settings(.*)",
+  "/",
+
+]);
+
+export default clerkMiddleware((auth, req) => {
+  console.log("Middleware invoked for route:", req.nextUrl.pathname);
+  if (isProtectedRoute(req)) {
+    console.log("Protecting route:", req.nextUrl.pathname);
+    auth().protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+}
