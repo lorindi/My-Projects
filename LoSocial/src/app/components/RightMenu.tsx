d7d9ec6969742cdc9b5ec58c23d712f07367688e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-14 23:21:20 +0300

    Update RightMenu.tsx

--- LoSocial/src/app/components/RightMenu.tsx
+++ LoSocial/src/app/components/RightMenu.tsx
@@ -4,14 +4,15 @@ import Birthdays from "./Birthdays";
 import Ad from "./Ad";
 import UserInformationCard from "./UserInformationCard";
 import UserMediaCard from "./UserMediaCard";
+import User, { IUser } from "../../models/User";
 
-function RightMenu({ userId }: { userId: string }) {
+function RightMenu({ user }: { user: IUser }) {
   return (
     <div className="flex flex-col gap-6">
-      {userId ? (
+      {user ? (
         <>
-          <UserInformationCard userId={userId} />
-          <UserMediaCard userId={userId} />
+          <UserInformationCard user={user} />
+          <UserMediaCard user={user} />
         </>
       ) : null}
       <FriendsRequests />
