d4d4afc538a1b5470f5ef4d519d127323e6d7760
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-14 23:25:41 +0300

    Update ProfileCard.tsx

--- LoSocial/src/app/components/ProfileCard.tsx
+++ LoSocial/src/app/components/ProfileCard.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
-function ProfileCard() {
+async function ProfileCard() {
   return (
     <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-6">
       <div className="h-20 relative">
