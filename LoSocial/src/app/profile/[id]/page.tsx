a7f1765be7ef9aae45d661ca303e151a315aad3d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-14 23:21:27 +0300

    Update page.tsx

--- LoSocial/src/app/profile/[id]/page.tsx
+++ LoSocial/src/app/profile/[id]/page.tsx
@@ -50,20 +50,20 @@ async function ProfilePage({ params }: { params: { id: string } }) {
             </h1>
             <div className="flex items-center justify-center gap-12 mb-4">
               <div className="flex flex-col items-center">
-                <span className="font-medium">{user._count.posts}</span>
+                <span className="font-medium">{user.posts.length}</span>
                 <span className="text-sm">Posts</span>
               </div>
               <div className="flex flex-col items-center">
-                <span className="font-medium">{user._count.followers}</span>
+                <span className="font-medium">{user.followers.length}</span>
                 <span className="text-sm">Followers</span>
               </div>
               <div className="flex flex-col items-center">
-                <span className="font-medium">{user._count.followings}</span>
+                <span className="font-medium">{user.followings.length}</span>
                 <span className="text-sm">Following</span>
               </div>
             </div>
           </div>
-          <Feed username={user.username} />
+          <Feed user={user}/>
         </div>
       </div>
       <div className="hidden lg:block w-[30%]">
