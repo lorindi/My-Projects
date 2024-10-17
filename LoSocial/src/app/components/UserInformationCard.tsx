a25b8d2c27274c745a9a762672968148b1c947ed
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-17 17:30:59 +0300

    Update UserInformationCard.tsx

--- LoSocial/src/app/components/UserInformationCard.tsx
+++ LoSocial/src/app/components/UserInformationCard.tsx
@@ -3,7 +3,13 @@ import Link from "next/link";
 import React from "react";
 import User, { IUser } from "../../models/User";
 
-function UserInformationCard({ user }: { user: IUser}) {
+function UserInformationCard({ user }: { user: IUser }) {
+  const createdAtDate = new Date(user.createdAt);
+  const formattedDate = createdAtDate.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
   return (
     <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
       {/* TOP */}
@@ -16,51 +22,77 @@ function UserInformationCard({ user }: { user: IUser}) {
       {/* BOTTOM */}
       <div className="flex flex-col gap-4 text-gray-500">
         <div className="flex items-center gap-2">
-          <span className="text-xl text-black">Loyd Fleming</span>
-          <span className="text-sm">@jonathan</span>
+          <span className="text-xl text-black">
+            {user.name && user.surname
+              ? user.name + " " + user.surname
+              : user.username}
+          </span>
+          <span className="text-sm">{user.email}</span>
         </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quo quasi
-          voluptatibus perspiciatis.
-        </p>
-        <div className="flex items-center gap-2">
-          <Image src="/map.png" alt="" width={16} height={16} className="" />
-          <p className="">
-            Living in <b>Denver</b>
-          </p>
-        </div>
+        {user.description && <p>{user.description}</p>}
 
-        <div className="flex items-center gap-2">
-          <Image src="/school.png" alt="" width={16} height={16} className="" />
-          <p className="">
-            Want to <b>Edgar High School</b>
-          </p>
-        </div>
+        {user.city && (
+          <div className="flex items-center gap-2">
+            <Image src="/map.png" alt="" width={16} height={16} className="" />
+            <p className="">
+              Living in <b>{user.city}</b>
+            </p>
+          </div>
+        )}
 
-        <div className="flex items-center gap-2">
-          <Image src="/work.png" alt="" width={16} height={16} className="" />
-          <p className="">
-            Works at <b>Apple Inc.</b>
-          </p>
-        </div>
+        {user.school && (
+          <div className="flex items-center gap-2">
+            <Image
+              src="/school.png"
+              alt=""
+              width={16}
+              height={16}
+              className=""
+            />
+            <p className="">
+              Want to <b>{user.school}</b>
+            </p>
+          </div>
+        )}
+
+        {user.work && (
+          <div className="flex items-center gap-2">
+            <Image src="/work.png" alt="" width={16} height={16} className="" />
+            <p className="">
+              Works at <b>{user.work}</b>
+            </p>
+          </div>
+        )}
 
         <div className="flex items-center justify-between">
-          <div className="flex gap-1 items-center">
-            <Image src="/link.png" alt="" width={16} height={16} className="" />
-            <Link
-              href="https://lorindi.vercel.app/"
-              className="text-blue-500 font-medium"
-            >
-              lora.mitova
-            </Link>
-          </div>
+          {user.website && (
+            <div className="flex gap-1 items-center">
+              <Image
+                src="/link.png"
+                alt=""
+                width={16}
+                height={16}
+                className=""
+              />
+              <Link
+                href="https://lorindi.vercel.app/"
+                className="text-blue-500 font-medium"
+              >
+                {user.website}
+              </Link>
+            </div>
+          )}
           <div className="flex gap-1 items-center">
             <Image src="/date.png" alt="" width={16} height={16} className="" />
-            <span>Joined November 2024</span>
+            <span>Joined {formattedDate}</span>
           </div>
         </div>
-        <button className="bg-blue-500 text-white text-sm rounded-md p-[5px]">Follow</button>
-        <span className="text-red-400 self-end text-xs cursor-pointer">Block User</span>
+        <button className="bg-blue-500 text-white text-sm rounded-md p-[5px]">
+          Follow
+        </button>
+        <span className="text-red-400 self-end text-xs cursor-pointer">
+          Block User
+        </span>
       </div>
     </div>
   );
