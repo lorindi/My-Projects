c41c9d2c269324ef5f7b8ba81d2213a97726314f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-18 22:18:45 +0300

    Update page.tsx

--- LoSocial/src/app/page.tsx
+++ LoSocial/src/app/page.tsx
@@ -1,9 +1,10 @@
-import AddPost from "./components/AddPost";
-import Feed from "./components/Feed";
-import LeftMenu from "./components/LeftMenu";
-import RightMenu from "./components/RightMenu";
-import Stories from "./components/Stories";
+
 import connectionToDatabase from "../lib/mongoose";
+import AddPost from "./components/AddPost";
+import Feed from "./components/feed/Feed";
+import LeftMenu from "./components/leftMenu/LeftMenu";
+import RightMenu from "./components/rightMenu/RightMenu";
+import Stories from "./components/Stories";
 const Homepage = () => {
   const user = "1"
   return (
