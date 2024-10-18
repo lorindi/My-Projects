b64ab1e8a8cc794ea88fce4f67d47e3ac3ceeadf
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-18 22:18:38 +0300

    Create UserMediaCard.tsx

---
+++ LoSocial/src/app/components/rightMenu/UserMediaCard.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import User, { IUser } from "../../../models/User";
+
+function UserMediaCard({ user }: { user: IUser }) {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+      {/* TOP */}
+      <div className="flex justify-between items-center font-medium">
+        <span className="text-gray-500">User Media</span>
+        <Link href="/" className="text-blue-500 text-xs">
+          See all
+        </Link>
+      </div>
+      {/* BOTTOM */}
+      <div className="flex gap-4 justify-between flex-wrap">
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/28398729/pexels-photo-28398729/free-photo-of-silhouette-shadow-on-vibrant-green-leaf.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/28398729/pexels-photo-28398729/free-photo-of-silhouette-shadow-on-vibrant-green-leaf.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/28398729/pexels-photo-28398729/free-photo-of-silhouette-shadow-on-vibrant-green-leaf.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <div className="relative w-1/5 h-24">
+          <Image
+            src="https://images.pexels.com/photos/28398729/pexels-photo-28398729/free-photo-of-silhouette-shadow-on-vibrant-green-leaf.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserMediaCard;
