4411416436a828c396e3cc4ac1100f7e057fd955
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-23 12:30:06 +0300

    Update page.tsx

--- LoSocial/src/app/page.tsx
+++ LoSocial/src/app/page.tsx
@@ -1,22 +1,24 @@
-
+import User from "@/models/User";
 import connectionToDatabase from "../lib/mongoose";
 import AddPost from "./components/AddPost";
 import Feed from "./components/feed/Feed";
 import LeftMenu from "./components/leftMenu/LeftMenu";
 import RightMenu from "./components/rightMenu/RightMenu";
 import Stories from "./components/Stories";
-const Homepage = () => {
-  const user = "1"
+import { auth } from "@clerk/nextjs/server";
+const Homepage = async () => {
+  const { userId } = auth();
+  const user = await User.findOne({ clerkId: userId });
   return (
     <div className="flex gap-6 pt-6">
       <div className="hidden xl:block w-[20%]">
-        <LeftMenu type='home' />
+        <LeftMenu type="home" />
       </div>
       <div className="w-full lg:w-[70%] xl:w-[50%]">
         <div className="flex flex-col gap-6">
           <Stories />
           <AddPost />
-          <Feed user={user}/>
+          <Feed user={user} />
         </div>
       </div>
       <div className="hidden lg:block w-[30%]">
