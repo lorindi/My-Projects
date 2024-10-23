97807b23be801eb03dcddaac0ef2c4974584dd77
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-23 12:30:08 +0300

    Update actions.ts

--- LoSocial/src/lib/actions.ts
+++ LoSocial/src/lib/actions.ts
@@ -6,32 +6,47 @@ import User, { IUser } from "@/models/User";
 import Follower from "@/models/Follower";
 import FollowRequest from "@/models/FollowRequest";
 
-export const switchFollow = async (userId: string) => {
+export const switchFollow = async (loggedInUserId: Object) => {
+  // Свързване с базата данни
   connectionToDatabase();
+
+  // Извличане на текущия потребителски ID от auth
   const { userId: currentUserId } = auth();
 
   if (!currentUserId) throw new Error("User is not authenticated!");
+
+  // Намиране на текущия потребител в базата данни
   const user = await User.findOne({ clerkId: currentUserId });
-  if (!user) throw new Error("Current user not found!");
+  if (!user) throw new Error("User not found!");
 
   try {
+    // Проверка дали вече съществува следване
     const existingFollow = await Follower.findOne({
       followerId: user?._id,
-      followingId: userId,
+      followingId: loggedInUserId,
     });
     if (existingFollow) {
-      await Follower.findByIdAndDelete(existingFollow._id);
+      // Премахване на съществуващото следване
+      await Follower.findByIdAndDelete({ _id: existingFollow._id });
     } else {
+      // Проверка дали има съществуваща заявка за следване
       const existingFollowRequest = await FollowRequest.findOne({
         senderId: user?._id,
-        receiverId: userId,
+        receiverId: loggedInUserId,
       });
+
       if (existingFollowRequest) {
-        await FollowRequest.findByIdAndDelete(existingFollowRequest._id);
+        // Премахване на съществуваща заявка за следване
+        await FollowRequest.findByIdAndDelete({
+          _id: existingFollowRequest._id,
+        });
       } else {
+        // Създаване на нова заявка за следване
         await FollowRequest.create({
-          senderId: user?._id,
-          receiverId: userId,
+          data: {
+            senderId: user?._id,
+            receiverId: loggedInUserId,
+          },
         });
       }
     }
