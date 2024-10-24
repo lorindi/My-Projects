3b1214871cd1d19b52de1cd3658e07c9088f07d7
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-10-24 10:05:09 +0300

    Update UserInfoCardInteraction.tsx

--- LoSocial/src/app/components/rightMenu/UserInfoCardInteraction.tsx
+++ LoSocial/src/app/components/rightMenu/UserInfoCardInteraction.tsx
@@ -1,45 +1,45 @@
-// "use client";
-// import React, { useState } from "react";
+"use client";
+import React, { useState } from "react";
 
-// function UserInfoCardInteraction({
-//   userId,
-//   currentUserId,
-//   isUserBlocked,
-//   isFollowing,
-//   isFollowingSent,
+function UserInfoCardInteraction({
+  userId,
+  currentUserId,
+  isUserBlocked,
+  isFollowing,
+  isFollowingSent,
 
-// }: {
-//   userId: string;
-//   currentUserId: string;
-//   isUserBlocked: boolean;
-//   isFollowing: boolean;
-//   isFollowingSent: boolean;
-// }) {
+}: {
+  userId: string;
+  currentUserId: string;
+  isUserBlocked: boolean;
+  isFollowing: boolean;
+  isFollowingSent: boolean;
+}) {
 
-//     const [userState, setUserState] = useState({
-//         following:isFollowing,
-//         blocked:isUserBlocked,
-//         followingRequestSent:isFollowingSent,
+    const [userState, setUserState] = useState({
+        following:isFollowing,
+        blocked:isUserBlocked,
+        followingRequestSent:isFollowingSent,
 
-//     })
-//   return (
-//     <>
-//       <form action="">
-//         <button className="w-full bg-blue-500 text-white text-sm rounded-md p-[5px]">
-//           {isFollowing
-//             ? "Following"
-//             : userState.followingRequestSent
-//             ? "Friend Request Sent"
-//             : "Follow"}
-//         </button>
-//       </form>
-//       <form action="" className="self-end">
-//         <span className="text-red-400 self-end text-xs cursor-pointer">
-//           {userState.blocked ? "Unblock User" : "Block User"}
-//         </span>
-//       </form>
-//     </>
-//   );
-// }
+    })
+  return (
+    <>
+      <form action="">
+        <button className="w-full bg-blue-500 text-white text-sm rounded-md p-[5px]">
+          {isFollowing
+            ? "Following"
+            : userState.followingRequestSent
+            ? "Friend Request Sent"
+            : "Follow"}
+        </button>
+      </form>
+      <form action="" className="self-end">
+        <span className="text-red-400 self-end text-xs cursor-pointer">
+          {userState.blocked ? "Unblock User" : "Block User"}
+        </span>
+      </form>
+    </>
+  );
+}
 
-// export default UserInfoCardInteraction;
+export default UserInfoCardInteraction;
