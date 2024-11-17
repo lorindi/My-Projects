14f4273ff1ab43504fb97ee62d02ed097c89fa46
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-17 15:29:10 +0200

    Create store.js

---
+++ HealthyAndDelicious/client/src/app/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
