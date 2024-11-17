4d0d194f7e9e4dfa24b048082b691cd98f4197d3
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-17 15:29:19 +0200

    Update main.jsx

--- HealthyAndDelicious/client/src/main.jsx
+++ HealthyAndDelicious/client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-
+import { store } from "./app/store.js";
+import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   </BrowserRouter>
 );
