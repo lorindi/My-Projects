f152bf47d0efd2149a801e0dfa0eea5519407022
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-17 15:29:30 +0200

    Update SignIn.jsx

--- HealthyAndDelicious/client/src/routes/signIn/SignIn.jsx
+++ HealthyAndDelicious/client/src/routes/signIn/SignIn.jsx
@@ -2,59 +2,18 @@ import { useState } from "react";
 import "./SignIn.scss";
 
 function SignIn() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState(null);
-  const [touched, setTouched] = useState({
-    name: false,
-    password: false,
-  });
-
-  const validatePassword = () => {
-    if (!password) {
-      return 'Password is required!';
-    }
-    if (password.length < 6) {
-      return 'Password should have more than 6 characters!';
-    }
-    return null;
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const passwordError = validatePassword();
-    if (passwordError) {
-      setErrors(passwordError);
-      return;
-    }
-  };
-
-  const handleBlur = (field) => (e) => {
-    setTouched({
-      ...touched,
-      [field]: true,
-    });
-  };
-
-  const passwordError = validatePassword();
-  const isFormInvalid = !email || passwordError;
+ 
 
   return (
     <div className="signInSection">
-      <form className="signInForm" onSubmit={handleSubmit}>
+      <form className="signInForm" >
         <label htmlFor="email" className="signInFormLabel">
           Email
         </label>
         <input
           type="text"
           name="email"
-          className={`signInFormInput ${
-            touched.name && !email ? "signInFormInputError" : ""
-          }`}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          onBlur={handleBlur("email")}
-          required
+
         />
 
         <label htmlFor="password" className="signInFormLabel">
@@ -63,25 +22,17 @@ function SignIn() {
         <input
           type="password"
           name="password"
-          className={`signInFormInput ${
-            touched.password && passwordError ? "signInFormInputError" : ""
-          }`}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          onBlur={handleBlur("password")}
-          required
+
         />
 
-        {touched.password && passwordError && (
-          <p className="error">{passwordError}</p>
-        )}
+    
+          <p className="error"></p>
+        
 
-        {errors && <p className="mainError">{errors}</p>}
+   
 
         <button
           type="submit"
-          disabled={isFormInvalid}
-          style={{ backgroundColor: isFormInvalid ? "lightgray" : "orange" }}
           className="signInFormButton"
         >
           SignIn
