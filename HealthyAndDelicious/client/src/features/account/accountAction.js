caa5672fa75ca7335f65c41e2c7a45093dc4eca2
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-25 15:23:49 +0200

    Create accountAction.js

---
+++ HealthyAndDelicious/client/src/features/account/accountAction.js
@@ -0,0 +1,49 @@
+import api from "../../services/axiosConfig";
+
+import { signInAccount, createAccount, signOutAccount } from "./accountSlice";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+// Асинхронно действие за вход
+export const signInUser = createAsyncThunk(
+  "account/signInUser",
+  async (credentials, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await api.post("/auth/sign-in", credentials);
+      const token = response.data.token;
+      localStorage.setItem("token", token);
+      dispatch(signInAccount(response.data.userInfo));
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || { message: "Unknown error" });
+    }
+  }
+);
+
+// Асинхронно действие за създаване на акаунт
+export const registerUser = createAsyncThunk(
+  "account/registerUser",
+  async (userData, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await api.post("/auth/create-account", userData);
+      dispatch(createAccount(response.data));
+      return response.data;
+    } catch (error) {
+      console.error("Registration failed:", error);
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+// Асинхронно действие за изход
+export const logoutUser = createAsyncThunk(
+  "account/logoutUser",
+  async (_, { dispatch, rejectWithValue }) => {
+    try {
+      await api.post("/auth/logout");
+      dispatch(signOutAccount());
+    } catch (error) {
+      console.error("Logout failed:", error);
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
