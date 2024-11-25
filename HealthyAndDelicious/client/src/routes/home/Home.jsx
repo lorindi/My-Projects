39b55bba863871883e6be7da08470c165e276986
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-25 15:26:18 +0200

    Update Home.jsx

--- HealthyAndDelicious/client/src/routes/home/Home.jsx
+++ HealthyAndDelicious/client/src/routes/home/Home.jsx
@@ -1,17 +1,17 @@
 import "./Home.scss";
-
+import { Link } from "react-router-dom";
 function Home() {
   return (
-    <div class="cover">
-      <div class="cover-title">
-        <p class="cover-title-text">All new recipes,</p>
-        <p class="cover-title-text">you can find </p>
-        <p class="cover-title-text">
+    <div className="cover">
+      <div className="cover-title">
+        <p className="cover-title-text">All new recipes,</p>
+        <p className="cover-title-text">you can find </p>
+        <p className="cover-title-text">
           only <span>here</span>!
         </p>
-        <a class="about-recipe-link" routerLink="/dashboard">
+        <Link className="about-recipe-link" to="/dashboard">
           Recipes
-        </a>
+        </Link>
       </div>
     </div>
   );
