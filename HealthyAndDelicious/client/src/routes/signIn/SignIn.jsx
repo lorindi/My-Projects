60f7da36171020f44e3b64426622a0b9d2fbf5de
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-25 15:26:23 +0200

    Update SignIn.jsx

--- HealthyAndDelicious/client/src/routes/signIn/SignIn.jsx
+++ HealthyAndDelicious/client/src/routes/signIn/SignIn.jsx
@@ -1,45 +1,95 @@
-import { useState } from "react";
-import "./SignIn.scss";
+import { TextField, Button, Box, Typography, Container } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { signInUser } from "../../features/account/accountAction";
+
+const schema = yup.object().shape({
+  email: yup.string().email("Invalid email").required("Email is required"),
+  password: yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
+});
 
 function SignIn() {
- 
+  const dispatch = useDispatch();
+  const { control, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+  });
+  const accountStatus = useSelector((state) => state.account.status);
+  const accountError = useSelector((state) => state.account.error);
+
+  const onSubmit = (data) => {
+    dispatch(signInUser(data));
+    console.log(data);
+    
+  };
 
   return (
-    <div className="signInSection">
-      <form className="signInForm" >
-        <label htmlFor="email" className="signInFormLabel">
-          Email
-        </label>
-        <input
-          type="text"
-          name="email"
-
-        />
-
-        <label htmlFor="password" className="signInFormLabel">
-          Password
-        </label>
-        <input
-          type="password"
-          name="password"
-
-        />
-
-    
-          <p className="error"></p>
-        
-
-   
-
-        <button
-          type="submit"
-          className="signInFormButton"
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          Sign In
+        </Typography>
+        <Box
+          component="form"
+          noValidate
+          onSubmit={handleSubmit(onSubmit)}
+          sx={{ mt: 1 }}
         >
-          SignIn
-        </button>
-
-      </form>
-    </div>
+          <Controller
+            name="email"
+            control={control}
+            defaultValue=""
+            render={({ field, fieldState }) => (
+              <TextField
+                {...field}
+                margin="normal"
+                fullWidth
+                label="Email Address"
+                error={!!fieldState.error}
+                helperText={fieldState.error?.message}
+              />
+            )}
+          />
+          <Controller
+            name="password"
+            control={control}
+            defaultValue=""
+            render={({ field, fieldState }) => (
+              <TextField
+                {...field}
+                margin="normal"
+                fullWidth
+                label="Password"
+                type="password"
+                error={!!fieldState.error}
+                helperText={fieldState.error?.message}
+              />
+            )}
+          />
+          {accountError && (
+            <Typography color="error" variant="body2">
+              {accountError.message || "Sign-in failed. Please try again."}
+            </Typography>
+          )}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            {accountStatus === "pending" ? "Signing In..." : "Sign In"}
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 }
 
