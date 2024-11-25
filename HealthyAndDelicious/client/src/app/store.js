c9a5aac2ab0ec62ba36fe53e86787ab3c82698aa
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-25 15:23:25 +0200

    Update store.js

--- HealthyAndDelicious/client/src/app/store.js
+++ HealthyAndDelicious/client/src/app/store.js
@@ -1,7 +1,22 @@
+// Импортиране на функцията configureStore от Redux Toolkit за създаване на Redux Store
 import { configureStore } from "@reduxjs/toolkit";
+
+// Импортиране на redux-thunk middleware за обработка на асинхронни действия
+import { thunk } from "redux-thunk";
+
+// Импортиране на редуктори от различни slice файлове
 import counterReducer from "../features/counter/counterSlice";
+import accountReducer from "../features/account/accountSlice";
+import recipesReducer from "../features/recipes/recipesSlice";
+
+// Създаване и конфигуриране на Redux Store
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    // Дефиниране на редуктори за различни части на състоянието
+    counter: counterReducer, // Редуктор за управление на състоянието на брояча
+    account: accountReducer, // Редуктор за управление на състоянието на акаунта
+    recipes: recipesReducer, // Редуктор за управление на състоянието на рецепти
   },
+  // Добавяне на middleware, което включва thunk за обработка на асинхронни действия
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)
 });
