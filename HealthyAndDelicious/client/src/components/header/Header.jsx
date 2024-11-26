b2dafc9dab8a9af931b15d2b1796196781f8bdf4
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-26 15:19:52 +0200

    Update Header.jsx

--- HealthyAndDelicious/client/src/components/header/Header.jsx
+++ HealthyAndDelicious/client/src/components/header/Header.jsx
@@ -2,19 +2,23 @@ import "./Header.scss";
 import Web from "./web/Web";
 import Mobile from "./mobile/Mobile";
 import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 function Header() {
   const location = useLocation()
+  const isAuthenticated = useSelector((state) => state.account.isAuthenticated);
+
   if (location.pathname === '/admin'){
     return null
   }
+
   return (
     <header className="header">
       <div className="web">
-        <Web />
+        <Web isAuthenticated={isAuthenticated}/>
       </div>
  
       <div className="mobile">
-        <Mobile />
+        <Mobile isAuthenticated={isAuthenticated}/>
       </div>
     </header>
   );
