c24c463c5693ed3878324b409eb30ae085102543
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-26 15:19:56 +0200

    Update Web.jsx

--- HealthyAndDelicious/client/src/components/header/web/Web.jsx
+++ HealthyAndDelicious/client/src/components/header/web/Web.jsx
@@ -1,19 +1,20 @@
 import "./Web.scss";
 import { Link } from "react-router-dom";
-function Web() {
-
+function Web({ isAuthenticated }) {
   return (
     <nav className="navigation">
       <div className="contentLogo">
-        <img src="/logo.png" alt=""  className="logo"/>
+        <img src="/logo.png" alt="" className="logo" />
         <Link className="logoLink" to="/">
           Healthy Delicious
         </Link>
       </div>
       <div className="contentLinks">
-        <Link className="link" to="/create-recipe">
-          Create Recipe
-        </Link>
+        {isAuthenticated && (
+          <Link className="link" to="/create-recipe">
+            Create Recipe
+          </Link>
+        )}
         <Link className="link" to="/recipes">
           Recipes
         </Link>
@@ -23,19 +24,23 @@ function Web() {
         <Link className="link" to="/contact">
           Contacts
         </Link>
-
-       
       </div>
       <div className="contentLinksIcon">
-        <Link className="link" to="/profile">
-          <i className="fa-solid fa-user"></i>Profile
-        </Link>
-        {/* <Link className="linkSignIn" to="/sign-in">
-          Sign In
-        </Link>
-        <Link className="linkCreateAccount" to="/create-account">
-          Create Account
-        </Link> */}
+        {isAuthenticated && (
+          <Link className="link" to="/profile">
+            <i className="fa-solid fa-user"></i>Profile
+          </Link>
+        )}
+        {!isAuthenticated && (
+          <>
+            <Link className="linkSignIn" to="/sign-in">
+              Sign In
+            </Link>
+            <Link className="linkCreateAccount" to="/create-account">
+              Create Account
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
