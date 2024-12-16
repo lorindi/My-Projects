88a3dc8ece468ae0f1552d408e687de3b3c055ac
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-12-16 23:46:14 +0200

    Update Heroine.jsx

--- Portfolio/portfolio/src/components/heroine/Heroine.jsx
+++ Portfolio/portfolio/src/components/heroine/Heroine.jsx
@@ -3,65 +3,17 @@ import loraImg from "./IMG_20231225_205932.jpg";
 import scrollImg from "./pngwing.com.png";
 import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
-const textVariants = {
-  initial: {
-    x: -500,
-    opacity: 0,
-  },
-  animate: {
-    x: 0,
-    opacity: 1,
-    transition: {
-      duration: 1,
-      staggerChildren: 0.1,
-    },
-  },
-  scrollButton: {
-    opacity: 0,
-    y: 10,
-    transition: {
-      duration: 2,
-      repeat: Infinity,
-    },
-  },
-};
-
-const sliderVariants = {
-  initial: {
-    x: 0,
-  },
-  animate: {
-    x: "-700%",
-    transition: {
-      repeat: Infinity,
-      repeatType: "mirror",
-      duration: 30,
-    },
-  },
-};
-
-const typewriterVariants = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.1,
-    },
-  },
-};
-
-const letterVariants = {
-  hidden: { opacity: 0, y: 50 },
-  visible: {
-    opacity: 1,
-    y: 0,
-  },
-};
+import {
+  textVariants,
+  sliderVariants,
+  typewriterVariants,
+  letterVariants,
+} from "./animations.js";
 
 const phrases = [
   "I am Lora Mitova, a passionate developer.",
   "I don't stop until I achieve my goals now.",
-  "I have no days off as coding is my passion."
+  "I have no days off as coding is my passion.",
 ];
 export const Heroine = () => {
   const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
